4cb5acfdbd8365cfa5ede79a431be08756d1c285
Riccardo <riccardo.bellan@cern.ch>
2014-02-05 16:40:30 +0100

    first time in

---
+++ Producers/plugins/WCandidateFiller.cc
@@ -0,0 +1,97 @@
+/** \class WCandidateFiller
+ *
+ *  No description available.
+ *
+ *  $Date: 2014/01/28 00:00:00 $
+ *  $Revision: 1.1 $
+ *  \author R. Bellan (Torino)
+ */
+
+#include <FWCore/Framework/interface/Frameworkfwd.h>
+#include <FWCore/Framework/interface/EDProducer.h>
+#include <FWCore/Framework/interface/Event.h>
+#include <FWCore/ParameterSet/interface/ParameterSet.h>
+#include <FWCore/Framework/interface/ESHandle.h>
+#include <CommonTools/Utils/interface/StringCutObjectSelector.h>
+#include <CommonTools/Utils/interface/StringObjectFunction.h>
+#include <DataFormats/PatCandidates/interface/CompositeCandidate.h>
+#include <DataFormats/Candidate/interface/ShallowCloneCandidate.h>
+#include <DataFormats/PatCandidates/interface/Muon.h>
+#include <DataFormats/PatCandidates/interface/Electron.h>
+#include <ZZAnalysis/AnalysisStep/interface/PhotonFwd.h>
+
+#include <ZZAnalysis/AnalysisStep/interface/CutSet.h>
+#include <ZZAnalysis/AnalysisStep/interface/LeptonIsoHelper.h>
+#include <ZZAnalysis/AnalysisStep/interface/DaughterDataHelpers.h>
+
+#include <string>
+#include <Math/VectorUtil.h>
+
+class WCandidateFiller : public edm::EDProducer {
+ public:
+  /// Constructor
+  explicit WCandidateFiller(const edm::ParameterSet&);
+    
+  /// Destructor
+  ~WCandidateFiller(){};  
+
+ private:
+  virtual void beginJob(){};  
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual void endJob(){};
+  
+  edm::InputTag theCandidateTag;
+};
+
+
+WCandidateFiller::WCandidateFiller(const edm::ParameterSet& iConfig) :
+  theCandidateTag(iConfig.getParameter<edm::InputTag>("src"))
+{
+  produces<pat::CompositeCandidateCollection>();
+}
+
+
+void
+WCandidateFiller::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{  
+  using namespace edm;
+  using namespace std;
+  using namespace reco;
+  
+  std::auto_ptr<pat::CompositeCandidateCollection> result(new pat::CompositeCandidateCollection);
+
+  //-- Get jj candidates
+  Handle<View<reco::CompositeCandidate> > jjCands;
+  iEvent.getByLabel(theCandidateTag, jjCands);
+
+
+  //--- Fill user info
+
+  const float WmassValue = 80.385;
+
+  float closestWjjMassDiff = 99999.;
+  int bestWjjIdx = -1;
+
+  //--- Loop over LL Candidates
+  for(unsigned int i = 0; i < jjCands->size(); ++i) {
+    const CompositeCandidate& c = (*jjCands)[i];
+    pat::CompositeCandidate myCand(c); 
+    
+    //--- Find "best Z" (closest to mZ) among those passing the "bestZAmong" selection (2011 PRL logic)
+    float diffWmass = fabs(WmassValue - myCand.mass());
+    if (diffWmass < closestWjjMassDiff) { // Best among any ll in the collection
+      bestWjjIdx = i;
+      closestWjjMassDiff = diffWmass;
+    }
+   
+    result->push_back(myCand);
+  }
+
+ 
+  iEvent.put(result);
+}
+
+
+
+#include <FWCore/Framework/interface/MakerMacros.h>
+DEFINE_FWK_MODULE(WCandidateFiller);
