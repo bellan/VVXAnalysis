20ab8a3f3c03477cee8fa9f22833aef784f7963e
bellan <riccardo.bellan@cern.ch>
2014-02-07 18:38:00 +0100

    adjusted the binning of the generic plots

--- TreeAnalysis/src/EventAnalyzer.cc
+++ TreeAnalysis/src/EventAnalyzer.cc
@@ -225,9 +225,10 @@ void EventAnalyzer::fillBasicPlots(){
 
 
 void EventAnalyzer::fillParticlePlots(const std::string &type, const phys::Particle & lepton){
-  histograms.fill(type+"_pt" , "p_{T} spectrum", 100,   0   , 500   ,lepton.pt(),   theWeight);
-  histograms.fill(type+"_eta", "#eta spectrum" , 100,  -2.5 ,   2.5 ,lepton.eta(), theWeight);
-  histograms.fill(type+"_phi", "#phi spectrum" ,  50,  -3.15,   3.15,lepton.phi(), theWeight);
+  histograms.fill(type+"_pt" ,    "p_{T} spectrum", 100,   0   , 500   ,lepton.pt()    , theWeight);
+  histograms.fill(type+"_eta",    "#eta spectrum" , 100,  -2.5 ,   2.5 ,lepton.eta()   , theWeight);
+  histograms.fill(type+"_phi",    "#phi spectrum" ,  50,  -3.15,   3.15,lepton.phi()   , theWeight);
+  histograms.fill(type+"_charge", "charge"        ,  50,  -25  ,  25   ,lepton.charge(), theWeight);
 }
 
 
@@ -238,13 +239,13 @@ void EventAnalyzer::fillLeptonPlots(const std::string &type, const phys::Lepton 
 
   histograms.fill(type+"_dxy"             , "d_{xy}"         , 200,   0,   0.5, lepton.dxy()            , theWeight);   
   histograms.fill(type+"_dz"              , "d_{z}"          , 200,   0,   0.5, lepton.dz()             , theWeight);
-  histograms.fill(type+"_sip"             , "sip"            , 100, -10,  10  , lepton.sip()            , theWeight); 
+  histograms.fill(type+"_sip"             , "sip"            , 150,   0,  15  , lepton.sip()            , theWeight); 
   histograms.fill(type+"_combRelIso"      , "combRelIso"     , 200,   0,   1  , lepton.combRelIso()     , theWeight);       
   histograms.fill(type+"_pfCombRelIso"    , "pfCombRelIso"   , 200,   0,   1  , lepton.pfChargedHadIso(), theWeight);           
   histograms.fill(type+"_pfNeutralHadIso" , "pfNeutralHadIso", 200,   0,   1  , lepton.pfNeutralHadIso(), theWeight);        
   histograms.fill(type+"_pfChargedHadIso" , "pfChargedHadIso", 200,   0,   1  , lepton.pfPhotonIso()    , theWeight);        
   histograms.fill(type+"_pfPhotonHadIso"  , "pfPhotonHadIso" , 200,   0,   1  , lepton.pfCombRelIso()   , theWeight);         
-  histograms.fill(type+"_rho"             , "rho"            , 200,   0, 200  , lepton.rho()            , theWeight);                    
+  histograms.fill(type+"_rho"             , "rho"            ,  50,   0,  50  , lepton.rho()            , theWeight);                    
 }
 
 
@@ -259,21 +260,24 @@ void EventAnalyzer::fillElectronPlots (const std::string &type, const phys::Elec
 void EventAnalyzer::fillExtraPlotsForElectrons(const std::string &type, const phys::Electron &electron){
 
   histograms.fill      (type+"_energy"    , "energy spectrum"   , 100,  0   , 500   , electron.energy()    , theWeight);  
-  histograms.fill      (type+"_phiWidth"  , "#phi width"        ,  50, -3.15,   3.15, electron.phiWidth()  , theWeight);  
-  histograms.fill      (type+"_etaWidth"  , "#eta width"        , 100, -2.5 ,   2.5 , electron.etaWidth()  , theWeight);  
+  histograms.fill      (type+"_phiWidth"  , "#phi width"        ,  50,  0   ,   2   , electron.phiWidth()  , theWeight);  
+  histograms.fill      (type+"_etaWidth"  , "#eta width"        ,  50,  0   ,   2   , electron.etaWidth()  , theWeight);  
   histograms.fill      (type+"_BDT"       , "BDT"               , 100, -1   ,   1   , electron.BDT()       , theWeight);  
-  histograms.fill<TH1I>(type+"_isBDT"     , "is BDT?"           ,   2,  0   ,   2   , electron.isBDT()     , theWeight);  
-  histograms.fill<TH1I>(type+"_missingHit", "Missing hits"      ,  50,  0   ,  50   , electron.missingHit(), theWeight);  
-  histograms.fill<TH1I>(type+"_nCrystals" , "Number of Crystals",  50,  0   ,  50   , electron.nCrystals() , theWeight);  
+  histograms.fill<TH1I>(type+"_isBDT"     , "is BDT?"           ,   2,  0   ,   2   , electron.isBDT()     , theWeight);  // void??
+  histograms.fill<TH1I>(type+"_missingHit", "Missing hits"      ,  50,  0   ,  50   , electron.missingHit(), theWeight);  // void?? 
+  histograms.fill<TH1I>(type+"_nCrystals" , "Number of Crystals", 200,  0   , 200   , electron.nCrystals() , theWeight);  
 }
 
 
 
 void EventAnalyzer::fillJetPlots(const std::string &type, const phys::Jet      &jet){
 
-  histograms.fill<TH1I>(type+"_nConstituents", 100, 0, 100,jet.nConstituents(),theWeight);
-  histograms.fill<TH1I>(type+"_nCharged"     , 100, 0, 100,jet.nCharged(),theWeight);
-  histograms.fill<TH1I>(type+"_nNeutral"     , 100, 0, 100,jet.nNeutral(),theWeight);
+  fillParticlePlots(type, jet);
+
+
+  histograms.fill<TH1I>(type+"_nConstituents", 120, 0, 120, jet.nConstituents(),theWeight);
+  histograms.fill<TH1I>(type+"_nCharged"     , 100, 0, 100, jet.nCharged(),theWeight);
+  histograms.fill<TH1I>(type+"_nNeutral"     , 100, 0, 100, jet.nNeutral(),theWeight);
 
   histograms.fill(type+"_neutralHadronEnergyFraction", 100, 0, 1, jet.neutralHadronEnergyFraction(), theWeight);
   histograms.fill(type+"_chargedHadronEnergyFraction", 100, 0, 1, jet.chargedHadronEnergyFraction(), theWeight);
@@ -281,22 +285,22 @@ void EventAnalyzer::fillJetPlots(const std::string &type, const phys::Jet      &
   histograms.fill(type+"_neutralEmEnergyFraction"    , 100, 0, 1, jet.neutralEmEnergyFraction    (), theWeight);
   histograms.fill(type+"_muonEnergyFraction"         , 100, 0, 1, jet.muonEnergyFraction         (), theWeight);
 
-  histograms.fill(type+"_csvtagger"    , 200, -1, 1 , jet.csvtagger    (), theWeight);
-  histograms.fill(type+"_girth"        , 200,  0, 1 , jet.girth        (), theWeight);
-  histograms.fill(type+"_girth_charged", 200,  0, 1 , jet.girth_charged(), theWeight);
-  histograms.fill(type+"_ptd"          , 1, 1, 1    , jet.ptd          (), theWeight);
-  histograms.fill(type+"_rms"          , 1, 1, 1    , jet.rms          (), theWeight);
-  histograms.fill(type+"_beta"         , 1, 1, 1    , jet.beta         (), theWeight);
-  histograms.fill(type+"_jetArea"      , 1, 1, 1    , jet.jetArea      (), theWeight);
-  histograms.fill(type+"_secvtxMass"   , 1, 1, 1    , jet.secvtxMass   (), theWeight);
-  histograms.fill(type+"_Lxy"          , 1, 1, 1    , jet.Lxy          (), theWeight);
-  histograms.fill(type+"_LxyErr"       , 1, 1, 1    , jet.LxyErr       (), theWeight);
-  histograms.fill(type+"_rawFactor"    , 1, 1, 1    , jet.rawFactor    (), theWeight);
+  histograms.fill(type+"_csvtagger"    , 200,  0, 1   , jet.csvtagger    (), theWeight);
+  histograms.fill(type+"_girth"        , 200,  0, 1   , jet.girth        (), theWeight);
+  histograms.fill(type+"_girth_charged", 200,  0, 1   , jet.girth_charged(), theWeight);
+  histograms.fill(type+"_ptd"          , 100,  0, 1   , jet.ptd          (), theWeight);
+  histograms.fill(type+"_rms"          ,  50,  0, 0.15, jet.rms          (), theWeight);
+  histograms.fill(type+"_beta"         ,  50,  0, 1   , jet.beta         (), theWeight);
+  histograms.fill(type+"_jetArea"      ,  60,  0, 1.2 , jet.jetArea      (), theWeight);
+  histograms.fill(type+"_secvtxMass"   ,  50,  0, 5   , jet.secvtxMass   (), theWeight);
+  histograms.fill(type+"_Lxy"          ,  50,  0, 10  , jet.Lxy          (), theWeight);
+  histograms.fill(type+"_LxyErr"       ,  50,  0, 1.5 , jet.LxyErr       (), theWeight);
+  histograms.fill(type+"_rawFactor"    ,  50,  0, 2.5 , jet.rawFactor    (), theWeight);
 
-  histograms.fill(type+"_uncOnFourVectorScale", 1, 1, 1, jet.uncOnFourVectorScale(), theWeight);
-  histograms.fill(type+"_puMVAFull"  , 1, 1, 1, jet.puMVAFull  (), theWeight);
-  histograms.fill(type+"_puMVASimple", 1, 1, 1, jet.puMVASimple(), theWeight);
-  histograms.fill(type+"_puCutBased" , 1, 1, 1, jet.puCutBased (), theWeight);
+  histograms.fill(type+"_uncOnFourVectorScale", 50, 0, 0.1, jet.uncOnFourVectorScale(), theWeight);
+  histograms.fill(type+"_puMVAFull"  , 100, -1, 1, jet.puMVAFull  (), theWeight);
+  histograms.fill(type+"_puMVASimple", 100, -1, 1, jet.puMVASimple(), theWeight);
+  histograms.fill(type+"_puCutBased" , 100, -1, 1, jet.puCutBased (), theWeight); // void??
 
   histograms.fill<TH1I>(type+"_pass_puMVAFull_loose"   , 2, 0, 2, jet.pass_puMVAFull_loose   (), theWeight);
   histograms.fill<TH1I>(type+"_pass_pUMVAFull_medium"  , 2, 0, 2, jet.pass_pUMVAFull_medium  (), theWeight);
@@ -306,7 +310,7 @@ void EventAnalyzer::fillJetPlots(const std::string &type, const phys::Jet      &
   histograms.fill<TH1I>(type+"_pass_puMVASimple_medium", 2, 0, 2, jet.pass_puMVASimple_medium(), theWeight);
   histograms.fill<TH1I>(type+"_pass_puMVASimple_tight" , 2, 0, 2, jet.pass_puMVASimple_tight (), theWeight);
 				                          	                             
-  histograms.fill<TH1I>(type+"_pass_puCutBased_loose"  , 2, 0, 2, jet.pass_puCutBased_loose  (), theWeight);
-  histograms.fill<TH1I>(type+"_pass_puCutBased_medium" , 2, 0, 2, jet.pass_puCutBased_medium (), theWeight);
-  histograms.fill<TH1I>(type+"_pass_puCutBased_tight"  , 2, 0, 2, jet.pass_puCutBased_tight  (), theWeight);
+  histograms.fill<TH1I>(type+"_pass_puCutBased_loose"  , 2, 0, 2, jet.pass_puCutBased_loose  (), theWeight); // void??
+  histograms.fill<TH1I>(type+"_pass_puCutBased_medium" , 2, 0, 2, jet.pass_puCutBased_medium (), theWeight); // void??
+  histograms.fill<TH1I>(type+"_pass_puCutBased_tight"  , 2, 0, 2, jet.pass_puCutBased_tight  (), theWeight); // void??
 }
