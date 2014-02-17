fc20c460cc56bbe58d2d77f5e25b3b2733942db1
bellan <riccardo.bellan@cern.ch>
2014-02-17 19:02:53 +0100

    some print outs

--- TreeAnalysis/python/run.py
+++ TreeAnalysis/python/run.py
@@ -6,8 +6,7 @@
 ##################################
 
 
-print 'Ciao'
-
+print '*** UNITO Framework ***'
 
 
 import sys, os, commands, math
@@ -15,42 +14,69 @@ from readSampleInfo import *
 
 
 
+
+############################################################################
+############################## User's inputs ###############################
+############################################################################
+
+executable = sys.argv[1]
+typeofsample = sys.argv[2]
+
+getExternalCrossSectionFromFile = False
+if len(sys.argv) > 3: getExternalCrossSectionFromFile = sys.argv[3] 
+
+cregion = 'baseline' # in case, make it a further external argument
+
+luminosity = 19029.853
+baseinputdir = 'samples/'
+
+
+
 ############################## Configuration ##############################
+################ Change this if you know what you are doing ###############
+###########################################################################
+
+
 csvfile = '../Producers/python/samples_8TeV.csv'
+DB = readSampleDB(csvfile)
+
+typeofsamples = typeOfSamples(csvfile)
+typeofsamples.append('test')
+
 failure, output = commands.getstatusoutput('ls ./bin/ | grep -v .cpp | grep -v .xml | grep -v .md')
 availableExecutable = output.split()
-executable = sys.argv[1]
-if executable in availableExecutable:
+if executable in availableExecutable: 
     executable = 'bin/'+executable
 else:
     print "ERROR! Unknown executable. Availble executables are:",availableExecutable
     sys.exit(1)
 
-typeofsamples = typeOfSamples(csvfile)
-typeofsamples.append('test')
 
-DB = readSampleDB(csvfile)
+############################################################################
+############################ Print the configuration #######################
+############################################################################
 
-typeofsample = sys.argv[2]
+print "Configuration:"
+print "Executable:", executable
+print "Sample/type of samples:", typeofsample
+print "Get (again) cross section from csv file:", getExternalCrossSectionFromFile
+print "CSV file:", csvfile
+print "Control region type:", cregion
+print "Integrated luminosity:", luminosity
 
-baseinputdir = '/afs/cern.ch/work/b/bellan/public/samples/'
-
-cregion = 'baseline' #sys.argv[2]
 
 
-
-getExternalCrossSectionFromFile = False
-if len(sys.argv) > 2:
-    getExternalCrossSectionFromFile = sys.argv[2] 
 ############################################################################
 
 
-
-def run(executable, typeofsample, cregion):
+def run(executable, typeofsample, cregion, luminosity):
     inputdir  = 'samples/'
-    outputdir = 'output'
-    
-    lumi = 19029.853
+    outputdir = 'results/'
+    if not os.path.exists(outputdir): os.popen('mkdir "%s"' %outputdir)
+
+    outputdir = outputdir+executable[4:]+"_"+cregion+"/"
+    if not os.path.exists(outputdir): os.popen('mkdir "%s"' %outputdir)
+
 
     datasets = getSamplesBy('process',typeofsample,csvfile)
     if len(datasets) == 0:
@@ -58,9 +84,7 @@ def run(executable, typeofsample, cregion):
 
     sampleprefix = ''
 
-    outputdir = outputdir+"_"+cregion+"/"
-    if not os.path.exists(outputdir):
-        os.popen('mkdir "%s"' %outputdir)
+    
 
     ### Override configuration, for test only ###
     if typeofsample == 'test':
@@ -69,13 +93,13 @@ def run(executable, typeofsample, cregion):
 
     #################################################################################
 
-    ### DATA ###
+    ### Special treatment for DATA ###
 
     #################################################################################
 
     if typeofsample == 'mudata' or typeofsample == 'edata': 
         datasets = ['2012A-13Jul', '2012A-06Aug', '2012B-13Jul', '2012C-24Aug', '2012C-11Dec', '2012C-PromptReco', '2012D-PromptReco']
-        lumi = -1
+        luminosity = -1
         if typeofsample == 'mudata':
             inputdir  = 'samples/'
             sampleprefix = 'MuData-'
@@ -95,7 +119,7 @@ def run(executable, typeofsample, cregion):
         if not typeofsample == 'mudata' and not typeofsample == 'edata' and getExternalCrossSectionFromFile:
             externalXsec = crossSection(period, csvfile)
             print period, " ---> External cross section: ", externalXsec
-        command = "./{0:s} {1:s}/{3:s}.root {2:s}/{3:s}.root {4:.0f} {5:.3f}".format(executable,inputdir,outputdir, basefile, lumi, externalXsec)
+        command = "./{0:s} {1:s}/{3:s}.root {2:s}/{3:s}.root {4:.0f} {5:.3f}".format(executable,inputdir,outputdir, basefile, luminosity, externalXsec)
         print command
         failure, output = commands.getstatusoutput(command)
         print output
@@ -111,12 +135,12 @@ if typeofsample == 'all':
     for sample in typeofsamples:
         if cregion == 'all':
             for cr in range(0,4):
-                run(executable, sample, cr)    # runs over all samples in all control reagions
+                run(executable, sample, cr, luminosity)    # runs over all samples in all control reagions
         else:
-            run(executable, sample, cregion)   # runs over all samples in a specific control reagions
+            run(executable, sample, cregion, luminosity)   # runs over all samples in a specific control reagions
 else:
     if cregion == 'all':
         for cr in range(0,4):     
-            run(executable, typeofsample, cr)  # runs over a specific sample in all control regions
+            run(executable, typeofsample, cr, luminosity)  # runs over a specific sample in all control regions
     else:
-        run(executable, typeofsample, cregion) # runs over a specific sample in a specific region
+        run(executable, typeofsample, cregion, luminosity) # runs over a specific sample in a specific region
