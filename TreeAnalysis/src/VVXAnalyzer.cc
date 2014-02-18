de99d43a8e12d266e1639c6ea5c327ba6a268d0c
bellan <riccardo.bellan@cern.ch>
2014-02-18 09:36:13 +0100

    more colours

--- TreeAnalysis/src/VVXAnalyzer.cc
+++ TreeAnalysis/src/VVXAnalyzer.cc
@@ -1,14 +1,7 @@
 #include "VVXAnalysis/TreeAnalysis/interface/VVXAnalyzer.h"
 
-#include "VVXAnalysis/TreeAnalysis/interface/Colours.h"
-
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
 
-using namespace std;
-
 void VVXAnalyzer::analyze(){
-  Colour cc("\033[0;41m");
-  cout<<cc(3)<<45<<cc("TROTA")<<" " <<cc(9.94)<<endl;
-  
 }
