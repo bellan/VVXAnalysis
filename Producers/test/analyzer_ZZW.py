e30ef3448fef1f4255524bc86b80acab184909a7
Riccardo <riccardo.bellan@cern.ch>
2014-03-12 14:58:02 +0100

    added lepton cleaning

--- Producers/test/analyzer_ZZW.py
+++ Producers/test/analyzer_ZZW.py
@@ -59,7 +59,7 @@ execfile(PyFilePath + "MasterPy/ZZ4lAnalysis.py")         # 2012 reference analy
 #   '/store/cmst3/group/cmgtools/CMG/WZZ_8TeV-aMCatNLO-herwig/Summer12_DR53X-PU_S10_START53_V7C-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_13_1_YZF.root'
 
     #'root://lxcms00//data3/2013/HZZ_cmgTuple/BE539_H1258TeV.root' #533 V5_15_0 version
-    #'/store/cmst3/group/cmgtools/CMG/WZZNoGstarJets_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_10_1_nLP.root'
+#    '/store/cmst3/group/cmgtools/CMG/WZZNoGstarJets_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_10_1_nLP.root'
     #     #'/store/cmst3/group/cmgtools/CMG/WZZ_8TeV-aMCatNLO-herwig/Summer12_DR53X-PU_S10_START53_V7C-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_100_1_GEb.root'
     #     #'/store/cmst3/group/cmgtools/CMG/ZZZNoGstarJets_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_10_1_UV1.root'
 #    )
@@ -86,12 +86,12 @@ process.disambiguatedJets = cms.EDProducer("JetsWithLeptonsRemover",
                                            EnergyFractionAllowed = cms.double(0)) # maximum energy fraction carried by the lepton in the jet, to accept a jet as non from lepton
 
 
-process.centralJets = cms.EDFilter("EtaPtMinCandViewSelector",
-                                     src = cms.InputTag("disambiguatedJets"),
-                                     ptMin   = cms.double(20),
-                                     etaMin = cms.double(-2.5),
-                                     etaMax = cms.double(2.5)
-                                     )
+process.centralJets = cms.EDFilter("EtaPtMinCMGPFJetSelector", 
+                                   src = cms.InputTag("disambiguatedJets"),
+                                   ptMin   = cms.double(20),
+                                   etaMin = cms.double(-2.5),
+                                   etaMax = cms.double(2.5)
+                                   )
 
 
 process.bareWCand = cms.EDProducer("CandViewShallowCloneCombiner",
