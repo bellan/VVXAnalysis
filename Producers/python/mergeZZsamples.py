43860c8da0dd564f1ed4c51e4bd0fdaf58626e71
Riccardo <riccardo.bellan@cern.ch>
2014-03-17 18:36:32 +0100

    avoid double counting for ZZ samples

---
+++ Producers/python/mergeZZsamples.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import sys, os, commands
+
+print "Merging ZZ samples"
+
+outputdir = '/afs/cern.ch/work/b/bellan/public/samples/'
+inputdir  = outputdir+'ZZunmerged'
+
+print inputdir
+
+failure, output = commands.getstatusoutput('ls {0:s}/*.root | grep -v ext'.format(inputdir))
+
+samples = output.split()
+for sample in samples:
+    failure, name = commands.getstatusoutput('basename {0:s} .root'.format(sample))
+    command =  'hadd {0:s}/{1:s}.root {2:s}/{1:s}.root {2:s}/{1:s}_ext.root'.format(outputdir, name, inputdir)
+    print command
+    failure, output = commands.getstatusoutput(command)
