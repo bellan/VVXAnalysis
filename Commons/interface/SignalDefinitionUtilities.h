9d9f3a459890f26ae861a469ba7a0dc908f8de2e
Riccardo <riccardo.bellan@cern.ch>
2014-04-02 15:58:57 +0200

    decouple signal definition from the GenfilterProducer code

--- Commons/interface/SignalDefinitionUtilities.h
+++ Commons/interface/SignalDefinitionUtilities.h
@@ -3,6 +3,15 @@
 #include <utility>
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "VVXAnalysis/DataFormats/interface/Boson.h"
+#include <tuple>
+
+typedef std::tuple<int,           // GenCategory
+  phys::Boson<phys::Particle>,    // Z0 --> ll
+  phys::Boson<phys::Particle>,    // Z1 --> ll
+  phys::Boson<phys::Particle>,    // Z2 --> jj
+  phys::Boson<phys::Particle>     //  W --> jj
+> ZZWGenTopology;
+
 
 std::pair<phys::Boson<phys::Particle>, phys::Boson<phys::Particle> > makeZBosonsFromLeptons(const std::vector<const reco::Candidate *>& lm, const std::vector<const reco::Candidate *>& lp, int leptonCode, float mZ);
 
@@ -11,3 +20,7 @@ std::pair<phys::Boson<phys::Particle>, phys::Boson<phys::Particle> > makeZBosons
 phys::Particle convert(const reco::Candidate&);
 
 int makeVBosonsFromIds(int j0Id, int j1Id);
+
+ZZWGenTopology getGenTopology(int signalDefinition,
+			      const std::vector<phys::Particle> &theGenl, const std::vector<phys::Particle> &theGenj, 
+			      const std::vector<phys::Particle> &theGenZ, const std::vector<phys::Particle> &theGenW);
--- Producers/plugins/GenFilterCategory.cc
+++ Producers/plugins/GenFilterCategory.cc
@@ -26,46 +26,22 @@
 #include <tuple> 
 
 #include "VVXAnalysis/DataFormats/interface/Boson.h"
-#include "VVXAnalysis/Producers/interface/SignalDefinitionUtilities.h"
+#include "VVXAnalysis/Commons/interface/SignalDefinitionUtilities.h"
 
 using namespace std;
 using namespace edm;
 using namespace reco;
 
 
-const float MZ = 91.19;
-const float MW = 80.39;
-
-
 class GenFilterCategory: public edm::EDFilter {
 
 public:
-  typedef std::tuple<uint,uint,int,double> QuarkPairFeatures;
-  typedef std::vector<QuarkPairFeatures> QuarkPairsFeatures;
-
-  struct MassComparator{
-    MassComparator(int id, const double& ref): id_(id), ref_(ref){}
-
-    bool operator()(const QuarkPairFeatures & a , 
-		    const QuarkPairFeatures & b) const{ 
-      if       (abs(std::get<2>(a)) == id_ && abs(std::get<2>(b)) != id_) return true;
-      else if  (abs(std::get<2>(a)) != id_ && abs(std::get<2>(b)) == id_) return false;
-      else if  (abs(std::get<2>(a)) == id_ && abs(std::get<2>(b)) == id_)
-	return fabs(std::get<3>(a) - ref_) < fabs(std::get<3>(b) - ref_); 
-      else    
-	return true;
-    }
-
-    int id_;
-    double ref_;
-  };
   
 
-
   GenFilterCategory(const ParameterSet& pset)
-    : sel_     (pset.getParameter<int>("Category"))
-    , num      (pset.getParameter<int>("SignalDefinition"))
-    , genLabel_(pset.getParameter<edm::InputTag>("src")) {
+    : sel_             (pset.getParameter<int>("Category"))
+    , signalDefinition_(pset.getParameter<int>("SignalDefinition"))
+    , genLabel_        (pset.getParameter<edm::InputTag>("src")) {
     produces<int>();
     produces<std::vector<reco::GenParticle> >();
 
@@ -83,7 +59,7 @@ public:
 
 private:
   int sel_;
-  int num;
+  int signalDefinition_;
   edm::InputTag genLabel_;
   TH1F* category;
 
@@ -94,11 +70,7 @@ void GenFilterCategory::beginJob() {}
 bool GenFilterCategory::filter(Event & event, const EventSetup& eventSetup) { 
 
   std::vector<phys::Particle> theGenZ, theGenW;
-  std::vector<phys::Particle> theGenl, theGenlp, theGenlm, theGenj, theGenq;
-
-  int categoryNum = 999; 
-  int numMu       = 0;
-  int numE        = 0;
+  std::vector<phys::Particle> theGenl, theGenj, theGenq;
   
   // Get the collection of gen particles
   edm::Handle<edm::View<reco::Candidate> > genParticles;
@@ -109,8 +81,7 @@ bool GenFilterCategory::filter(Event & event, const EventSetup& eventSetup) {
     
     if (p->status()==3) {                      // Status 3 particles are the generated ones.
   
-      int s_id = p->pdgId();
-      int id   = abs(s_id);
+      int id   = abs(p->pdgId());
       int idx  = std::distance(genParticles->begin(),p);     
       
       
@@ -122,275 +93,50 @@ bool GenFilterCategory::filter(Event & event, const EventSetup& eventSetup) {
    
       else if ( id == 24 )                   { theGenW.push_back(convert(*p)); } // W 
 
-      else if ( id >= 11 && id <= 16 )       {                                   // leptons  
-	numE  = id == 11 ? numE+1  : numE; numMu = id == 13 ? numMu+1 : numMu;
-	                                       theGenl.push_back (convert(*p));
-	if (s_id>0)                          { theGenlp.push_back(convert(*p));} // positive leptons                                          
-	else                                 { theGenlm.push_back(convert(*p));} // negative leptons                                          
-      } 
+      else if ( id >= 11 && id <= 16 )       { theGenl.push_back (convert(*p));}// leptons        
     }
-    
-  }   
-  //------------------ end of loop over genparticles --------------------------------------------------
-  
-  int leptonCode = 0;
-  if(  numMu == 2 && numE == 2)                                leptonCode = 2;
-  if( (numMu == 4 && numE == 0) || (numMu == 0 && numE == 4) ) leptonCode = 4;
-
-  phys::Boson<phys::Particle> Z0, Z1, Z2, W;
-
-  if ( theGenq.size() >= 2 && theGenl.size() == 4 && (leptonCode == 2 || leptonCode == 4) ) {
-
-    bool isWloose  = false, isZloose  = false, isWtight  = false, isZtight  = false;
-
-    phys::Particle q0, q1;
-    int bosonId = -99;
-
-    QuarkPairsFeatures quarkPairsFeatures;
-    
-    for(uint i = 0;  i < theGenq.size()-1; ++i) for(uint j = i+1;  j < theGenq.size(); ++j)
-      quarkPairsFeatures.push_back(std::make_tuple(i, j, makeVBosonsFromIds(theGenq[i].id(), theGenq[j].id()), (theGenq[i].p4() + theGenq[j].p4()).M()));
-      
-    // ----- Search for a true W in the event -----
-    std::stable_sort(quarkPairsFeatures.begin(), quarkPairsFeatures.end(), MassComparator(24, MW));
-    QuarkPairFeatures bestQuarkPair = quarkPairsFeatures.front();
-    if(abs(std::get<2>(bestQuarkPair)) == 24 and fabs(std::get<3>(bestQuarkPair) - MW) < 10){
-      q0 = theGenq[std::get<0>(bestQuarkPair)];
-      q1 = theGenq[std::get<1>(bestQuarkPair)];
-      if ( q0.pt() < q1.pt() ) { q0 = theGenq[std::get<1>(bestQuarkPair)];  q1 = theGenq[std::get<0>(bestQuarkPair)]; }
-      bosonId =  std::get<2>(bestQuarkPair);
-      isWtight = true;
-    }
-    // --------------------------------------------
-
-    // ----- Search for a true Z in the event -----
-    if(!isWtight){
-      std::stable_sort(quarkPairsFeatures.begin(), quarkPairsFeatures.end(), MassComparator(23, MZ));
-      QuarkPairFeatures bestQuarkPair = quarkPairsFeatures.front();
-      if(abs(std::get<2>(bestQuarkPair)) == 23 and fabs(std::get<3>(bestQuarkPair) - MZ) < 10){
-	q0 = theGenq[std::get<0>(bestQuarkPair)];
-	q1 = theGenq[std::get<1>(bestQuarkPair)];
-	if ( q0.pt() < q1.pt() ) { q0 = theGenq[std::get<1>(bestQuarkPair)];  q1 = theGenq[std::get<0>(bestQuarkPair)]; }
-	bosonId =  std::get<2>(bestQuarkPair);
-	isZtight = true;
-      }
-    }
-    // --------------------------------------------
-
-    // ---- Search for loose W/Z (i.e., not true boson, but rather combinations of partons that resemble a boson ----- 
-
-    if(!isWtight && !isZtight){
-      QuarkPairsFeatures jetPairsFeatures;
-      for(uint i = 0;  i < theGenj.size()-1; ++i) for(uint j = i+1;  j < theGenj.size(); ++j)
-	jetPairsFeatures.push_back(std::make_tuple(i, j, 0, (theGenj[i].p4() + theGenj[j].p4()).M()));
-
-      QuarkPairFeatures bestJetPairW;
-      std::stable_sort(jetPairsFeatures.begin(), jetPairsFeatures.end(), MassComparator(0, MW));
-      bestJetPairW = jetPairsFeatures.front();
-      
-      QuarkPairFeatures bestJetPairZ;
-      std::stable_sort(jetPairsFeatures.begin(), jetPairsFeatures.end(), MassComparator(0, MZ));
-      bestJetPairZ = jetPairsFeatures.front();
-
-      if ( fabs(std::get<3>(bestJetPairZ) - MZ) < 10. ){
-	isZloose = true; 
-	bosonId = 123;
-	q0 = theGenj[std::get<0>(bestJetPairZ)];
-	q1 = theGenj[std::get<1>(bestJetPairZ)];
-	if ( q0.pt() < q1.pt() ) { q0 = theGenj[std::get<1>(bestJetPairZ)];  q1 = theGenj[std::get<0>(bestJetPairZ)]; }
-      }
-
-      // Give priority to W loose, accordingly with background categorization
-      if ( fabs(std::get<3>(bestJetPairW) - MW) < 10. ){
-	isWloose = true;
-	bosonId = 124;  
-	q0 = theGenj[std::get<0>(bestJetPairW)];
-	q1 = theGenj[std::get<1>(bestJetPairW)];
-	if ( q0.pt() < q1.pt() ) { q0 = theGenj[std::get<1>(bestJetPairW)];  q1 = theGenj[std::get<0>(bestJetPairW)]; }
-      }
-	
-    }
-    // --------------------------------------------
-
-    
-    //--------------------1: MC history------------------------------------
-    if ( num==1 ) {              
-      
-      bool LeptonsMotherSelec = true;   
-      for(int t=0; t<4; ++t) LeptonsMotherSelec = LeptonsMotherSelec && theGenl[t].motherId() == 23;
-      
-      
-      if ( theGenW.size() == 1) isWtight = true;      //definition of tight W (mass + cat)
-      if ( theGenZ.size() == 3) isZtight = true;      //definition of tight Z (mass + cat)
-      
-      if ( theGenZ.size() >= 2 && LeptonsMotherSelec ) {
-
-	Z0.setDaughter(0, theGenl[0]);
-	Z0.setDaughter(1, theGenl[1]);
-	Z0.setId(theGenZ[0].id());
-
- 	Z1.setDaughter(0, theGenl[2]);
-	Z1.setDaughter(1, theGenl[3]);
-	Z1.setId(theGenZ[1].id());
-
-
-	if ( isWtight ) {       
-	  // FIXME... it should be done using parentage
-	  W.setDaughter(0, q0);
-	  W.setDaughter(1, q1);
-	  W.setId(theGenW[0].id());  
-	}
-	 
-	else if ( isZtight ) {
-	  // FIXME... it should be done using parentage
-	  Z2.setDaughter(0, q0);
-	  Z2.setDaughter(1, q1);
-	  Z2.setId(theGenZ[2].id());
-	}
-      }
-    }
-    
-
-    // -----------------2: Real signal, MadGraph pairing------------------
-    else if ( num==2 ) {         
-      
-      Z0.setDaughter(0, theGenl[0]);
-      Z0.setDaughter(1, theGenl[1]);
-      Z0.setId(theGenZ[0].id());
-      
-      Z1.setDaughter(0, theGenl[2]);
-      Z1.setDaughter(1, theGenl[3]);
-      Z1.setId(theGenZ[1].id());
-      
-      if (isWloose || isWtight) {      //definition of tight W (mass + cat)
-	
-    	W.setDaughter(0, q0);
-	W.setDaughter(1, q1);
-	W.setId(bosonId);
-	
-      } else if (isZloose || isZtight) {     //definition of tight Z (mass + cat)
-	
-	Z2.setDaughter(0, q0);
-	Z2.setDaughter(1, q1);
-	Z2.setId(bosonId);
-      } 	
-    }
- 
-   
-    //-----------------3: Real signal, real pairing-----------------------
-    else if ( num==3 ) {         
-      std::pair<phys::Boson<phys::Particle>, phys::Boson<phys::Particle> > ZZ = makeZBosonsFromLeptons(theGenlm, theGenlp, leptonCode, MZ);
-
-      Z0 = ZZ.first;
-      Z1 = ZZ.second;
-
-      if ( Z0.p4().M() != 0 && Z1.p4().M() != 0 ) {
-
-	if (isWloose || isWtight) {    //definition of tight W (mass + cat)
-	  
-	  W.setDaughter(0, q0);
-	  W.setDaughter(1, q1);
-	  W.setId(bosonId);
-	  
-	} else if (isZloose || isZtight) {   //definition of tight Z (mass + cat)
-	  
-	  Z2.setDaughter(0, q0);
-	  Z2.setDaughter(1, q1);
-	  Z2.setId(bosonId);
-	  
-	}
-      }     
-    } 
-    
-    else { cout << "*** Signal definition not found! ***" << endl; abort(); }
-
-    //=====================================================================================
-
-    bool hasZZ4l    = fabs(Z0.p4().M()-MZ) < 10. && fabs(Z1.p4().M()-MZ) < 10.;    
-    bool isMySignal = hasZZ4l && isWtight;
-    bool has3Z      = hasZZ4l && isZtight;
-      
-    bool passEtaAccLep = true;
-      
-    for(int i=0; i<4; ++i) passEtaAccLep = passEtaAccLep && fabs(theGenl[i].eta()) < 2.5;       
- 
-    // eta cut for all leptons
-    
-    if ( passEtaAccLep) {
-
-      // ========== Signal: ZZW ==========
-
-      if ( isMySignal ) categoryNum = 0;
-
-      // ========== Background ==========
-
-      else {     
-	
-	if( hasZZ4l ){
-
-	  // ---------- ZZZ ---------- 
-
-	  if ( has3Z )                                categoryNum = 1;
-	  
-	  // ---------- ZZWloose ----------
-
-	  else if ( !has3Z && isWloose )              categoryNum = 2;
-	  
-	  // ---------- ZZZloose ----------
-
-	  else if ( !has3Z && !isWloose && isZloose ) categoryNum = 3;
-	  
-	  // ---------- ZZ+X ----------
-
-	  else                                        categoryNum = 4;
-	}
-
-	else {
-	  
-	  // ---------- WZ+X ----------
-
-	  if ( isWtight )                             categoryNum = 5;
-	  	  
-	  // ---------- ZZjj+X ----------
-	  
-	  else if ( has3Z )                           categoryNum = 6;
-	  
-	  // ---------- ZWloose+X ----------
-	  
-	  else if ( !isWtight && !has3Z && isWloose ) categoryNum = 7;
-	  	  
-	  // ---------- ZZjj+X ----------
-
-	  else if ( !has3Z && !isWloose && isZloose ) categoryNum = 8;
-	  
-	  // ---------- Z+X+Y----------
-
-	  else              	                      categoryNum = 9;
-  	}		
-      }      
-    }
+    //     if(p->status()==1 && p->mother()->pdgId() == 23  && (abs(p->pdgId()) == 11 || abs(p->pdgId()) == 13)){
+    //       numE_st1  = abs(p->pdgId()) == 11 ? numE_st1+1  : numE_st1; numMu_st1 = abs(p->pdgId()) == 13 ? numMu_st1+1 : numMu_st1;
+    //       theGenl_st1.push_back (convert(*p));
+    //       if (p->pdgId() > 0)                  { theGenlp_st1.push_back(convert(*p));} // positive leptons                                          
+    //       else                                 { theGenlm_st1.push_back(convert(*p));} // negative leptons      
+    //     }
   }
- 
-  std::auto_ptr<int> output(new int(categoryNum));
+  //   if(theGenl.size() == 0){
+  //     theGenl  = theGenl_st1;
+  //     theGenlp = theGenlp_st1;
+  //     theGenlm = theGenlm_st1;
+  //     numMu    = numMu_st1;
+  //     numE    = numE_st1;
+  //   }
+  
+  //------------------ end of loop over genparticles --------------------------------------------------
+
+  
+  ZZWGenTopology zzwGenTopology = getGenTopology(signalDefinition_, theGenl, theGenj, theGenZ, theGenW);
+
+  std::auto_ptr<int> output(new int(std::get<0>(zzwGenTopology))); // GenCategory
   event.put(output);
-
-
+  
+  
   std::auto_ptr<std::vector<reco::GenParticle> > outputGenColl(new std::vector<reco::GenParticle>());
-
+  
   GenParticleRefProd genRefs = event.getRefBeforePut<std::vector<reco::GenParticle> >();
 
-  if(Z0.id() > 0) outputGenColl = loadGenBoson(Z0, genRefs, outputGenColl);
-  if(Z1.id() > 0) outputGenColl = loadGenBoson(Z1, genRefs, outputGenColl);
-  if(Z2.id() > 0) outputGenColl = loadGenBoson(Z2, genRefs, outputGenColl);
-  if(fabs(W.id()) > 0)  outputGenColl = loadGenBoson(W, genRefs, outputGenColl);
+  // a simple, beautiful loop over the tuple elements is not possible without writing more code than what implemented in this class, for curiosity see
+  // http://stackoverflow.com/questions/1198260/iterate-over-tuple, http://stackoverflow.com/questions/18155533/how-to-iterate-through-stdtuple, http://www.metaxcompile.com/blog/2013/03/14/A_cleaner_way_to_do_tuple_iteration.html
+  if(     std::get<1>(zzwGenTopology).id()  > 0) outputGenColl = loadGenBoson(std::get<1>(zzwGenTopology), genRefs, outputGenColl); // Z0 --> ll
+  if(     std::get<2>(zzwGenTopology).id()  > 0) outputGenColl = loadGenBoson(std::get<2>(zzwGenTopology), genRefs, outputGenColl); // Z1 --> ll
+  if(     std::get<3>(zzwGenTopology).id()  > 0) outputGenColl = loadGenBoson(std::get<3>(zzwGenTopology), genRefs, outputGenColl); // Z2 --> jj
+  if(fabs(std::get<4>(zzwGenTopology).id()) > 0) outputGenColl = loadGenBoson(std::get<4>(zzwGenTopology), genRefs, outputGenColl); //  W --> jj
   
   event.put(outputGenColl);
   
   if(sel_ >= 0)
-    return sel_ == categoryNum;
+    return sel_ == std::get<0>(zzwGenTopology);
   else
     return true;
- }
+}
 
 
 std::auto_ptr<std::vector<reco::GenParticle> > GenFilterCategory::loadGenBoson(const phys::Boson<phys::Particle> &vb, const GenParticleRefProd &genRefs, std::auto_ptr<std::vector<reco::GenParticle> > outputGenColl){
