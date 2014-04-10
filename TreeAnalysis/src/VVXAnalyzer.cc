ee1a436f5a34659f13a66ae6d937943f49d8bb9c
Riccardo <riccardo.bellan@cern.ch>
2014-04-10 16:19:42 +0200

    add number of events in input

--- TreeAnalysis/src/VVXAnalyzer.cc
+++ TreeAnalysis/src/VVXAnalyzer.cc
@@ -9,15 +9,7 @@ using std::endl;
 
 using namespace phys;
 
-void VVXAnalyzer::analyze(){
+void VVXAnalyzer::analyze(){}
 
-  std::vector<const Particle* > Z;
-  foreach(const Boson<Lepton>& z, *Zmm)
-    Z.push_back(&z);
 
-  foreach(const Boson<Electron>& z, *Zee)
-    Z.push_back(&z);
 
-  std::stable_sort(Z.begin(),Z.end(),MassComparator(ZMASS));
-
-}
