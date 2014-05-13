b106e3bc3fe79a7bae38ce9c9ea6fcf46c42f363
bellan <riccardo.bellan@cern.ch>
2014-05-13 11:00:53 +0200

    new matching function

--- Commons/interface/Utilities.h
+++ Commons/interface/Utilities.h
@@ -3,18 +3,24 @@
 
 #include "VVXAnalysis/DataFormats/interface/Particle.h"
 
-template<typename T> bool isAlmostEqual(const T& a, const T& b, const double &tollerance = 0.0001){
-  if      (a != 0) return abs(a-b)/a < tollerance;
-  else if (b != 0) return abs(a-b)/b < tollerance;
-  else             return abs(a-b)   < tollerance*1e-5;
-  
-}
-
 namespace physmath{
  
   template<typename T1, typename T2> double deltaR(const T1& p1, const T2& p2){
     return sqrt( (p1.phi()-p2.phi())*(p1.phi()-p2.phi()) +
 		 (p1.eta()-p2.eta())*(p1.eta()-p2.eta()) );
+  }
+  
+  template<typename T> bool isAlmostEqual(const T& a, const T& b, const double &tollerance = 0.0001){
+    if      (a != 0) return abs(a-b)/a < tollerance;
+    else if (b != 0) return abs(a-b)/b < tollerance;
+    else             return abs(a-b)   < tollerance*1e-5;
+  }
+  
+  enum MatchingType{ratio,DR};
+  template<typename T1, typename T2> bool isAlmostEqual(const T1& a, const T2& b, MatchingType mtype, const double &tollerance = 0.01){
+
+    if(mtype == MatchingType::DR) return deltaR(a,b) < tollerance;
+    else return false;
   }
 }
 #endif
