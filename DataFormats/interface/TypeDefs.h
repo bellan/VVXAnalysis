227ac8ef0938846f3157484dfa042b6bf1f1f170
bellan <riccardo.bellan@cern.ch>
2014-06-11 16:14:31 +0200

    ftin

---
+++ DataFormats/interface/TypeDefs.h
@@ -0,0 +1,11 @@
+#include "Electron.h"
+#include "Lepton.h"
+#include "Jet.h"
+#include "Boson.h"
+#include "DiBoson.h"
+
+namespace phys{
+  typedef phys::DiBoson<phys::Lepton  , phys::Lepton>   ZZ4MU;
+  typedef phys::DiBoson<phys::Electron, phys::Electron> ZZ4E;
+  typedef phys::DiBoson<phys::Electron, phys::Lepton>   ZZ2E2MU;
+}
