07766817516229c00f7e440377f131d19cf20802
Riccardo <riccardo.bellan@cern.ch>
2014-07-15 13:30:18 +0200

    make it working

--- TreeAnalysis/python/plot.py
+++ TreeAnalysis/python/plot.py
@@ -5,7 +5,7 @@ import ROOT, copy
 from ROOT import TH1F
 
 from readSampleInfo import *
-csvfile = '../Producers/python/samples_8TeV.csv'
+from collections import OrderedDict
 
 analysis = "VVXAnalyzer"
 cregion = "baseline"
@@ -14,23 +14,29 @@ plot = "nCentralJets"
 
 results = "results/{0:s}_{1:s}/".format(analysis,cregion)
 
-typeofsamples = typeOfSamples(csvfile)
+typeofsamples_uno = {'triboson':2,'H':3,'WZ':4,'WZZ':5,'tt':6,'Z':5,'ZZ':1}
+typeofsamples = OrderedDict([('triboson',2) , ('H',3) , ('WZ',4) , ('WZZ',5) , ('tt',6) , ('Z',5) , ('ZZ',1)])
 
 print typeofsamples
 
+files = {}
+for sample in typeofsamples:
+    files[sample] = ROOT.TFile(results+sample+".root")
+    
+
 stack = ROOT.THStack("stack",plot+"_stack")
-for sample in reversed(typeofsamples):
-    if not sample == "Phantom_2e2mu2j" and not sample == "ZZJetsTo4L" and not sample == "W" and not sample == "WW" \
-    and not (sample == 'DoubleMu' or sample == 'DoubleEle' or sample == 'MuEG'): 
-        f = ROOT.TFile(results+sample+".root")
-        h = copy.deepcopy(f.Get(plot))
-        print sample, h.Integral()
-        h.SetLineColor(2)
-        stack.Add(h)
+for sample in typeofsamples:
+    h = files[sample].Get(plot)
+    print sample, ", total number of events: ", h.Integral()
+    h.SetLineColor(typeofsamples[sample])
+    h.SetFillColor(typeofsamples[sample])
+    h.SetMarkerStyle(21)
+    h.SetMarkerColor(typeofsamples[sample])
+    stack.Add(h)
 
 c1 = ROOT.TCanvas(plot+"_stack",plot+"_stack")
 c1.cd()
-stack.Draw()
+stack.Draw('hist')
 c1.SaveAs(plot+"_stack.png")
 input()
 
