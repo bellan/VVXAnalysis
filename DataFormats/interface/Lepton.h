5e5e41cb1bf282f9a09867bff5e2ce34855f9360
Riccardo <riccardo.bellan@cern.ch>
2014-08-08 14:20:49 +0200

    added isolation corrected for FSR

--- DataFormats/interface/Lepton.h
+++ DataFormats/interface/Lepton.h
@@ -33,6 +33,7 @@ namespace phys {
       , pfNeutralHadIso_(-9999.)
       , pfPhotonIso_(-9999.)
       , pfCombRelIso_(-9999.)
+      , pfCombRelIsoFSRCorr_(-9999.)
       , rho_(-9999.) 
       , isPF_(false)
       , matchHLT_(false)
@@ -44,18 +45,19 @@ namespace phys {
     virtual ~Lepton(){};
     
     // Operations
-    Double_t dxy()             const {return dxy_;}            
-    Double_t dz()              const {return dz_;}             
-    Double_t sip()             const {return sip_;}	      
-    Double_t combRelIso()      const {return combRelIso_;}     
-    Double_t pfChargedHadIso() const {return pfChargedHadIso_;}
-    Double_t pfNeutralHadIso() const {return pfNeutralHadIso_;}
-    Double_t pfPhotonIso()     const {return pfPhotonIso_;}    
-    Double_t pfCombRelIso()    const {return pfCombRelIso_;}   
-    Double_t rho()             const {return rho_;}             
-    Bool_t   isPF()            const {return isPF_;}
-    Bool_t   matchHLT()        const {return matchHLT_;}
-    Bool_t   isGood()          const {return isGood_;}
+    Double_t dxy()                 const {return dxy_;}            
+    Double_t dz()                  const {return dz_;}             
+    Double_t sip()                 const {return sip_;}	      
+    Double_t combRelIso()          const {return combRelIso_;}     
+    Double_t pfChargedHadIso()     const {return pfChargedHadIso_;}
+    Double_t pfNeutralHadIso()     const {return pfNeutralHadIso_;}
+    Double_t pfPhotonIso()         const {return pfPhotonIso_;}    
+    Double_t pfCombRelIso()        const {return pfCombRelIso_;}   
+    Double_t pfCombRelIsoFSRCorr() const {return pfCombRelIsoFSRCorr_;}   
+    Double_t rho()                 const {return rho_;}             
+    Bool_t   isPF()                const {return isPF_;}
+    Bool_t   matchHLT()            const {return matchHLT_;}
+    Bool_t   isGood()              const {return isGood_;}
 
   protected:
     
@@ -69,6 +71,7 @@ namespace phys {
     Double_t pfNeutralHadIso_;
     Double_t pfPhotonIso_;
     Double_t pfCombRelIso_;
+    Double_t pfCombRelIsoFSRCorr_;
     Double_t rho_;
 
     Bool_t isPF_;
