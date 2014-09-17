e1e099ac16e9cfd53dbc017e9a54161ed565ef78
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2014-09-17 11:13:19 +0200

    PDF systematic, first time in

---
+++ Producers/python/PDFSysEvaluation.py
@@ -0,0 +1,147 @@
+#! /usr/bin/env python
+
+import ROOT
+
+from ROOT import gSystem
+gSystem.Load("libFWCoreFWLite")
+from ROOT import AutoLibraryLoader
+AutoLibraryLoader.enable()
+
+import sys
+import math
+import operator
+
+def evaluateMax(a,b,c):   
+    max = 0.
+    if( a > b ):
+        if( a > c ):
+            max = a
+        else :
+            max = c
+    else:
+        if( b > c ):
+            max = b
+        else :
+            max = c
+#    if not max == 0 : print "For the MAX. There are {0:.3f}, {1:.3f}, {2:.3f}. The MAX is {3:.3f}".format(a,b,c,max)
+    return max
+
+
+def evaluateMin(a,b,c):
+    min = 0.
+    if( a < b ):
+        if( a < c ):
+            min = a
+        else :
+            min = c
+    else:
+        if( b < c ):
+            min = b
+        else :
+            min = c
+#    if not min == 0 : print "For the MIN. There are {0:.3f}, {1:.3f}, {2:.3f}. The MIN is {3:.3f}".format(a,b,c,min)
+    return min
+
+def GetPdfResult(fileIn,i):
+
+    nnpdfFlag = False
+    if i==1: h1 = file.Get("pdfSystematics/hPdf_CT10")
+    elif i==2: h1 = file.Get("pdfSystematics/hPdf_MSTW2008nlo68cl")
+    elif i==3: h1 = file.Get("pdfSystematics/hPdf_NNPDF20")
+    
+    Wh1=[]
+    WhSel1=[]
+    Wh2=[]
+    WhSel2=[]
+    
+    Orig= h1.GetBinContent(1,8)
+    TotWhSel= h1.GetBinContent(1,6)
+    TotWh= h1.GetBinContent(1,7)
+
+    npars=0
+    acc_central = TotWhSel/TotWh
+    for j in range(1,55):
+        
+        if h1.GetBinContent(j,1)==0: break
+        npars+=1        
+        Wh1.append(h1.GetBinContent(j,1))
+        WhSel1.append(h1.GetBinContent(j,2))
+        Wh2.append(h1.GetBinContent(j,3))
+        WhSel2.append(h1.GetBinContent(j,4))
+
+    if i==3:
+        nnpdfFlag = True
+
+    wplus=0        
+    wminus=0
+    nplus=0
+    nminus=0
+    
+    for j in range(0,npars):
+        wa = 0.
+	if Wh1[j]>0 : wa = WhSel1[j]/Wh1[j]/acc_central-1.
+        wb = 0.
+        if WhSel2[j]>0 : wb =  WhSel2[j]/Wh2[j]/acc_central-1.
+        if nnpdfFlag :
+            if wa>0.:
+                wplus += wa*wa 
+                nplus+=1
+            else:
+                wminus += wa*wa
+                nminus+=1
+                    
+            if wb>0.: 
+                wplus += wb*wb 
+                nplus+=1
+            else:
+                wminus += wb*wb
+                nminus+=1
+        else:
+            if wa>wb :
+                if wa<0.: wa = 0.
+                if wb>0.: wb = 0.
+                wplus += wa*wa
+                wminus += wb*wb
+            else:
+                if wb<0.: wb = 0.
+                if wa>0.: wa = 0.
+                wplus += wb*wb
+                wminus += wa*wa
+                  
+    if wplus>0:
+        wplus = math.sqrt( wplus )
+    if wminus>0:
+        wminus = math.sqrt( wminus )
+    if nnpdfFlag:
+        if nplus>0: wplus /= math.sqrt( nplus )
+        if nminus>0: wminus /= math.sqrt( nminus )
+        
+    HistoName=h1.GetName()
+    PdfName=HistoName[5:]
+
+    listAcc = []
+    listAcc.append(PdfName)
+    listAcc.append(Orig)
+    listAcc.append(TotWh)
+    listAcc.append(TotWhSel)
+    listAcc.append(acc_central)
+    listAcc.append(wplus)
+    listAcc.append(wminus)
+    return listAcc
+
+file =  ROOT.TFile("../test/PDFStudies.root")
+
+Acc1 = GetPdfResult(file,1)
+Acc2 = GetPdfResult(file,2)
+Acc3 = GetPdfResult(file,3)
+
+print "\n","######################################## \n","######### PDF Systematic Errors ######## \n","######################################## \n \n"
+print "######### Pdf ",Acc1[0],"######### \n Original Events  = ",Acc1[2]," \n Selected Events  = ",Acc1[3] ,"\n Acceptance      = ",Acc1[4]," + ",Acc1[5]/1.645*100," - ",Acc1[6]/1.645*100," % \n"
+print "######### Pdf ",Acc2[0],"######### \n Original Events  = ",Acc2[2]," \n Selected Events  = ",Acc2[3] ,"\n Acceptance      = ",Acc2[4]," + ",Acc2[5]*100," - ",Acc2[6]*100," % \n"
+print "######### Pdf ",Acc3[0],"######### \n Original Events  = ",Acc3[2]," \n Selected Events  = ",Acc2[3] ,"\n Acceptance      = ",Acc3[4]," + ",Acc3[5]*100," - ",Acc3[6]*100," % \n"
+
+CentralValue = 0.5*( evaluateMax(Acc1[4]*(1 + Acc1[5]/1.645) ,Acc2[4]*( 1 + Acc2[5]) , Acc3[4]*(1+Acc3[5]) ) + evaluateMin(Acc1[4]*(1 - Acc1[5]/1.645),Acc2[4]*(1 - Acc2[5]) , Acc3[4]*( 1- Acc3[5])))
+
+Err = 0.5*( evaluateMax(Acc1[4]*(1 + Acc1[5]/1.645) ,Acc2[4]*( 1 + Acc2[5]) , Acc3[4]*(1+Acc3[5]) ) - evaluateMin(Acc1[4]*(1 - Acc1[5]/1.645),Acc2[4]*(1 - Acc2[5]) , Acc3[4]*( 1- Acc3[5])))
+
+print "Envelope Value = ",CentralValue," +/- ",Err/CentralValue*100," % \n "
---
+++ Producers/test/PdfSystematicsAnalyzerStep2.py
@@ -0,0 +1,109 @@
+1### NOTE: This is prepared to run on the newest PDFs with LHAPDF >=3.8.4
+### so it requires local installation of LHAPDF libraries in order to run 
+### out of the box. Otherwise, substitute the PDF sets by older sets
+
+import FWCore.ParameterSet.Config as cms
+
+
+### ----------------------------------------------------------------------
+### Based on ZZ->4l strategy.
+###----------------------------------------------------------------------
+LEPTON_SETUP = 2012
+JET_SETUP = 2012
+#PD = ""
+#MCFILTER = ""
+ELECORRTYPE = "Paper" # "None", "Moriond", or "Paper"
+ELEREGRESSION = "Paper" # "None", "Moriond", "PaperNoComb", or "Paper"
+APPLYMUCORR = True
+#MCFILTER = "signaldefinition"
+SIGNALDEFINITION = int('1',2) # -1 means get everything, 1 means the request of having a ZZ pair with the mass in the choosedn windows. For other topology see the README under VVXAnalysis/Commons.
+try:
+    IsMC
+except NameError:
+    IsMC = True
+
+try:
+    LEPTON_SETUP
+except NameError:
+    LEPTON_SETUP = 2012 # define the set of effective areas, rho corrections, etc.
+
+try:
+    JET_SETUP
+except NameError:
+    JET_SETUP = 2012 # define the MVA for the jet PU Id
+
+try:
+    PD
+except NameError:
+    PD = "" # "" for MC, "DoubleEle", "DoubleMu", or "MuEG" for data
+
+try:
+    MCFILTER
+except NameError:
+    MCFILTER = ""
+
+try:
+    XSEC
+except NameError:
+    XSEC = -1
+
+
+import os
+PyFilePath = os.environ['CMSSW_BASE'] + "/src/VVXAnalysis/Producers/test/"
+execfile(PyFilePath + "analyzer_ZZjj.py")
+
+# Max events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+infile1='/store/cmst3/user/cmgtools/CMG/ZZTo4mu_8TeV-powheg-pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_100_1_UR6.root'
+infile2= '/store/cmst3/user/cmgtools/CMG/ZZTo2e2mu_8TeV-powheg-pythia6/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_100_1_irQ.root'
+infile3='/store/cmst3/group/cmgtools/CMG/ZZZNoGstarJets_8TeV-madgraph/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM/PAT_CMG_V5_15_0/cmgTuple_10_1_UV1.root'
+# Input files (on disk)
+
+weightfile='file:PdfWeight.root'
+
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring(weightfile),
+secondaryFileNames = cms.untracked.vstring(infile2)
+)
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
+
+process.TFileService=cms.Service('TFileService',
+fileName=cms.string('PDFStudies.root')
+
+)
+
+process.zzGenCategory = cms.EDFilter("ZZGenFilterCategory",
+Topology = cms.int32(1), # -1 means get everything
+src = cms.InputTag("genParticlesPruned")
+)
+
+process.signalFilters += process.zzGenCategory
+
+
+# Collect uncertainties for rate and acceptance
+process.pdfSystematics = cms.EDFilter("PdfSystematicsAnalyzerZZ",
+      SelectorPath = cms.untracked.string('preselection'),
+      PdfWeightTags = cms.untracked.VInputTag(
+              "pdfWeights:CT10"
+           , "pdfWeights:MSTW2008nlo68cl"
+            , "pdfWeights:NNPDF20"
+      )
+)
+
+
+process.thisIsTheEnd = cms.EndPath( process.zzGenCategory*process.pdfSystematics)
+#process.end = cms.EndPath(process.pdfSystematics)
+
+
+# Printouts
+process.MessageLogger = cms.Service("MessageLogger",
+      cout = cms.untracked.PSet(
+            default = cms.untracked.PSet(limit = cms.untracked.int32(100)),
+            threshold = cms.untracked.string('INFO')
+      ),
+      destinations = cms.untracked.vstring('cout')
+)
