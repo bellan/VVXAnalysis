97ba96a54162c7c916d4373695dc96dd9803bc79
Linda <linda.finco@cern.ch>
2014-12-10 11:26:25 +0100

    update

--- TreeAnalysis/src/ZZSAnalyzer.cc
+++ TreeAnalysis/src/ZZSAnalyzer.cc
@@ -1,217 +1,207 @@
 #include "VVXAnalysis/TreeAnalysis/interface/ZZSAnalyzer.h"
 #include "VVXAnalysis/Commons/interface/SignalDefinitions.h"
-
+#include "VVXAnalysis/Commons/interface/Comparators.h"
+//#include "VVXAnalysis/Commons/interface/Colours.h"
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
 
+#include <TVector.h>
+//#include "TPair.h"
+#include <TChain.h>
+#include <TGraph.h>
+#include <TVectorD.h>
+#include <vector>
+#include <utility>
+#include <iostream>
+#include <algorithm>
+
+
 using namespace phys;
-//using namespace zz;
+using namespace std;
+//using namespace colour;
+
+Int_t ZZSAnalyzer::cut() {
+  
+  if(!ZZ->passTrigger())          theHistograms.fill("CutCheck",5,0,5,0);
+  if(! 60 < ZZ->first().mass())   theHistograms.fill("CutCheck",5,0,5,1);
+  if(! ZZ->first().mass() < 120)  theHistograms.fill("CutCheck",5,0,5,2);
+  if(! 60 < ZZ->second().mass())  theHistograms.fill("CutCheck",5,0,5,3);
+  if(! ZZ->second().mass() < 120) theHistograms.fill("CutCheck",5,0,5,4);
+
+  if(ZZ->passTrigger())
+    return 1;
+  
+  theHistograms.fill("mZ1_nonPassing", "Invariant mass of Z_{1} for non passing events",  50, 0, 200, ZZ->first().mass() , theWeight); 
+  theHistograms.fill("mZ2_nonPassing", "Invariant mass of Z_{2} for non passing events",  50, 0, 200, ZZ->second().mass(), theWeight); 
+
+  return -1;
+}
+
+void ZZSAnalyzer::ZZplots(int id){
+
+   if(ZZ->id() != id && id != -1) return; // -1 here means generic 4l final state
+
+  std::string decay  = "4l";
+  std::string decay1 = "l";
+  std::string decay2 = "l";
+
+  if      (id == 52) {decay = "4m"  ; decay1 = "m"; decay1 = "m";}
+  else if (id == 48) {decay = "2e2m"; decay1 = "e"; decay2 = "m";}
+  else if (id == 44) {decay = "4e"  ; decay1 = "e"; decay2 = "e";}
+
+  theHistograms.fill(std::string("mZ1To2")+decay1, std::string("Invariant mass of Z_{1}#rightarrow 2")+decay1,  15, 60,  120, ZZ->first().mass() , theWeight); 
+  theHistograms.fill(std::string("mZ2To2")+decay2, std::string("Invariant mass of Z_{2}#rightarrow 2")+decay2,  15, 60,  120, ZZ->second().mass(), theWeight); 
+  theHistograms.fill(std::string("mZZTo") +decay , std::string("Invariant mass of ZZ#rightarrow ")    +decay ,  40,  0, 1000, ZZ->mass()         , theWeight);
+ 
+ theHistograms.fill(std::string("nJets_ZZTo") +decay      , std::string("Number of jets (|#eta|<4.7 and p_T > 30 GeV) ZZ#rightarrow ") +decay        , 10, 0, 10, jets->size(), theWeight); 
+  theHistograms.fill(std::string("nCentralJets_ZZTo") +decay ,std::string("Number of jets (|#eta|<2.5 and p_T > 30 GeV) ZZ#rightarrow ") +decay, 10, 0, 10, centralJets->size(), theWeight);
+
+ theHistograms.fill(std::string("met_ZZTo") +decay      , std::string("Missing transverse energy (|#eta|<4.7 and p_T > 30 GeV) ZZ#rightarrow ") +decay        , 200, 0, 800, met->pt(), theWeight); 
+
+ if(jets->size() >= 2){
+   theHistograms.fill(std::string("DeltaYJJ_ZZTo") +decay, std::string("#Delta Y(j,j) between the two most energetic jets ZZ#rightarrow ") +decay,  50, 0, 8, fabs(jets->at(0).rapidity() - jets->at(1).rapidity()), theWeight); 
+   theHistograms.fill(std::string("DeltaEtaJJ_ZZTo") +decay, std::string("#Delta #eta(j,j) between the two most energetic jets ZZ#rightarrow ") +decay,  50, 0, 8, fabs(jets->at(0).eta() - jets->at(1).eta()), theWeight); 
+  }  
+
+  if(centralJets->size() >= 2){
+    theHistograms.fill(std::string("DeltaYJJcentral_ZZTo") +decay, std::string("#Delta Y(j,j) between the two most energetic central jets ZZ#rightarrow ") +decay ,  50, 0, 8, fabs(centralJets->at(0).rapidity() - centralJets->at(1).rapidity()), theWeight);
+    theHistograms.fill(std::string("DeltaEtaJJcentral_ZZTo") +decay,  std::string("#Delta #eta(j,j) between the two most energetic central jets ZZ#rightarrow ") +decay,  50, 0, 8, fabs(centralJets->at(0).eta() - centralJets->at(1).eta()), theWeight);  
+ 		       theHistograms.fill(std::string("mJJ")+decay, "m_{jj}",  100, 0, 3000, (centralJets->at(0).p4() + centralJets->at(1).p4()).M(), theWeight); 
+  }
+
+}
+
+int e =0;
+int genCat = 0;
+int top_0 = 0;
+int top_2 = 0;
+int top_3 = 0;
+int top_4 = 0;
+int top_5 = 0;
+int sig = 0;
+// int q = 0;
+// int g = 0;
+// int q_30 = 0;
+// int g_30 = 0;
+int inDeltaEta_4l = 0;
+int inDeltaY_4l = 0;
+int inDeltaEta_ZZ = 0;
+int inDeltaY_ZZ = 0;
+// TVectorD DeltaEta_c1;
+// TVectorD mJJ_c1;
+int c1 =0;
+// TVectorD DeltaEta_c2;
+// TVectorD mJJ_c2;
+int c2 =0;
+// int n = tree()->GetEntries();
+// float DeltaEta[n];
+// float mJJ[n];
+//const AnalysisConfiguration& configuration;
 
 void ZZSAnalyzer::analyze(){
   
+  e++;
+  
+  if(genCategory>0){
+    
+    // Particle gjet_1;
+    // Jet jet_1;
+    
+    // std::vector< std::pair<const Particle , const Jet > > JetcomparatorVector;
+    std::vector< std::pair<const Particle* , const Jet*> > GenRecoJetMatchedVector;
+    std::vector< std::pair<const Particle* , const Particle*> > RecoJetGenParticleMatchedVector;
 
-   foreach(const Boson<Lepton> zmm, *Zmm)
-     theHistograms.fill("ZmmMass",100,65,115,zmm.p4().M(),theWeight);
-  
-  foreach(const Boson<Jet> w, *Wjj)
-    if(w.daughter(0).pt() > 40 && w.daughter(1).pt()>40)    
-      theHistograms.fill("WjjMass",80,40,120,w.p4().M(),theWeight);
-  
-  foreach(const Boson<Electron> zee, *Zee)
-    theHistograms.fill("ZeeMass",100,65,115,zee.p4().M(),theWeight);
-  
-  foreach(const Jet jet, *jets)
-    theHistograms.fill("jet_Eta",100,-6,6, jet.eta(),theWeight);
+    foreach(const Jet &jet, *jets){
+      double bestDeltaR = 1e30;
+      const Particle* matchedJet =0; 
+      foreach(const Particle &gjet, *pgenJets){
+	double myDeltaR=physmath::deltaR(jet, gjet);
+	if (myDeltaR<bestDeltaR) {
+	  matchedJet=&gjet;
+	  bestDeltaR=myDeltaR;
+	}
+      }
+      GenRecoJetMatchedVector.push_back(make_pair(matchedJet,&jet));
+    }
 
-  
-  
-  std::tuple<bool,Boson<Lepton>, Boson<Lepton> > ZZcand = zz::zz4l(*Zmm,*Zee);
-  theHistograms.fill("ZZcand",5,-2,2,std::get<0>(ZZcand),theWeight);
-  
-  if(std::get<0>(ZZcand)){//da mettere poi la condizione sul best candidate ZZ
-   
-    const int size = jets->size();
 
-    double jet_pt[size];
-    double jet_mass = 0;
-    //    double jet_mass_check = 0;
-    double jet_deltaeta =0;
-    double jet_deltay =0;
+    for(std::vector<std::pair<const Particle*, const Jet*> >::iterator it = GenRecoJetMatchedVector.begin(); it != GenRecoJetMatchedVector.end(); it++){
+      // std::cout << "(" << it->first->pt() << ", " << it->second->pt() << ")" << std::endl;
 
-    TLorentzVector jet_p4[size];
-    TLorentzVector lead_jet1;
-    TLorentzVector lead_jet2;
+      std::cout << "DeltaR = " << physmath::deltaR(it->first, it->second) << std::endl;
+ 
+     //  double bestDeltaR = 1e30;
+     //  const Particle* matchedParticle =0; 
+     //  foreach(const Particle &gparticle, *pgenJets){
+     // 	double myDeltaR=physmath::deltaR(it->first, gparticle);
+     // 	if (myDeltaR<bestDeltaR) {
+     // 	  matchedParticle=&gparticle;
+     // 	  bestDeltaR=myDeltaR;
+     // 	}
+     //  }
+     // RecoJetGenParticleMatchedVector.push_back(make_pair(matchedParticle,it->second));
 
-    int i= 0;
-    int j =0;
+    }
+
+    // int q = 0;
+    // int g = 0;
+    
+    // for(std::vector<std::pair<const Particle*, const Particle*> >::iterator j = RecoJetGenParticleMatchedVector.begin(); j != RecoJetGenParticleMatchedVector.end(); j++){
+      
+    //   if(abs(j->first->id()) == 1 || abs(j->first->id()) == 2 )	q++;
+      
+    //   else if(abs(gen.id()) == 21)  g++;
+      
+    // }
+    
+    //  std::cout << "quark-jet = " << q  << "gluon-jet = " << g << std::endl;
+
      
-    theHistograms.fill("JetMult",10,0,10,size,theWeight);
-  
-  
-
-  //#events vs M_jj - DeltaEta - DeltaY (eta<4.7, pt>30)
-      foreach(const Jet jet, *jets){
       
-      jet_pt[i] = jet.pt();
-      jet_p4[i] = jet.p4();
-
-      //std::cout << jet_pt[i] << std::endl;
-
-      //std::cout  << " " << jet_p4[i][0]  << " " << jet_p4[i][1] << " " << jet_p4[i][2] << " " << jet_p4[i][3]<< " " << jets->size()<< " " << i << " " << jet_pt[i] << " " << jet.eta() << std::endl;  
+    if(topology.test(2)) {
       
-      i++;
-
-    
-    }
-    
+      top_2++; 
+      theHistograms.fill("ngenJets_top2_sigVVS"       , "Number of generated jets (|#eta|<4.7 and p_T > 30 GeV) - VVS signal condictions required"        , 10, 0, 10, genJets->size(), theWeight);
       
-    std::sort(jet_pt,jet_pt + size);  
-    
-    if(i!=0 && jet_pt[size-1]!=0 && jet_pt[size-2]!=0){
-      
-      //std::cout << "lead_jet1_pt= " << jet_pt[size-1] <<  " lead_jet2_pt= " << jet_pt[size-2] << " " << std::endl;
-      
-      for(j = 0; j < size; j++){
-	
-	if(jet_p4[j].Pt() == jet_pt[size-1]) lead_jet1 = jet_p4[j];
-	else if(jet_p4[j].Pt() == jet_pt[size-2]) lead_jet2 = jet_p4[j];
-	else continue;
-		
-      }
-
-      jet_mass = (lead_jet1+lead_jet2).M();
-      jet_deltaeta = TMath::Abs(lead_jet1.PseudoRapidity() - lead_jet2.PseudoRapidity());
-      jet_deltay = TMath::Abs(lead_jet1.Rapidity() - lead_jet2.Rapidity());
-
-      //jet_mass_check = TMath::Sqrt(lead_jet1.M()*lead_jet1.M()+lead_jet2.M()*lead_jet2.M()+2*(lead_jet1[3]*lead_jet2[3] - lead_jet1[0]*lead_jet2[0] - lead_jet1[1]*lead_jet2[1] - lead_jet1[2]*lead_jet2[2]));
-      
-      //std::cout<< "M_jj = " << jet_mass << " M1= " << lead_jet1.M() <<  " M2= " << lead_jet2.M() << " " << jet_deltaeta << std::endl; 
-      
-      
-      if(jet_pt[size-2]>30){ 
-	
-	theHistograms.fill("M_jj",500,0,1000,jet_mass,theWeight);
-	theHistograms.fill("DeltaEta_jj",50,0,5,jet_deltaeta,theWeight);
-	theHistograms.fill("DeltaY_jj",50,0,5,jet_deltay,theWeight);
-     
-      }
     }
 
+    if(topology.test(3)) top_3++; 
+    if(topology.test(4)) top_4++; 
+    if(topology.test(5)) top_5++; 
     
-    //#events vs M_jj  - DeltaEta - DeltaY (eta<2.5, pt>30)
-    const int csize = centralJets->size();
-
-    // std::cout << "size: " << size << " csize: " << csize << std::endl;
-
-    double cjet_pt[csize];
-    double cjet_mass = 0;
-    //double cjet_mass_check = 0;
-    double cjet_deltaeta =0;
-    double cjet_deltay =0;
-
-    TLorentzVector cjet_p4[csize];
-    TLorentzVector lead_cjet1;
-    TLorentzVector lead_cjet2;
-
-    int i_c= 0;
-    int j_c =0;
-    // int k_c =0;
-
-    theHistograms.fill("JetMultcentral",10,0,10,csize,theWeight);
-
-    foreach(const Jet cjet, *centralJets){
-       
-      cjet_pt[i_c] = cjet.pt();
-      cjet_p4[i_c] = cjet.p4();
+    if(topology.test(2)==1 && topology.test(3)==0 && topology.test(4)==0 && topology.test(5)==0){
       
-      //std::cout  << " " << cjet_p4[i_c][0]  << " " << cjet_p4[i_c][1] << " " << cjet_p4[i_c][2] << " " << cjet_p4[i_c][3]<< " " << csize<< " " << i_c << " " << cjet_pt[i_c] << " " << cjet.eta() << std::endl;  
+      sig ++;   
       
-      i_c++;
+      theHistograms.fill("ngenJets_sigVVS"       , "Number of generated jets (|#eta|<4.7 and p_T > 30 GeV) - VVS signal condictions required"        , 10, 0, 10, genJets->size(), theWeight);
+      theHistograms.fill("ngenCentralJets_sigVVS", "Number of generated central jets (|#eta|<2.5 and p_T > 30 GeV) - VVS signal condictions required", 10, 0, 10,  centralGenJets->size(), theWeight);
+      //genJets pt>30, |eta|< 4.7
+      //  foreach(const Particle genJet, *genJets){
+      // theHistograms.fill("jet_girth", "jet girth",  100, -100, 100, genJets->at(0).girth(), theWeight);
+	// }
+
     }
-    
-    std::sort(cjet_pt,cjet_pt + csize);  
-    
-    if(i_c!=0 && cjet_pt[csize-1]!=0 && cjet_pt[csize-2]!=0){
-      
-      //std::cout << "lead_cjet1_pt= " << cjet_pt[csize-1] <<  " lead_cjet2_pt= " << cjet_pt[csize-2] << " " << std::endl;
-      
-      for(j_c = 0; j_c < csize; j_c++){
-	
-	if(cjet_p4[j_c].Pt() == cjet_pt[csize-1]) lead_cjet1 = cjet_p4[j_c];
-	else if(cjet_p4[j_c].Pt() == cjet_pt[csize-2]) lead_cjet2 = cjet_p4[j_c];
-	else continue;
-	
-      }
-      
-      cjet_mass = (lead_cjet1+lead_cjet2).M();
-      cjet_deltaeta = TMath::Abs(lead_cjet1.PseudoRapidity() - lead_cjet2.PseudoRapidity());
-      cjet_deltay = TMath::Abs(lead_cjet1.Rapidity() - lead_cjet2.Rapidity());
-      
-      //cjet_mass_check = TMath::Sqrt(lead_cjet1.M()*lead_cjet1.M()+lead_cjet2.M()*lead_cjet2.M()+2*(lead_cjet1[3]*lead_cjet2[3] - lead_cjet1[0]*lead_cjet2[0] - lead_cjet1[1]*lead_cjet2[1] - lead_cjet1[2]*lead_cjet2[2]));
-      
-      //std::cout<< "M_jj = " << cjet_mass << " M1= " << lead_cjet1.M() <<  " M2= " << lead_cjet2.M() << " " << cjet_deltaeta << std::endl; 
-      
-      //#events vs M_jj (eta<4.7, pt>30)
-      if(cjet_pt[csize-2]>30) {
 
-	theHistograms.fill("M_jj_central",500,0,1000,cjet_mass,theWeight);
-	theHistograms.fill("DeltaEta_jj_central",50,0,5,cjet_deltaeta,theWeight);
-	theHistograms.fill("DeltaY_jj_central",50,0,5,cjet_deltay,theWeight);
-      }
+  }
+
+
+  if(ZZ->passTrigger()){//baseline selection required? FIXME
+    if(topology.test(2)==1 && topology.test(3)==0 && topology.test(4)==0 && topology.test(5)==0){
+     	if(jets->size() >= 2){
+
+      
+	} 
+     }
+    else{
     }
   }
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // foreach(const DiBoson< Boson<Lepton>, Boson<Lepton>> zz4m, *ZZ4m)
-//     theHistograms.fill("ZZ4m",100,0,300,(zz4m.first().p4()+zz4m.second().p4()).M())
-
-
-//   }
-
-//   if(!std::get<0>(ZZ)) theHistograms.fill("ZZCand",5,-2,2,0);
-//   else theHistograms.fill("ZZCand",5,-2,2,1);
-  
-//   if(!std::get<0>(ZZ)) std::cout << "-1" ;
-//   else std::cout << "0" ;
-
-  //if(!std::get<0>(ZZ)) std::cout << "-1" ;
-
-  // if(!std::get<0>(ZZ)) std::cout << "-1" <
-  // < std::endl;//theHistograms.fill("ZZCand",,150,300,zz.p4().M());
-  
-
-    // std::cout << "-1" << std::endl;
-
- //zz, *ZZ)
-    // theHistograms.fill("ZZMass",100,150,300,zz.p4().M());
-
-  //const DiBoson< Boson<Lepton>, Boson<Lepton>> zz4m = *ZZ4m;
-
-//   //foreach(const DiBoson< Boson<Lepton>, Boson<Lepton>> zz4m, *ZZ4m)
-// foreach(const DiBoson zz4m, *ZZ4m)
-//   //  if(zz4m.passFullSel() == true)    
-//   theHistograms.fill("ZZ4m",100,0,300,(zz4m.first().p4()+zz4m.second().p4()).M());
-  
 }
+
+ 
+
+  
+
+
+
+  
+
