7e33d3aab541072a09accd5eabfa6c537cf9b061
Riccardo <riccardo.bellan@cern.ch>
2014-12-16 16:30:14 +0100

    keep all status 3 particles

--- Producers/plugins/TreePlanter.cc
+++ Producers/plugins/TreePlanter.cc
@@ -337,7 +337,7 @@ bool TreePlanter::fillEventInfo(const edm::Event& event){
     event.getByLabel(theGenCollectionLabel,  genParticles);
     
     for (edm::View<reco::Candidate>::const_iterator p = genParticles->begin(); p != genParticles->end(); ++p) 
-      if (p->status() ==3 && std::distance(genParticles->begin(),p) > 5)
+      if (p->status() == 3)
 	genParticles_.push_back(phys::Particle(p->p4(), phys::Particle::computeCharge(p->pdgId()), p->pdgId()));
          
   
