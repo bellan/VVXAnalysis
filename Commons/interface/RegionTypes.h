b7ba2689124fd415feb1faaba0c3314b930f7d5f
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2015-11-10 17:30:53 +0100

    Add SR_ZZOnShell

--- Commons/interface/RegionTypes.h
+++ Commons/interface/RegionTypes.h
@@ -4,8 +4,8 @@
 #include <string>
 
 namespace phys{
-  enum RegionTypes {SR, CR, CR2P2F, CR3P1F, CR2P2F_HZZ, CR3P1F_HZZ, MC};
-  
+  enum RegionTypes {SR, CR, CR2P2F, CR3P1F, CR2P2F_HZZ, CR3P1F_HZZ, MC,SR_ZZOnShell};
+ 
   RegionTypes regionType(const std::string& input);
   std::string regionType(RegionTypes input);
 
