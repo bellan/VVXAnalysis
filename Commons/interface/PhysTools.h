9c38c515359c8fbaade513685ad76150dfdc4940
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2015-12-12 16:30:18 +0100

    Add convert method with GenStatusFlag

--- Commons/interface/PhysTools.h
+++ Commons/interface/PhysTools.h
@@ -3,8 +3,12 @@
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "VVXAnalysis/DataFormats/interface/Particle.h"
+#include <DataFormats/HepMCCandidate/interface/GenParticle.h>
+#include "VVXAnalysis/DataFormats/interface/GenStatusBit.h"
 
 namespace phys{
   phys::Particle convert(const reco::Candidate &rc);
+  phys::Particle convert(const reco::Candidate &rc,std::bitset<15> flags);
+
 }
 #endif
--- Commons/src/PhysicsTools.cc
+++ Commons/src/PhysicsTools.cc
@@ -1,9 +1,17 @@
 #include "VVXAnalysis/Commons/interface/PhysTools.h"
 
-phys::Particle phys::convert(const reco::Candidate &rc){
+phys::Particle phys::convert(const reco::Candidate &rc,std::bitset<15> flags){
   
-  phys::Particle p(rc.p4(),phys::Particle::computeCharge(rc.pdgId()),rc.pdgId());
+  phys::Particle p(rc.p4(),phys::Particle::computeCharge(rc.pdgId()),rc.pdgId(),flags);
+
   p.setMotherId(rc.numberOfMothers() > 0 ? rc.mother()->pdgId() : -9999);
 
   return p;
 }
+
+ phys::Particle phys::convert(const reco::Candidate &rc){
+  
+   std::bitset<15> flags (-99);
+   return phys::convert(rc,flags);
+  }
+
