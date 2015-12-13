cd49da21f9f82d6efa50d7aa971007bb21771376
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2015-12-13 20:48:15 +0100

    proper event numbers for amcatnlo

--- TreeAnalysis/interface/MCInfo.h
+++ TreeAnalysis/interface/MCInfo.h
@@ -20,8 +20,8 @@ class MCInfo {
   double externalCrossSection() const {return externalCrossSection_;}
   double crossSection()         const {return *crossSection_;}
   double sampleWeight()         const {return sampleWeight_;}
-  //double mcProcWeight()         const {return mcprocweight_*genEvents_/summcprocweight_;}
-  double mcProcWeight()         const {return 1.;}
+  double mcProcWeight()         const {return mcprocweight_*genEvents_/summcprocweight_;}
+  //double mcProcWeight()         const {return 1.;}
   double mcProcWeightNormalization() const {return genEvents_/summcprocweight_;}
   double mcProcWeightUnormalized()   const {return mcprocweight_;}
   double summcprocweight()      const {return summcprocweight_;}     
