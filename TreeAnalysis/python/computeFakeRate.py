85cf38255d9f2301791e2aea84f9d5e5d9a92ec7
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2015-12-13 21:05:43 +0100

    Cosmetic

--- TreeAnalysis/python/computeFakeRate.py
+++ TreeAnalysis/python/computeFakeRate.py
@@ -25,7 +25,7 @@ def GetGrEff(hpass, htotal):
 
         if hpass.GetBinContent(bin)<0: hpass.SetBinContent(bin,0.)
         if htotal.GetBinContent(bin)<0: htotal.SetBinContent(bin,0.)
-        #print  "bin",bin,"pass",hpass.GetBinContent(bin),"total", htotal.GetBinContent(bin)
+    
     hpass_= copy.deepcopy(hpass)
     hpass_.Divide(htotal)
     for bin in range(1,Nbin+1):  
@@ -33,7 +33,7 @@ def GetGrEff(hpass, htotal):
     grEff = ROOT.TGraphAsymmErrors(hpass,htotal,"e0")
 
     for bin in range(1,Nbin+1):   
-        #print "bin",bin,"pass",hpass.GetBinContent(bin),"total",htotal.GetBinContent(bin),"\nVal",hpass_.GetBinContent(bin) 
+        #print "bin ",bin," passed events ",hpass.GetBinContent(bin),"total events ",htotal.GetBinContent(bin),"\nRatio ",hpass_.GetBinContent(bin) 
 
         XVal=ROOT.Double(0.)
         YVal=ROOT.Double(0.)
@@ -47,9 +47,6 @@ def GetGrEff(hpass, htotal):
             grEff.SetPointEYlow(bin-1, GetError(htotal.GetBinContent(bin), hpass.GetBinContent(bin),0))
 
         grEff.GetPoint(bin-1,XVal,YVal)
-        #print "Gr",XVal,YVal,grEff.GetErrorY(bin-1),grEff.GetErrorYhigh(bin-1)
-        #print "\n"
-
         grEff.SetMarkerStyle(20)
         grEff.SetMarkerSize(.3)    
     return grEff
@@ -61,7 +58,6 @@ def write(particle,region,outname,fout):
     f = ROOT.TFile("results/FakeRateAnalyzer_MC/data.root")
     hn = f.Get("FakeRate_num_"+particle+"_"+region+"_pt")
     hd = f.Get("FakeRate_denom_"+particle+"_"+region+"_pt")
-
     
     fWZ = ROOT.TFile("results/FakeRateAnalyzer_MC/WZ.root")
     hnWZ = fWZ.Get("FakeRate_num_"+particle+"_"+region+"_pt")
@@ -69,13 +65,6 @@ def write(particle,region,outname,fout):
 
     hFake =  copy.deepcopy(hn)
     hFake.Divide(hd)
-
-    #Err=ROOT.Double(0.)
-    TotalFakeRate = hn.Integral()/hd.Integral()
-    TotalErrUp = GetError( hd.Integral(), hn.Integral(), True)
-    TotalErrDown = GetError( hd.Integral(), hn.Integral(), False)
-
-    print "Total Fake Rate",TotalFakeRate,"+",TotalErrUp,"(",TotalErrUp/TotalFakeRate,"%)","-",TotalErrDown,"(",TotalErrDown/TotalFakeRate,"%)"
 
     hFake.SetName("hFakeRate_"+outname)   
     hFake.SetTitle("hFakeRate_"+outname)
