f475236473301ce64339f4ab2a12204be21fc99e
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2016-02-02 17:15:08 +0100

    Add ZZFull regions and MC_HZZ region

--- Commons/interface/RegionTypes.h
+++ Commons/interface/RegionTypes.h
@@ -4,7 +4,7 @@
 #include <string>
 
 namespace phys{
-  enum RegionTypes {SR, CR, CR2P2F, CR3P1F, SR_HZZ, CR_HZZ, CR2P2F_HZZ, CR3P1F_HZZ, MC};
+  enum RegionTypes {SR, CR, CR2P2F, CR3P1F, SR_HZZ, CR_HZZ, CR2P2F_HZZ, CR3P1F_HZZ, SR_ZZFull, CR_ZZFull, CR2P2F_ZZFull, CR3P1F_ZZFull, MC,MC_ZZFull};
  
   RegionTypes regionType(const std::string& input);
   std::string regionType(RegionTypes input);
