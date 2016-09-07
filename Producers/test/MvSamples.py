7d665ab0857a9fffca16d7bcdc75d8dba60894a8
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2016-09-07 11:53:58 +0200

    update samples

--- Producers/test/MvSamples.py
+++ Producers/test/MvSamples.py
@@ -10,21 +10,18 @@ Targetfile= sys.argv[1]
 
 What= sys.argv[2]
 
-datalist = ["DoubleEG2016B","DoubleMu2016B","MuonEG2016B","SingleEle2016B","DoubleEG2016C","DoubleMu2016C","MuonEG2016C","SingleEle2016C","DoubleEG2015C","DoubleMu2015C","MuonEG2015C","SingleEle2015C","DoubleEG2015D","DoubleMu2015D","MuonEG2015D","SingleEle2015D","DoubleEG2016B","DoubleMu2016B","MuonEG2016B","SingleEle2016B"]
+datalist = ["DoubleEG2016B","DoubleMu2016B","MuonEG2016B","SingleEle2016B","DoubleEG2016C","DoubleMu2016C","MuonEG2016C","SingleEle2016C","DoubleEG2016D","DoubleMu2016D","MuonEG2016D","SingleEle2016D","DoubleEG2016E","DoubleMu2016E","MuonEG2016E","SingleEle2016E","DoubleEG2016F","DoubleMu2016F","MuonEG2016F","SingleEle2016F"]
 
-#MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","DYJetsToLL_M50","TTJets","TTTo2L2Nu","WZ","ZGTo2LG","ZH125","ttH125","VBFH125","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","WWZ","TTGJets","TTWJets","WWW","ggH126"]
+datalist_2015 = ["DoubleEG2015C","DoubleMu2015C","MuonEG2015C","SingleEle2015C","DoubleEG2015D","DoubleMu2015D","MuonEG2015D","SingleEle2015D"]
 
-MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","ggH126"]
+MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","DYJetsToLL_M50","TTJets","TTTo2L2Nu","WZ","ZGTo2LG","ZH125","ttH125","VBFH125","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","WWZ","TTGJets","TTWJets","WWW","ggH126"]
+
+#MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","ggH126"]
 
 
 AddbashCommand="hadd -f "+Targetfile+"/Data.root "
 bashCommand="mv "
 
-#f = []
-#for (dirpath, dirnames, filenames) in walk('.'):
- #   f.extend(dirnames)
-    #print dirnames
-  #  break
 if What=="All" or What=="Data":
     for Dir in datalist:
         AddbashCommand=AddbashCommand+" "+Targetfile+"/"+Dir+".root"
