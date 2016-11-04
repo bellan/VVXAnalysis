5f102aa4732916491273976383627740f4399c00
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2016-11-04 11:09:03 +0100

    add likelihood sx inclusive script

---
+++ TreeAnalysis/python/LikelihoodXS.py
@@ -0,0 +1,126 @@
+#! /usr/bin/env python
+
+import ROOT
+import CrossInfo
+from CrossInfo import* 
+from ROOT import RooStats
+import copy
+
+## to time the macro
+
+def LikeCross(wspace):
+           
+    #  make Poisson model * Gaussian constraint
+    wspace.factory("prod:s_4m(mu[1.,0.,1000],xs_4m,eff_4m,Lumi)")  
+    wspace.factory("prod:s_2e2m(mu[1.,0.,1000],xs_2e2m,eff_2e2m,Lumi)")  
+    wspace.factory("prod:s_4e(mu[1.,0.,1000],xs_4e,eff_4e,Lumi)")  
+        
+    wspace.factory("sum:nexp_4m(s_4m,bkg_4m,irrBkg_4m)")
+    wspace.factory("sum:nexp_2e2m(s_2e2m,bkg_2e2m, irrBkg_2e2m)")
+    wspace.factory("sum:nexp_4e(s_4e,bkg_4e,irrBkg_4e)")
+ 
+    wspace.Print()
+    
+    #Poisson of (n | s+b)
+    wspace.factory("Poisson:pdf_4m(nobs_4m,nexp_4m)")
+    wspace.factory("Poisson:pdf_2e2m(nobs_2e2m,nexp_2e2m)")
+    wspace.factory("Poisson:pdf_4e(nobs_4e,nexp_4e)")
+    
+    wspace.factory("Gaussian:constraint_4m(bkg0_4m,bkg_4m,sigmaBkg_4m)")
+    wspace.factory("Gaussian:constraint_2e2m(bkg0_2e2m,bkg_2e2m,sigmaBkg_2e2m)")
+    wspace.factory("Gaussian:constraint_4e(bkg0_4e,bkg_4e,sigmaBkg_4e)")
+
+    wspace.factory("Gaussian:constraintIrrBkg_4m(irrBkg0_4m,irrBkg_4m,sigmaIrrBkg_4m)")
+    wspace.factory("Gaussian:constraintIrrBkg_2e2m(irrBkg0_2e2m,irrBkg_2e2m,sigmaIrrBkg_2e2m)")
+    wspace.factory("Gaussian:constraintIrrBkg_4e(irrBkg0_4e,irrBkg_4e,sigmaIrrBkg_4e)")
+    
+    wspace.factory("Gaussian:constraintEff_4m(eff0_4m,eff_4m,sigmaEff_4m)")
+    wspace.factory("Gaussian:constraintEff_2e2m(eff0_2e2m,eff_2e2m,sigmaEff_2e2m)")
+    wspace.factory("Gaussian:constraintEff_4e(eff0_4e,eff_4e,sigmaEff_4e)")
+    
+    wspace.factory("PROD:model_4m(pdf_4m,constraint_4m,constraintIrrBkg_4m,constraintEff_4m)")
+    wspace.factory("PROD:model_2e2m(pdf_2e2m,constraint_2e2m,constraintIrrBkg_2e2m,constraintEff_2e2m)")
+    wspace.factory("PROD:model_4e(pdf_4e,constraint_4e,constraintIrrBkg_4e,constraintEff_4e)")
+    
+    wspace.factory("PROD:model(model_4m,model_2e2m,model_4e)")
+            
+    BR_4m   = BRmu*BRmu
+    BR_2e2m = 2*BRmu*BRele
+    BR_4e   = BRele*BRele    
+    BR      = BR_4m+BR_2e2m+BR_4e
+    
+    print "BR sum",BR," BRmean ",(BR_4m + BR_4e + BR_2e2m)/3.," BR_4m ",BR_4m," BR_2e2m ",BR_2e2m," BR_4e ",BR_4e
+    
+    # wspace.var("BR_4m").setVal(BR_4m)
+    # wspace.var("BR_2e2m").setVal(BR_2e2m)
+    # wspace.var("BR_4e").setVal(BR_4e)
+    
+    wspace.var("xs_4m").setConstant(True) # needed for being treated as global observables
+    wspace.var("xs_2e2m").setConstant(True) # needed for being treated as global observables
+    wspace.var("xs_4e").setConstant(True) # needed for being treated as global observables
+    
+    wspace.var("Lumi").setConstant(True) 
+           
+    wspace.defineSet("obs","nobs_4m,nobs_4e,nobs_2e2m")  #observables
+    wspace.defineSet("poi","mu"); #parameters of interest
+    wspace.defineSet("np","bkg_4m,bkg_4e,bkg_2e2m,irrBkg_4m,irrBkg_4e,irrBkg_2e2m,eff_4m,eff_4e,eff_2e2m") #nuisance_lumi
+        
+    wspace.Print()
+    
+    
+  # make data set with the namber of observed events
+    print "---Make data Set---"       
+    data = ROOT.RooDataSet("data","", ROOT.RooArgSet(wspace.var("nobs_4m"),wspace.var("nobs_2e2m"),wspace.var("nobs_4e")));
+     
+    data.add(ROOT.RooArgSet(wspace.var("nobs_4m") ))
+    data.add(ROOT.RooArgSet(wspace.var("nobs_2e2m") ))
+    data.add(ROOT.RooArgSet(wspace.var("nobs_4e") ))
+
+    print "---Make Frame---"       
+#    frame = wspace.var("mu").frame(0.95,1.1);
+    frame = wspace.var("mu").frame(1.1,1.25);
+    
+    print "---Make nll---"       
+    nll = wspace.pdf("model").createNLL(data)
+    print "Initial NLL value",nll.getVal()
+    nll.plotOn(frame,ROOT.RooFit.ShiftToZero()); #the ShiftToZero option puts the minimum at 0 on the y-axis
+
+    #nllcopy = copy.deepcopy(nll)
+
+    #nllcopy.plotOn(frame,ROOT.RooFit.ShiftToZero()); #the ShiftToZero option puts the minimum at 0 on the y-axis
+    #   nllcopy.plotOn(frame)
+    mini = ROOT.RooMinuit(nll);
+    mini.minos(wspace.set("poi"));
+
+    print "NLL value",nll.getVal()       
+    print "---Create profile---"   
+
+    pll = nll.createProfile(wspace.set("poi"))
+    pll.plotOn(frame,ROOT.RooFit.LineColor(ROOT.kRed))
+    
+    frame.SetMaximum(1.) 
+    c = ROOT.TCanvas( 'c', 'c', 200, 10, 900, 700 )
+    frame.Draw()
+    c.SaveAs("Plot/likelihood.png")
+    c.SaveAs("~/www/PlotsVV/13TeV/likelihood.png")
+    mu = wspace.var("mu").getValV()
+    mu_hi = wspace.var("mu").getErrorHi()
+    mu_lo = wspace.var("mu").getErrorLo()
+
+    #   print "mu",mu,"cross",mu*34.34,"+",(mu+mu_hi)*34.34,"-",(mu+mu_lo)*34.34
+    #   print "Total Cross ",(mu*34.34)/(0.5338*BR)
+
+    TotThCross = xs_tight["4l"] #xs_2e2m +xs_4m + xs_4e
+#    Acc = xs_4e/xs_OS_4e #FIXME 
+    Acc = xs_tight["4e"]/xs_OS_4e #FIXME 
+    print "Acc",Acc
+    #print "mu",mu,"cross",mu*TotThCross,"+",(mu+mu_hi)*TotThCross,"-",(mu+mu_lo)*TotThCross
+
+    print "mu",mu,"cross",mu*TotThCross,"+",(mu_hi)*TotThCross,(mu_lo)*TotThCross
+    print "Total Cross ",(mu*TotThCross)/(Acc*BR)
+
+    return mu*TotThCross
+
+
+
+
---
+++ TreeAnalysis/python/computeInlcusiveCross.py
@@ -0,0 +1,189 @@
+#! /usr/bin/env python
+
+##################################
+## G. Pinna (UNITO) - Jun 2015 ##
+##################################
+
+import ROOT,copy
+from ROOT import gSystem, TCanvas, TH1,  TPad, gStyle, TLegend,TGraphAsymmErrors,Math,TArrayD
+import collections 
+import CrossInfo
+from CrossInfo import*
+from Colours import *
+from optparse import OptionParser
+import sys,ast,os
+import math
+import operator
+import textwrap
+import collections                                                                                    
+
+import LikelihoodXS
+from LikelihoodXS import*
+
+
+parser = OptionParser(usage="usage: %prog <final state> [options]")
+
+parser.add_option("-S", "--Set", dest="Set",
+                  default="Pow",
+                  help="MC samples Set, default is Pow (Powheg) the other one is Amc (Amcatnlo)")
+
+parser.add_option("-A", "--Analysis", dest="Analysis",
+                  default="ZZ",
+                  help="Analysis, default is  ZZ, others are ZZFull and HZZ")
+
+
+(options, args) = parser.parse_args()
+
+Set = options.Set
+Analysis  = options.Analysis
+
+if Analysis!="ZZ":
+    inputdir     = "./results/ZZRecoAnalyzer_SR_"+Analysis+"/"
+    inputdir_CR  = "./results/ZZRecoAnalyzer_CR_"+Analysis+"/"
+else:
+    inputdir     = "./results/ZZRecoAnalyzer_SR/"
+    inputdir_CR  = "./results/ZZRecoAnalyzer_CR/"
+
+
+# def getRedBkg(FinState,Sign):
+
+#         hFakeRate.Add(setErrorsEntries(fileFake.Get("ZZTo"+FinState+"_Mass_FRVar")),-1) #CHECK
+#     if  Sign==-1:
+#         hFakeRate = fileFake.Get("ZZTo"+FinState+"_Mass_01")
+#         hFakeRate.Add(setErrorsEntries(fileFake.Get("ZZTo"+FinState+"_Mass_FRVar")),1)
+#     else:
+#         hFakeRate = fileFake.Get("ZZTo"+FinState+"_Mass_01")
+#     Err=ROOT.Double(0.)
+#     Integr= hFakeRate.IntegralAndError(0,-1,Err)
+#     if hFakeRate.Integral(0,-1)<=0: 
+#         hFakeRate.Scale(0)    
+
+#     print "{0} contribution {1}> {2:.2f}\n".format(FinState,(33-len(FinState))*"-",hFakeRate.Integral(0,-1))
+#     return  copy.deepcopy(hFakeRate)
+
+#################################################################################################################
+
+def setErrorsEntries(hVar):
+    Nbins=hVar.GetNbinsX()
+    for k in range(1,Nbins+1):	
+        hVar.SetBinContent(k,math.sqrt(hVar.GetBinContent(k)))
+#        print "integrale",hVar.Integral(0,-1)
+    return hVar
+
+
+##################################################################################################################
+
+####################  Get, sum and set histograms from data or from MC reco as they were data ####################
+
+##################################################################################################################
+
+
+DataFile = ROOT.TFile(inputdir+"data.root")
+
+hsum2e2mu = ROOT.TH1F()
+hsum4e    = ROOT.TH1F()
+hsum4mu   = ROOT.TH1F()
+
+hSum = [{"hist":hsum2e2mu,"name":'2e2m'},{"hist":hsum4e,"name":'4e'},{"hist":hsum4mu,"name":'4m'}]
+print Red("\nData\n")
+for h in hSum:
+    h1 = DataFile.Get("ZZTo"+h["name"]+"_Mass_01")
+    h["hist"] = copy.deepcopy(h1)
+    print "{0} contribution {1}> {2:.2f}\n".format(h["name"],(33-len(h["name"]))*"-",h1.Integral(0,-1))
+
+hIrredBkg2e2mu= ROOT.TH1F() 
+hIrredBkg4mu = ROOT.TH1F() 
+hIrredBkg4e = ROOT.TH1F() 
+
+hIrredSum = [{"hist":hIrredBkg2e2mu,"name":'2e2m'},{"hist":hIrredBkg4e,"name":'4e'},{"hist":hIrredBkg4mu,"name":'4m'}]         
+
+filesbkg ={}
+
+for b in BkgSamples:
+    filesbkg[b["sample"]] = ROOT.TFile(inputdir+b["sample"]+".root") 
+
+print Red("Irreducible Background\n")
+
+for h in hIrredSum:
+    isFirst=1
+    for b in BkgSamples:
+        h1 = filesbkg[b["sample"]].Get("ZZTo"+h["name"]+"_Mass_01")
+        if h1==None:
+            print "For sample ", b["sample"], "h"+h["name"],"has no enetries or is a zombie"      
+            continueb
+        if isFirst:
+            h["hist"]=copy.deepcopy(h1) 
+            isFirst=0
+            continue
+        print "{0} contribution {1}> {2:.2f}\n".format(h["name"],(33-len(h["name"]))*"-",h["hist"].Integral(0,-1))
+        h["hist"].Add(h1)     
+
+print Red("Reducible Background\n")
+
+
+fileFake = ROOT.TFile(inputdir_CR+"data.root")
+
+print Blue("Central value\n" )
+hRed2e2mu = copy.deepcopy(fileFake.Get("ZZTo2e2m_Mass_01"))
+hRed4e    = copy.deepcopy(fileFake.Get("ZZTo4e_Mass_01"))
+hRed4mu   = copy.deepcopy(fileFake.Get("ZZTo4m_Mass_01"))
+
+hRedSum = [{"hist":hRed2e2mu,"name":'2e2m'},{"hist":hRed4e,"name":'4e'},{"hist":hRed4mu,"name":'4m'}]
+
+for h in hRedSum:
+    Err=ROOT.Double(0.)
+    print "{0} contribution {1}> {2:.2f} +- {3:.2f}\n".format(h["name"],(33-len(h["name"]))*"-",h["hist"].IntegralAndError(0,-1,Err),Err)
+
+AccFile     = ROOT.TFile("./Acceptance/Acceptance_"+Set+"_Mass.root")
+AccFile_Hi  = ROOT.TFile("./Acceptance/AcceptanceSFactorSqPlus_"+Set+"_Mass.root")
+AccFile_Low = ROOT.TFile("./Acceptance/AcceptanceSFactorSqMinus_"+Set+"_Mass.root")
+
+wspace = ROOT.RooWorkspace("w")
+
+for hData,hRed,hIrr in zip(hSum,hRedSum,hIrredSum):
+
+    hData["hist"].Rebin(8)
+    hRed["hist"].Rebin(8)
+
+    print "\nnobs_" +hData["name"],hData["hist"].Integral(0,-1)
+    wspace.factory("nobs_"+hData["name"]+"["+str(hData["hist"].Integral(0,-1))+",0,300]")
+
+    Err=ROOT.Double(0.)
+    print "red_" +hData["name"],hRed["hist"].IntegralAndError(0,-1,Err),"+-",Err
+    wspace.factory("bkg_"+ hData["name"]+"["+str(hRed["hist"].Integral(0,-1))+"]")
+    wspace.factory("bkg0_"+ hData["name"]+"["+str(hRed["hist"].Integral(0,-1))+",0,50]")
+    wspace.factory("sigmaBkg_"+hData["name"]+"["+str(Err)+"]") 
+ 
+
+    print "Irr_" +hData["name"],hIrr["hist"].IntegralAndError(0,-1,Err),"+-",Err
+    wspace.factory("irrBkg_"+ hData["name"]+"["+str(hIrr["hist"].Integral(0,-1))+"]")
+    wspace.factory("irrBkg0_"+ hData["name"]+"["+str(hIrr["hist"].Integral(0,-1))+",0,50]")
+    wspace.factory("sigmaIrrBkg_"+hData["name"]+"["+str(Err)+"]") 
+
+
+    Acc     = AccFile.Get("TotAcc"+hData["name"]+"_fr").GetVal()
+    Acc_Hi  = AccFile_Hi.Get("TotAcc"+hData["name"]+"_fr").GetVal()
+    Acc_Low = AccFile_Low.Get("TotAcc"+hData["name"]+"_fr").GetVal()
+
+    print "Acc",Acc,"Sigma Acc",Acc_Hi-Acc
+    wspace.factory("eff_"+hData["name"]+"["+str(Acc)+"]")
+    wspace.factory("eff0_"+hData["name"]+"["+str(Acc)+",0,1.]")
+    wspace.factory("sigmaEff_"+hData["name"]+"["+str(Acc_Hi-Acc)+"]")
+
+    # Use diffent funcion to for nuisance parameter?
+    # wspace.factory("sigmaEff"+hData["name"]+"["+str(Acc-Acc_Low))+"]")
+    
+#xs_tight = {"2e2m":16.37,"4m":8.19,"4e":8.19,"4l":32.75} #To be corrected with MCFM values
+
+wspace.factory("xs_4m["+str(xs_tight["4m"])+",0,30]")
+wspace.factory("xs_4e["+str(xs_tight["4e"])+",0,30]")
+wspace.factory("xs_2e2m["+str(xs_tight["2e2m"])+",0,30]")
+
+Lumi = Lumi/1000.
+wspace.factory("Lumi["+str(Lumi)+",0,100]")
+
+cross  = LikeCross(wspace)
+#print "CROSS",cross
+    
+
+
