8eb72e0c1e2db656282c7d520fc9967569a221fc
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2016-11-23 09:49:39 +0100

    tools for string manipulation

---
+++ Commons/interface/StringTools.h
@@ -0,0 +1,11 @@
+#ifndef VVXAnalysis_Commons_StringTools_H
+#define VVXAnalysis_Commons_StringTools_H
+#include <string>
+#include <iostream> 
+
+namespace strtool{
+  bool sortEvents(std::string i, std::string j);
+  
+  std::string sRound(float val);
+}
+#endif
