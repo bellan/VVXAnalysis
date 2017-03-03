8c8bd48d4c8e1c5cdf33bcd03942833a0fa44d8d
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2017-03-03 10:33:06 +0100

    add gencatorgory request

--- Producers/plugins/ZLFilter.cc
+++ Producers/plugins/ZLFilter.cc
@@ -13,6 +13,7 @@
 #include <FWCore/ParameterSet/interface/ParameterSet.h>
 #include <DataFormats/PatCandidates/interface/CompositeCandidate.h>
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
+#include <ZZAnalysis/AnalysisStep/interface/bitops.h>
 
 class ZLFilter : public edm::EDFilter {
 public:
@@ -21,7 +22,10 @@ public:
     : theZLLToken(consumes<edm::View<pat::CompositeCandidate> >(config.getParameter<edm::InputTag>("ZLL")))
     , theZLToken(consumes<edm::View<pat::CompositeCandidate> >(config.getParameter<edm::InputTag>("ZL")))
     , selectionZL_(config.getParameter<std::string>("ZLSelection"))
+    , isMC_ (config.getUntrackedParameter<bool>("isMC",false))
   {
+
+    theGenCategoryToken = consumes<int> (config.getUntrackedParameter<edm::InputTag>("GenCategory" , edm::InputTag("genCategory")));
 
   }
   
@@ -36,26 +40,28 @@ private:
 
   edm::EDGetTokenT<edm::View<pat::CompositeCandidate> > theZLLToken;
   edm::EDGetTokenT<edm::View<pat::CompositeCandidate> > theZLToken;
+  edm::EDGetTokenT<int> theGenCategoryToken;
 
   StringCutObjectSelector<pat::CompositeCandidate> selectionZL_;
+
+  bool isMC_;
+
 };
 
 
 bool ZLFilter::filter(edm::Event& event, const edm::EventSetup& setup){  
 
+  edm::Handle<int> genCategory;  event.getByToken(theGenCategoryToken, genCategory);
 
   edm::Handle<edm::View<pat::CompositeCandidate> > ZLL  ; event.getByToken(theZLLToken, ZLL);
   edm::Handle<edm::View<pat::CompositeCandidate> > ZL   ; event.getByToken(theZLToken ,  ZL);
 
-
-  // at least a ZZ or a ZLL candidate in the event --> want to keep it
   if(ZLL->size() > 0) return true;
   // if there is only one ZL candidate --> want to keep it if it passes trigger requirements
   if(ZL->size() == 1 && selectionZL_(ZL->at(0))) return true; 
+  if(isMC_ && test_bit_16(*genCategory,0)) return true;
     
   return false;
-
-
 }
 
 
