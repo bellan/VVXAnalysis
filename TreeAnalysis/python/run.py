67d61cf1bed4709768efe34c3205b666cb298b8d
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2017-03-04 10:29:51 +0100

    update lumi

--- TreeAnalysis/python/run.py
+++ TreeAnalysis/python/run.py
@@ -26,8 +26,8 @@ parser.add_option("-e", "--external-cross-section", dest="getExternalCrossSectio
 
 parser.add_option("-l", "--luminosity", dest="luminosity",
                   type='int',
-                  default= 36460,
-                  help="Set luminosity scenario from command line. Default is  15941/pb.")
+                  default= 35900,
+                  help="Set luminosity scenario from command line. Default is  35900/pb.")
 
 parser.add_option("-d", "--directory", dest="directory",
                   default="samples",
