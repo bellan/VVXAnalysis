f89ac48d9225e104a6b8072c74c2eebe57d0e55a
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2017-03-07 11:06:44 +0100

    new samples

--- Producers/test/MvSamples.py
+++ Producers/test/MvSamples.py
@@ -9,14 +9,18 @@ from os import walk
 Targetfile= sys.argv[1]
 
 What= sys.argv[2]
-#datalist = ["DoubleEG2016B","DoubleMu2016B","MuonEG2016B","SingleEle2016B","SingleMuon2016B","DoubleEG2016C","DoubleMu2016C","MuonEG2016C","SingleEle2016C","SingleMuon2016C","DoubleEG2016D","DoubleMu2016D","MuonEG2016D","SingleEle2016D","SingleMuon2016D","DoubleEG2016E","DoubleMu2016E","MuonEG2016E","SingleEle2016E","SingleMuon2016E","DoubleEG2016F","DoubleMu2016F","MuonEG2016F","SingleEle2016F","SingleMuon2016G","DoubleEG2016G","DoubleMu2016G","MuonEG2016G","SingleEle2016G","SingleMuon2016F","DoubleEG2016H","DoubleMu2016H","MuonEG2016H","SingleEle2016H","SingleMuon2016H"]
-
-datalist = ["DoubleEG2016C","DoubleMu2016C","MuonEG2016C","SingleEle2016C","SingleMuon2016C","DoubleEG2016D","DoubleMu2016D","MuonEG2016D","SingleEle2016D","SingleMuon2016D"]
+datalist = ["DoubleEG2016B","DoubleMu2016B","MuonEG2016B","SingleEle2016B","SingleMuon2016B",
+            "DoubleEG2016C","DoubleMu2016C","MuonEG2016C","SingleEle2016C","SingleMuon2016C",
+            "DoubleEG2016D","DoubleMu2016D","MuonEG2016D","SingleEle2016D","SingleMuon2016D",
+            "DoubleEG2016E","DoubleMu2016E","MuonEG2016E","SingleEle2016E","SingleMuon2016E",
+            "DoubleEG2016F","DoubleMu2016F","MuonEG2016F","SingleEle2016F","SingleMuon2016F",
+            "DoubleEG2016G","DoubleMu2016G","MuonEG2016G","SingleEle2016G","SingleMuon2016G",
+            "DoubleEG2016Hv2","DoubleMu2016Hv2","MuonEG2016Hv2","SingleEle2016Hv2","SingleMuon2016Hv2",
+            "DoubleEG2016Hv3","DoubleMu2016Hv3","MuonEG2016Hv3","SingleEle2016Hv3","SingleMuon2016Hv3"]
 
 datalist_2015 = ["DoubleEG2015C","DoubleMu2015C","MuonEG2015C","SingleEle2015C","DoubleEG2015D","DoubleMu2015D","MuonEG2015D","SingleEle2015D"]
 
-MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","DYJetsToLL_M50","TTJets","TTTo2L2Nu","WZ","ZGTo2LG","ZH125","ttH125","VBFH125","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","WWZ","TTGJets","TTWJets","WWW","ZZZ","WZZ"]
-
+MCList = ["ZZTo4l","ZZTo4lamcatnlo","ggZZ4e","ggZZ4mu","ggZZ2e2mu","ggZZ4tau","ggZZ2e2tau","ggZZ2mu2tau","DYJetsToLL_M50","TTJets","TTTo2L2Nu","WZ","ZGTo2LG","ZH125","ttH125","VBFH125","ZZTo2e2muJJ","ZZTo4muJJ","ZZTo4eJJ","WWZ","TTGJets","TTWJets","WWW","ZZZ","WZZ","ZZJJTo4L_ewk","TTZToLL"]
 
 AddbashCommand="hadd -f "+Targetfile+"/Data.root "
 bashCommand="mv "
@@ -27,8 +31,8 @@ if What=="All" or What=="Data":
         print bashCommand+" "+Dir+"/ZZ4lAnalysis.root "+Targetfile+"/"+Dir+".root"
         os.system(bashCommand+" "+Dir+"/ZZ4lAnalysis.root "+Targetfile+"/"+Dir+".root")
 
-        print AddbashCommand
-        os.system(AddbashCommand)
+    print AddbashCommand
+#        os.system(AddbashCommand)
  
 if What=="All" or What=="MC":
     for s in MCList:
