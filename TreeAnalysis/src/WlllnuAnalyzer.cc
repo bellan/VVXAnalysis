fbfed5c826988dba26ae1fb43eb130d26a8353eb
Riccardo <riccardo.bellan@cern.ch>
2017-05-12 11:42:59 +0200

    fix parenthesis

--- TreeAnalysis/src/WlllnuAnalyzer.cc
+++ TreeAnalysis/src/WlllnuAnalyzer.cc
@@ -26,7 +26,7 @@ void WlllnuAnalyzer::analyze(){
   cout << "Run: " << run << " event: " << event << endl;
   
   foreach(const phys::Particle &gen, *genParticles){
-    if(abs(gen.id()) != 11 && abs(gen.id()) != 13 || (!(gen.genStatusFlags().test(phys::GenStatusBit::isPrompt)) || !(gen.genStatusFlags().test(phys::GenStatusBit::fromHardProcess))) continue;
+    if((abs(gen.id()) != 11 && abs(gen.id()) != 13) || (!(gen.genStatusFlags().test(phys::GenStatusBit::isPrompt)) || !(gen.genStatusFlags().test(phys::GenStatusBit::fromHardProcess)))) continue;
     cout << "id: " << gen.id() << " pt: " << gen.pt() << endl;
     theHistograms.fill("ptAllGenParticle","pt ", 100, 0, 100, gen.pt());
   }
