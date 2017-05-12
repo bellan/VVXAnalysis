e64f43447d36f167c6c1ea19d2c2fdc30937fa75
Arianna <arianna.corrado@edu.unito.it>
2017-05-12 15:48:40 +0200

    missing brace

--- TreeAnalysis/src/WlllnuAnalyzer.cc
+++ TreeAnalysis/src/WlllnuAnalyzer.cc
@@ -26,15 +26,10 @@ void WlllnuAnalyzer::analyze(){
   cout << "Run: " << run << " event: " << event << endl;
   
   foreach(const phys::Particle &gen, *genParticles){
-<<<<<<< HEAD
     if(abs(gen.id()) != 11 && abs(gen.id()) != 13 || (!(gen.genStatusFlags().test(phys::GenStatusBit::isPrompt)) || !(gen.genStatusFlags().test(phys::GenStatusBit::fromHardProcess)))) continue;
-       cout << "id: " << gen.id() << " pt: " << gen.pt() << endl;
-       theHistograms.fill("ptAllGenParticle","pt ", 100, 0, 100, gen.pt());
-=======
-    if((abs(gen.id()) != 11 && abs(gen.id()) != 13) || (!(gen.genStatusFlags().test(phys::GenStatusBit::isPrompt)) || !(gen.genStatusFlags().test(phys::GenStatusBit::fromHardProcess)))) continue;
     cout << "id: " << gen.id() << " pt: " << gen.pt() << endl;
     theHistograms.fill("ptAllGenParticle","pt ", 100, 0, 100, gen.pt());
->>>>>>> 1bb3e3e955425cd6fbf9a3f621411228308554cf
+    
   }
   
 }
