f1efecf76ffcd92f9948976239ae985e1f8d88ee
Eleonora <eleonora.racca@cern.ch>
2017-05-24 12:42:35 +0200

    first commit of WZ class

---
+++ TreeAnalysis/interface/WZAnalyzer.h
@@ -0,0 +1,66 @@
+#ifndef WZAnalyzer_h
+#define WZAnalyzer_h
+
+/** \class WZAnalyzer
+ *  Concrete class for WZ analysis
+ *
+ *  $Date: 2013/03/15 13:37:31 $
+ *  $Revision: 1.4 $
+ *  \author E. Racca - UNITO <eleonora.racca@cern.ch>
+ */
+
+
+#include "EventAnalyzer.h"
+#include "RegistrableAnalysis.h"
+#include "VVXAnalysis/Commons/interface/Constants.h"
+#include "VVXAnalysis/Commons/interface/LeptonScaleFactors.h"
+
+class WZAnalyzer: public EventAnalyzer, RegistrableAnalysis<WZAnalyzer>{
+
+public:
+
+  //, const std::string& filename, const double& lumi = 1., const double& externalXSection = -1., bool doBasicPlots = false
+
+ WZAnalyzer(const AnalysisConfiguration& configuration)
+   : EventAnalyzer(*(new Selector<WZAnalyzer>(*this)), 
+		   configuration){
+    //theHistograms.profile(genCategory);
+  }
+
+  virtual ~WZAnalyzer(){}
+
+  virtual void analyze();
+  
+  virtual Int_t cut();
+
+
+ private:
+  friend class Selector<WZAnalyzer>;
+  template< class PAR >
+    bool ZBosonDefinition(phys::Boson<PAR> cand) const{
+    bool checkCharge = cand.daughter(0).charge() + cand.daughter(1).charge() == 0;
+    return checkCharge && fabs(cand.p4().M() - phys::ZMASS) < 30;
+  }
+
+
+  template< class PAR >
+    bool WBosonDefinition(phys::Boson<PAR> cand) {
+
+    bool gooddaughters = false;
+    if(fabs(cand.daughter(0).eta()) < 2.5 && cand.daughter(0).pt() > 30 &&
+       cand.daughter(0).passPUID() && cand.daughter(0).passLooseJetID() &&
+       fabs(cand.daughter(1).eta()) < 2.5 && cand.daughter(1).pt() > 30 &&
+       cand.daughter(1).passPUID() && cand.daughter(1).passLooseJetID())
+      gooddaughters = true;
+
+    if(fabs(cand.p4().M() - phys::WMASS) < 150 && gooddaughters)
+      return true;
+    return false;
+
+  }
+
+
+
+};
+#endif
+
