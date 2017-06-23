dfe571fd9159b1995bae051e72091c5cc2a5e038
Arianna <arianna.corrado@edu.unito.it>
2017-06-23 10:41:00 +0200

    new comparators for pairs

--- Commons/interface/AriEle.h
+++ Commons/interface/AriEle.h
@@ -21,20 +21,38 @@ template<typename T> double mT(const T& p1, const T& p2, const T& p3){
 
 
 struct mTComparator{
-  mTComparator(const double& ref): ref_(ref){}
-    template<typename PAR>
-    bool operator()(const PAR & a , 
-		    const PAR & b) const{ 
-      return fabs(mT(a.daughter(0), a.daughter(1))-ref_) < fabs(mT(b.daughter(0), b.daughter(1))-ref_); 
-    }
+mTComparator(const double& ref): ref_(ref){}
+  template<typename BOS>
+  bool operator()(const BOS & a , 
+		  const BOS & b) const{ 
+    return fabs(mT(a.daughter(0), a.daughter(1))-ref_) < fabs(mT(b.daughter(0), b.daughter(1))-ref_); 
+  }
+  
+  template<typename PAR>
+  bool operator()(const std::pair<PAR, PAR> & a , 
+		  const std::pair<PAR, PAR> & b) const{ 
+    return fabs(mT(a.first, a.second)-ref_) < fabs(mT(b.first, b.second)-ref_); 
+  }
   /*    template<typename PAR>
 	bool operator()(const PAR * a , 
 	const PAR * b) const{ 
 	return fabs(a->p4().M()-ref_) < fabs(b->p4().M()-ref_); 
 	}*/
-   double ref_;
+  double ref_;
 };
+  
+struct massComparator{
+massComparator(bool element, const double& ref): element_(element), ref_(ref){}
+  template<typename T1, typename T2>
+    bool operator()(const std::pair<T1, T2> & a, const std::pair<T1, T2> & b) const{    
+    if(element_ == 0) return fabs(a.first.p4().M()-ref_) < fabs(b.first.p4.M()-ref_);
+    else if (element_ == 1) return fabs(a.second.p4().M()-ref_) < fabs(b.second.p4.M()-ref_);    
+  }
 
+  bool element_;
+  double ref_;
+};
+  
 template<typename T> bool isTheSame(const T& p1, const T& p2){
   return !(p1.pt() != p2.pt());
   //return abs(p1.pt()- p2.pt()) < 0.1;
