e94d53d6114154228029024d847bffa2d3c632fc
gian luca pinna angioni <gian.luca.pinna.angioni@cern.ch>
2017-12-07 17:58:03 +0100

    First time in

---
+++ TreeAnalysis/python/PlotStyle.py
@@ -0,0 +1,132 @@
+import ROOT
+
+hatchSpace    = 0.6
+MadLine       = 1
+MadLineStyle  = 0
+MadFillColor  = 0
+MadFillStyle  = 1001
+#MadColor      = ROOT.kAzure+4                                                                                                                                                                                                                
+MadDrawStyle  = "2"
+MadColor      = 852
+MadAlpha      = 0.6
+MadLineWidth  = 2
+
+MG5LineStyle  = 5
+MG5LineWidth  = 2
+MG5Color      = ROOT.kBlue
+MG5FillColor  = ROOT.kBlue
+MG5FillStyle  = 1001
+MG5Alpha      = 0.45
+
+PowLineStyle  = 2
+PowLineWidth  = 2
+PowColor      = ROOT.kRed
+PowFillColor  = ROOT.kRed
+PowAlpha      = 0.45
+
+DataFillStyle = 3354
+DataAlpha     = 0.6
+DataFillColor = ROOT.kBlack
+
+corrMad       = 0.3
+corrPow       = 0.2
+
+
+
+'''
+hatchSpace    = 0.6 
+MadLine       = 1
+MadLineStyle  = 7
+MadFillColor  = 851
+MadFillStyle  = 1001
+MadColor      = ROOT.kAzure+4
+#MadColor      = 852
+MadAlpha      = 0.6
+MadLineWidth  = 2 
+
+MG5LineStyle  = 5
+MG5LineWidth  = 2
+MG5Color      = ROOT.kBlue
+MG5FillColor  = ROOT.kBlue
+MG5FillStyle  = 1001
+MG5Alpha      = 0.45
+
+PowLineStyle  = 2
+PowLineWidth  = 2 
+PowColor      = ROOT.kRed
+PowFillColor  = ROOT.kRed
+PowAlpha      = 0.45
+
+DataFillStyle = 3354
+DataAlpha     = 0.6
+DataFillColor = ROOT.kBlack    
+
+corrMad       = 0.3
+corrPow       = 0.2
+
+'''
+
+'''
+hatchSpace    = 0.6 
+MadLine       = 1
+MadFillColor  = ROOT.kWhite
+MadFillStyle  = 1001
+MadColor      = 852
+MadAlpha      = 0.7
+MadLineWidth  = 2 
+
+MG5LineStyle  = 5
+MG5LineWidth  = 2
+MG5Color      = ROOT.kBlue
+MG5FillColor  = ROOT.kBlue
+MG5FillStyle  = 3463
+MG5Alpha      = 0.9
+
+PowLineStyle  = 2
+PowLineWidth  = 2 
+PowColor      = ROOT.kRed
+PowFillColor  = ROOT.kRed
+PowFillStyle  = 1001
+PowAlpha      = 0.45
+
+DataFillStyle = 3354
+DataAlpha     = 0.6
+DataFillColor = ROOT.kBlack    
+
+corrMad       = 0.3
+corrPow       = 0.2
+
+'''
+
+
+'''
+
+hatchSpace    = 0.6 
+MadLine       = 1
+MadFillColor  = 851
+MadFillStyle  = 3354
+MadColor      = 852
+MadAlpha      = 0.9
+MadLineWidth  = 2 
+
+MG5LineStyle  = 5
+MG5LineWidth  = 2
+MG5Color      = ROOT.kBlue
+MG5FillColor  = ROOT.kBlue
+MG5FillStyle  = 1001
+MG5Alpha      = 0.45
+
+PowLineStyle  = 2
+PowLineWidth  = 2 
+PowColor      = ROOT.kRed
+PowFillColor  = ROOT.kRed
+PowAlpha      = 0.45
+
+DataFillStyle = 1001
+DataFillColor = ROOT.kBlack
+DataAlpha     = 0.35
+
+corrMad       = 0.2
+corrPow       = 0.1
+
+'''
