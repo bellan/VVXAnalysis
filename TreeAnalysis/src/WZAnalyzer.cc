ba55081a45c60974adc2f3975bb38a39a3dae959
Eleonora <eleonora.racca@cern.ch>
2018-01-24 16:30:55 +0100

    genjets without leptons and minor changes

--- TreeAnalysis/src/WZAnalyzer.cc
+++ TreeAnalysis/src/WZAnalyzer.cc
@@ -19,12 +19,15 @@ using namespace std;
 
 void WZAnalyzer::begin() {
 
+  //events counters
   nunumber = 0;
   totalevent = 0;
   WZevent = 0;
   zahl = 0;
   recoZlempty = 0;
+  wrongrecognition = 0;
 
+  //free counters
   threemuonsplus = 0;
   threemuonsminus = 0;
   threeelesplus = 0;
@@ -71,11 +74,20 @@ void WZAnalyzer::analyze(){
   if(electron.size() + muon.size() + neutrino.size() != 4){
     return;
   }
-
+  
   // filter on jets number
-  theHistograms.fill("GenJets_number", "number of all gen jets",  13,  -0.5,  12.5, genJets->size(), theWeight);
-  if(genJets->size() != 2){
-    cout << "Wrong number of jets" << endl;
+  foreach(const Particle jet, *genJets){
+    bool leptonMatch = false; //in genJets leptons are included
+    foreach(const phys::Particle &gen, *genParticles)
+      if(physmath::deltaR(gen,jet) < 0.4 && (abs(gen.id()) == 11 || abs(gen.id()) == 13)) leptonMatch = true;
+    
+    if(!leptonMatch){
+      if(fabs(jet.eta()) < 4.7) genjets.push_back(jet);
+    }
+  }
+
+  if(genjets.size() < 2){
+    cout << "Not enough jets" << endl;
     return;
   }
   // */
@@ -100,9 +112,9 @@ void WZAnalyzer::analyze(){
     lepton.push_back(mu);
   }
   
-  stable_sort(electron.begin(), electron.end(), PtComparator());
-  stable_sort(lepton.begin(), lepton.end(), PtComparator());
-  stable_sort(muon.begin(), muon.end(), PtComparator());
+  sort(electron.begin(), electron.end(), PtComparator());
+  sort(lepton.begin(), lepton.end(), PtComparator());
+  sort(muon.begin(), muon.end(), PtComparator());
 
   // second filter on pt and eta
   // /*
@@ -130,8 +142,8 @@ void WZAnalyzer::analyze(){
   double masslllnu = Ptot.M();
   double trmasslllnu = Ptot.Mt();
 
-  theHistograms.fill("AllGenlllnu_mass",   "m 3 leptons and #nu",     600, 0, 1500, masslllnu  , theWeight);
-  theHistograms.fill("AllGenlllnu_trmass", "m_{T} 3 leptons and #nu", 600, 0, 1500, trmasslllnu, theWeight);
+  theHistograms.fill("AllGenlllnu_mass",   "m 3 leptons and #nu",     300, 0, 1500, masslllnu  , theWeight);
+  theHistograms.fill("AllGenlllnu_trmass", "m_{T} 3 leptons and #nu", 300, 0, 1500, trmasslllnu, theWeight);
 
   if(masslllnu < 165){
     cout << Yellow("\nTotal mass of the products insufficient for the WZ analysis.") << endl;
@@ -243,19 +255,24 @@ void WZAnalyzer::analyze(){
     // */
   }
 
+  //filter on Z and W mass
+  if(Zet.mass() < 60 || Zet.mass() > 120 || Weh.mass() < 50 || Weh.mass() > 110){
+    return;
+  }
+  
   WZ = ZZtype(Weh, Zet);
   
   double ZdeltaEta = Zet.daughter(0).eta() - Zet.daughter(1).eta();
   //double ZdeltaPhi = physmath::deltaPhi(Zet.daughter(0).phi(), Zet.daughter(1).phi());
-  double ZdeltaR = physmath::deltaR(Zet.daughter(0), Zet.daughter(1));
+  double ZdeltaR = abs(physmath::deltaR(Zet.daughter(0), Zet.daughter(1)));
   
   double WdeltaEta = Weh.daughter(0).eta() - Weh.daughter(1).eta();
   double WdeltaPhi = physmath::deltaPhi(Weh.daughter(0).phi(), Weh.daughter(1).phi());
-  double WdeltaR = physmath::deltaR(Weh.daughter(0), Weh.daughter(1));
+  double WdeltaR = abs(physmath::deltaR(Weh.daughter(0), Weh.daughter(1)));
   
   double WZdeltaEta = Zet.eta() - Weh.eta();
   //double WZdeltaPhi = physmath::deltaPhi(Zet.phi(), Weh.phi());
-  double WZdeltaR = physmath::deltaR(Zet, Weh);
+  double WZdeltaR = abs(physmath::deltaR(Zet, Weh));
 
   
   // Histograms and printouts
@@ -274,38 +291,38 @@ void WZAnalyzer::analyze(){
 
   //neutrino histograms
   // /*
-  theHistograms.fill("GenN_charge", "#nu's charge",  5, -2.5,   2.5, neutrino[0].charge(), theWeight); //just to be sure...
-  theHistograms.fill("GenN_pt",     "#nu's p_{t}", 280,  0  , 700  , neutrino[0].pt()    , theWeight);
+  theHistograms.fill("GenN_pt",     "#nu's p_{t}", 140,  0  , 700  , neutrino[0].pt()    , theWeight);
   theHistograms.fill("GenN_eta",    "#nu's #eta",   90, -6.5,   6.5, neutrino[0].eta()   , theWeight);
-  theHistograms.fill("GenN_phi",    "#nu's #phi",   50, -4  ,   4  , neutrino[0].phi()   , theWeight);
+  //theHistograms.fill("GenN_charge", "#nu's charge",  5, -2.5,   2.5, neutrino[0].charge(), theWeight); //just to be sure...
+  //theHistograms.fill("GenN_phi",    "#nu's #phi",   50, -4  ,   4  , neutrino[0].phi()   , theWeight);
   //theHistograms.fill("GenN_Y",      "#nu's Y",      90, -6.5,   6.5, neutrino[0].rapidity(), theWeight);
   // */
   
   //W histograms
   theHistograms.fill("GenW_charge",   "W's charge",       5, -2.5,   2.5, Weh.charge()  , theWeight);
-  theHistograms.fill("GenW_mass",     "W's mass",       400,  0  , 400  , Weh.mass()    , theWeight);
-  theHistograms.fill("GenW_pt",       "W's p_{t}",      260,  0  , 650  , Weh.pt()      , theWeight);
+  theHistograms.fill("GenW_mass",     "W's mass",        30, 50  , 110  , Weh.mass()    , theWeight);
+  theHistograms.fill("GenW_pt",       "W's p_{t}",      130,  0  , 650  , Weh.pt()      , theWeight);
   theHistograms.fill("GenW_Y",        "W's Y",           50, -5  ,   5  , Weh.rapidity(), theWeight);
   theHistograms.fill("GenW_deltaEta", "W's #Delta#eta",  50, -6.5,   6.5, WdeltaEta     , theWeight);
   theHistograms.fill("GenW_deltaR",   "W's #DeltaR",     50, -0.5,   6.5, WdeltaR       , theWeight);
   //theHistograms.fill("GenW_eta",      "W's #eta",       50, -9, 9, Weh.eta(), theWeight);
-  theHistograms.fill("GenW_phi",      "W's #phi",       50, -4, 4, Weh.phi(), theWeight);
+  //theHistograms.fill("GenW_phi",      "W's #phi",       50, -4, 4, Weh.phi(), theWeight);
   //theHistograms.fill("GenW_deltaPhi", "W's #Delta#phi", 50, -4, 4, WdeltaPhi, theWeight);
   
   //Z histograms
-  theHistograms.fill("GenZ_charge",   "Z's charge",       5, -2.5,   2.5, Zet.charge()  , theWeight); //just to be sure...
-  theHistograms.fill("GenZ_mass",     "Z's mass",       400,  0  , 400  , Zet.mass()    , theWeight);
-  theHistograms.fill("GenZ_pt",       "Z's p_{t}",      260,  0  , 650  , Zet.pt()      , theWeight);
+  theHistograms.fill("GenZ_mass",     "Z's mass",        30, 60  , 120  , Zet.mass()    , theWeight);
+  theHistograms.fill("GenZ_pt",       "Z's p_{t}",      130,  0  , 650  , Zet.pt()      , theWeight);
   theHistograms.fill("GenZ_Y",        "Z's Y",           45, -3  ,   3  , Zet.rapidity(), theWeight);
   theHistograms.fill("GenZ_deltaEta", "Z's #Delta#eta",  30, -5  ,   5  , ZdeltaEta     , theWeight);
   theHistograms.fill("GenZ_deltaR",   "Z's #DeltaR",     20, -0.5,   5.5, ZdeltaR       , theWeight);
+  //theHistograms.fill("GenZ_charge",   "Z's charge",       5, -2.5,   2.5, Zet.charge()  , theWeight); //just to be sure...
   //theHistograms.fill("GenZ_eta",      "Z's #eta",       100, -7, 7, Zet.eta(), theWeight);
-  theHistograms.fill("GenZ_phi",      "Z's #phi",        50, -4, 4, Zet.phi(), theWeight);
+  //theHistograms.fill("GenZ_phi",      "Z's #phi",        50, -4, 4, Zet.phi(), theWeight);
   //theHistograms.fill("GenZ_deltaPhi", "Z's #Delta#phi",  50, -4, 4, ZdeltaPhi, theWeight);
   
   //W&Z histograms
-  theHistograms.fill("AllGenWZ_mass", "m 3 leptons and #nu", 536, 160  , 1500, masslllnu , theWeight);
-  theHistograms.fill("WZ_mass",       "W and Z mass",        536, 160  , 1500, WZ.mass(),  theWeight);
+  theHistograms.fill("AllGenWZ_mass", "m 3 leptons and #nu", 268, 160  , 1500, masslllnu , theWeight);
+  theHistograms.fill("WZ_mass",       "W and Z mass",        268, 160  , 1500, WZ.mass() , theWeight);
   theHistograms.fill("WZ_deltaEta",   "W and Z #Delta#eta",   50,  -9  ,    9, WZdeltaEta, theWeight);
   theHistograms.fill("WZ_deltaR",     "W and Z #Delta R",     25,  -0.5,    9, WZdeltaR  , theWeight);
   //theHistograms.fill("WZ_deltaPhi", "W and Z #Delta#phi", 50, -4, 4, WZdeltaPhi, theWeight);
@@ -318,28 +335,31 @@ void WZAnalyzer::analyze(){
   //jets, pT > 30 GeV and |eta| < 4.7
   
   cout << "\n---------------- Jets -----------------\n" << endl;
-  foreach(const Particle jet, *genJets){
-    genjets.push_back(jet);
-
+  foreach(const Particle jet, genjets){
+    
     cout << "ID: " << jet.id() << " pt: " << jet.pt() << "\t eta: " << jet.eta() << endl;
 
-    theHistograms.fill("GenJet_pt",     "p_{t} jets",  160,  0  , 400  , jet.pt()    , theWeight);
+    theHistograms.fill("GenJet_pt",     "p_{t} jets",   80,  0  , 400  , jet.pt()    , theWeight);
     theHistograms.fill("GenJet_eta",    "#eta jets",    70, -5  ,   5  , jet.eta()   , theWeight);    
     theHistograms.fill("GenJet_charge", "charge jets",   5, -2.5,   2.5, jet.charge(), theWeight);
     //theHistograms.fill("GenJet_mass",   "mass jets",   120,  0  , 120  , jet.mass()  , theWeight);
     //theHistograms.fill("GenJet_Y",   "Y jets",    70, -5, 5, jet.rapidity(), theWeight);
-    theHistograms.fill("GenJet_phi", "#phi jets", 50, -4, 4, jet.phi()     , theWeight);
+    //theHistograms.fill("GenJet_phi", "#phi jets", 50, -4, 4, jet.phi()     , theWeight);
   }
+  
+  theHistograms.fill("GenJets_number", "number of all gen jets",  10,  -0.5,  9.5, genjets.size(), theWeight);
 
-  if(genjets.size() == 2){
-    double JJdeltaEta = genjets[0].eta() - genjets[1].eta();
-    double JJdeltaPhi = physmath::deltaPhi(genjets[0].phi(), genjets[1].phi());
-    double JJdeltaR = physmath::deltaR(genjets[0], genjets[1]);
+  sort(genjets.begin(), genjets.end(), PtComparator());
 
-    theHistograms.fill("GenJets_deltaEta", "Jets #Delta#eta", 50, -9  , 9, JJdeltaEta, theWeight); 
-    theHistograms.fill("GenJets_deltaR",   "Jets #DeltaR",    25, -0.5, 9, JJdeltaR  , theWeight);
-    theHistograms.fill("GenJets_deltaPhi", "Jets #Delta#phi", 50, -4, 4, JJdeltaPhi, theWeight);  
-  }
+  double JJdeltaEta = genjets[0].eta() - genjets[1].eta();
+  double JJdeltaPhi = physmath::deltaPhi(genjets[0].phi(), genjets[1].phi());
+  double JJdeltaR = abs(physmath::deltaR(genjets[0], genjets[1]));
+  double JJmT = mT(genjets[0], genjets[1]);
+  
+  theHistograms.fill("GenJet_deltaEta", "Jets' #Delta#eta", 100, -9  ,   9, JJdeltaEta, theWeight); 
+  theHistograms.fill("GenJet_deltaR",   "Jets' #DeltaR",     25, -0.5,   9, JJdeltaR  , theWeight);
+  theHistograms.fill("GenJet_deltaPhi", "Jets' #Delta#phi",  50, -4  ,   4, JJdeltaPhi, theWeight);
+  theHistograms.fill("GenJet_mT",       "Jets' m_{T}",      200,  0  , 500, JJmT      , theWeight);
 
 
   
@@ -391,31 +411,62 @@ void WZAnalyzer::analyze(){
   if(recoZls.size() == 0){
     cout << Yellow("No pair reconstructed") << endl;
     recoZlempty++;
+    return;
   }
 
   if(recoZls.size() != 0){
-    if(recoZls.size() == 1){//no recoZls.size()=1 matches Zls.size()=2
+    if(recoZls.size() == 1){
+
+      threeelesplus++;
+
+      if(Zls.size() == 1){
+	threeelesminus++;
+
+	int recoZlsID = abs(recoZls[0].first.daughter(0).id()) + abs(recoZls[0].first.daughter(1).id()) + abs(recoZls[0].second.id());
+	int ZlsID = abs(Zls[0].first.daughter(0).id()) + abs(Zls[0].first.daughter(1).id()) + abs(Zls[0].second.id());
+	theHistograms.fill("ZZZ_recovsgen_idreco_1", "Sum recoZls' ids", 9, 31.5, 40.5, recoZlsID);
+	theHistograms.fill("ZZZ_recovsgen_idgen_1", "test4", 9, 31.5, 40.5, ZlsID);
+
+	if(recoZlsID != ZlsID){
+	  wrongrecognition++;
+	  return;
+	}
+      }
       
+      if(Zls.size() == 2){
+	threemuonsplus++;
+
+	int recoZlsID = abs(recoZls[0].first.daughter(0).id()) + abs(recoZls[0].first.daughter(1).id()) + abs(recoZls[0].second.id());
+	int ZlsID = abs(Zls[0].first.daughter(0).id()) + abs(Zls[0].first.daughter(1).id()) + abs(Zls[0].second.id());
+	theHistograms.fill("ZZZ_recovsgen_idreco_2", "Sum recoZls' ids", 9, 31.5, 40.5, recoZlsID);
+	theHistograms.fill("ZZZ_recovsgen_idgen_2", "test4", 9, 31.5, 40.5, ZlsID);
+
+	if(recoZlsID != ZlsID){
+	  wrongrecognition++;
+	  return;
+	}
+      }
+	
       //Z vs recoZ
       recoZ = recoZls[0].first;
       double ZrecoZdeltaPt = Zet.pt() - recoZ.pt();
-      double ZrecoZdeltaR = ZdeltaR - physmath::deltaR(recoZ.daughter(0), recoZ.daughter(1));
+      double ZrecoZdeltaR = abs(ZdeltaR - physmath::deltaR(recoZ.daughter(0), recoZ.daughter(1)));
       double ZrecoZdeltaM = Zet.mass() - recoZ.mass();
       
-      theHistograms.fill("ZvsrecoZ_21_deltapt", "#Deltap_{t} between Z and recoZ", 100, -21  , 21, ZrecoZdeltaPt, theWeight);
-      theHistograms.fill("ZvsrecoZ_21_deltaR",  "#DeltaR between Z and recoZ",      60,  -0.2,  3, ZrecoZdeltaR , theWeight);
-      theHistograms.fill("ZvsrecoZ_21_deltam",  "#Deltam between Z and recoZ",      50, -30,   30, ZrecoZdeltaM , theWeight);
+      theHistograms.fill("ZvsrecoZ_21_deltapt", "#Deltap_{t} between Z and recoZ",  75, -21  , 21, ZrecoZdeltaPt, theWeight);
+      theHistograms.fill("ZvsrecoZ_21_deltaR",  "#DeltaR between Z and recoZ",      60,  -0.2,  2, ZrecoZdeltaR , theWeight);
+      theHistograms.fill("ZvsrecoZ_21_deltam",  "#Deltam between Z and recoZ",      25, -40,   40, ZrecoZdeltaM , theWeight);
       
       //W vs recoW
       recoW = BosonLepton(recoZls[0].second, MET, copysign(24, recoZls[0].second.charge()));
       double WrecoWdeltaPt = Weh.pt() - recoW.pt();
-      double WrecoWdeltaR = WdeltaR - physmath::deltaR(recoW.daughter(0), recoW.daughter(1));
+      double WrecoWdeltaR = abs(WdeltaR - physmath::deltaR(recoW.daughter(0), recoW.daughter(1)));
       double WrecoWdeltaPhi = WdeltaPhi - physmath::deltaPhi(recoW.daughter(0).phi(), recoW.daughter(1).phi());
       double WrecoWdeltaM = Weh.mass() - recoW.mass();
       
-      theHistograms.fill("WvsrecoW_21_deltapt",  "#Deltap_{t} between W and recoW", 75, -11  , 11, WrecoWdeltaPt , theWeight);
+      theHistograms.fill("WvsrecoW_21_deltapt",  "#Deltap_{t} between W and recoW", 75, -21  , 21, WrecoWdeltaPt , theWeight);
       theHistograms.fill("WvsrecoW_21_deltaR",   "#DeltaR between W and recoW",     50,  -0.2,  3, WrecoWdeltaR  , theWeight);
-      theHistograms.fill("WvsrecoW_21_deltam",   "#Deltam between W and recoW",     50, -30  , 30, WrecoWdeltaM  , theWeight);
+      theHistograms.fill("WvsrecoW_21_deltam",   "#Deltam between W and recoW",     25, -40  , 40, WrecoWdeltaM  , theWeight);
       theHistograms.fill("WvsrecoW_21_deltaphi", "#Delta#phi between W and recoW",  50,  -3  ,  3, WrecoWdeltaPhi, theWeight);
     }
     
@@ -433,28 +484,28 @@ void WZAnalyzer::analyze(){
       sort(recoZls.begin(), recoZls.end(), ZlDeltaRComparator(ZdeltaR));
       ZbydeltaR = recoZls[0].first;
 
-      ZRdeltaR = physmath::deltaR(Zet, ZbydeltaR);
+      ZRdeltaR = abs(physmath::deltaR(Zet, ZbydeltaR));
       ZRdeltaM = Zet.mass() - ZbydeltaR.mass();
 
-      theHistograms.fill("ZvsrecoZ_3R_deltaR", "#DeltaR between Z and recoZ by #DeltaR", 50,  -2,  2, ZRdeltaR, theWeight);
-      theHistograms.fill("ZvsrecoZ_3R_deltaM", "#Deltam between Z and recoZ by #DeltaR", 50, -30, 30, ZRdeltaM, theWeight);
+      theHistograms.fill("ZvsrecoZ_3R_deltaR", "#DeltaR between Z and recoZ by #DeltaR", 50,   0,  2, ZRdeltaR, theWeight);
+      theHistograms.fill("ZvsrecoZ_3R_deltaM", "#Deltam between Z and recoZ by #DeltaR", 25, -40, 40, ZRdeltaM, theWeight);
 
       //choosing recoZ by deltaM most similar to genZ's
       sort(recoZls.begin(), recoZls.end(), ZlMassComparator(ZMASS));
       ZbydeltaM = recoZls[0].first;
 
-      ZMdeltaR = physmath::deltaR(Zet, ZbydeltaM);
+      ZMdeltaR = abs(physmath::deltaR(Zet, ZbydeltaM));
       ZMdeltaM = Zet.mass() - ZbydeltaM.mass();
 
-      theHistograms.fill("ZvsrecoZ_3M_deltaR", "#DeltaR between Z and recoZ by #Deltam", 50,  -2,  2, ZMdeltaR, theWeight);
-      theHistograms.fill("ZvsrecoZ_3M_deltaM", "#Deltam between Z and recoZ by #Deltam", 50, -30, 30, ZMdeltaM, theWeight);
+      theHistograms.fill("ZvsrecoZ_3M_deltaR", "#DeltaR between Z and recoZ by #Deltam", 50,   0,  2, ZMdeltaR, theWeight);
+      theHistograms.fill("ZvsrecoZ_3M_deltaM", "#Deltam between Z and recoZ by #Deltam", 25, -40, 40, ZMdeltaM, theWeight);
       
       //W vs recoW
       //recoW = BosonLepton(recoZls[0].second, MET, copysign(24, recoZls[0].second.charge()));
     }
     
-    if(recoZls.size() == 4){
-      threemuonsplus++;
+    if(recoZls.size() >= 3){
+      threemuonsminus++;
        /*
       sort(recoZls.begin(), recoZls.end(), ZlDeltaRComparator(ZdeltaR));
 
@@ -468,6 +519,7 @@ void WZAnalyzer::analyze(){
       }while(physmath::deltaR(recoZls[j-1].first.daughter(0), recoZls[j-1].first.daughter(1)) == physmath::deltaR(recoZls[j].first.daughter(0), recoZls[j].first.daughter(1)));
        */
     }
+    
     /*
     double recoZdeltaEta = recoZ.daughter(0).eta() - recoZ.daughter(1).eta();
     //double recoZdeltaPhi = physmath::deltaPhi(recoZ.daughter(0), recoZ.daughter(1));
@@ -501,7 +553,19 @@ void WZAnalyzer::analyze(){
     //theHistograms.fill("recoJet_phi",    "Jets' #phi",    50,  -4  ,   4  , jet.phi()     , theWeight);
   }
 
-  theHistograms.fill("recoJet_number", "Jets' number", 4, -0.5, 3.5, recoJets.size(), theWeight);    
+  theHistograms.fill("recoJet_number", "Jets' number", 10, -0.5, 9.5, recoJets.size(), theWeight);
+
+  sort(recoJets.begin(), recoJets.end(), PtComparator());
+
+  if(recoJets.size() >= 2){
+    double recoJJdeltaEta = recoJets[0].eta() - recoJets[1].eta();
+    double recoJJdeltaPhi = physmath::deltaPhi(recoJets[0].phi(), recoJets[1].phi());
+    double recoJJdeltaR = abs(physmath::deltaR(recoJets[0], recoJets[1]));
+
+    theHistograms.fill("recoJets_deltaEta", "Jets #Delta#eta", 100, -9  , 9, recoJJdeltaEta, theWeight); 
+    theHistograms.fill("recoJets_deltaR",   "Jets #DeltaR",     25, -0.5, 9, recoJJdeltaR  , theWeight);
+    theHistograms.fill("recoJets_deltaPhi", "Jets #Delta#phi",  50, -4,   4, recoJJdeltaPhi, theWeight);  
+  }  
 }
   
 void WZAnalyzer::end(TFile &){
@@ -513,13 +577,14 @@ void WZAnalyzer::end(TFile &){
   cout << "Number of events useful for Wlllnu and WZ analysis:             " << setw(7) << totalevent << endl;
   cout << "Number of events useful for WZ analysis:                        " << setw(7) << WZevent << endl;
   cout << "Number of events with recoZls empty:                            " << setw(7) << recoZlempty << endl;
+  cout << "Number of recoZls' ids != Zls' ids:                             " << setw(7) << wrongrecognition << endl;
   
-  /*
-  cout << "\nNumber of " << setw(7) << threeelesplus << endl;
-  cout << "Number of " << setw(7) << threeelesminus << endl;
-  cout << "Number of " << setw(7) << threemuonsplus << endl;
-  cout << "Number of " << setw(7) << threemuonsminus << endl;
-  */
+  // /*
+  cout << "\nNumber of recoZls.size() = 1                        " << setw(7) << threeelesplus << endl;
+  cout << "Number of recoZls.size() = 1 matching Zls.size() = 1" << setw(7) << threeelesminus << endl;
+  cout << "Number of recoZls.size() = 1 matching Zls.size() = 2" << setw(7) << threemuonsplus << endl;
+  cout << "Number of recoZls.size() >= 3                       " << setw(7) << threemuonsminus << endl;
+  // */
   
   // execution time
   endtime = ((float)clock())/CLOCKS_PER_SEC;
