39b31e777ca74127a8fb502236c4433d96911b6f
Riccardo <riccardo.bellan@cern.ch>
2018-05-14 16:51:29 +0200

    start new workflow

--- Producers/python/analyzer_ZZjj.py
+++ Producers/python/analyzer_ZZjj.py
@@ -1,49 +1,13 @@
 ### ----------------------------------------------------------------------
 ### Based on ZZ->4l strategy.
 ###----------------------------------------------------------------------
-from ZZAnalysis.AnalysisStep.defaults import declareDefault
-
-SIGNALDEFINITION = int('101',2)  # -1 means get everything, 1 means the request of having a HZZ pair with the  mass in the chosen windows. 11 means the request of having a ZZ pair with the  mass in the chosen windows. For other topology see the README under VVXAnalysis/Commons.
-
-declareDefault("PD","",globals())
-declareDefault("XSEC",-1.,globals())
-declareDefault("MCFILTER","",globals())
-declareDefault("SKIM_REQUIRED",True,globals())
-declareDefault("KINREFIT", False, globals())
-declareDefault("BESTCANDCOMPARATOR", "byBestZ1bestZ2", globals())
-declareDefault("APPLYTRIG", True, globals()) 
-declareDefault("VVMODE", 1, globals())
-declareDefault("VVDECAYMODE", 0, globals())
-declareDefault("ADDLHEKINEMATICS", False, globals())
-
-# Get absolute path
-import os
-PyFilePath = os.environ['CMSSW_BASE'] + "/src/ZZAnalysis/AnalysisStep/test/"
-
-### ----------------------------------------------------------------------
-### Standard sequence
-### ----------------------------------------------------------------------
-
-execfile(PyFilePath + "MasterPy/ZZ4lAnalysis.py")         # 2012 reference analysis
-
-### ----------------------------------------------------------------------
-### Replace parameters
-### ----------------------------------------------------------------------
-SkimPaths.append("preselection")
-
-### ----------------------------------------------------------------------
-### Output root file
-### ----------------------------------------------------------------------
-
-process.TFileService=cms.Service('TFileService', fileName=cms.string('ZZ4lAnalysis.root'))
-
-
 #####################################################################################################################################################
 
 
 ### ------------------------------------------------------------------------- ###
 ### Activate some skimming and cleaning of the collections
 ### ------------------------------------------------------------------------- ###
+SkimPaths.append("preselection")
 
 
 ### ......................................................................... ###
@@ -341,90 +305,3 @@ process.srCounter    = cms.EDProducer("SelectedEventCountProducer", names = cms.
 
 
 
-### ------------------------------------------------------------------------- ###
-### Fill the tree for the analysis
-### ------------------------------------------------------------------------- ###
-
- 
-process.treePlanter = cms.EDAnalyzer("TreePlanter",
-                                     sampleName   = cms.string(SAMPLENAME),
-                                     setup        = cms.int32(LEPTON_SETUP),
-                                     sampleType   = cms.int32(SAMPLE_TYPE),
-                                     PD           = cms.string(PD),
-                                     skimPaths    = cms.vstring(SkimPaths),
-                                     SkimRequired = cms.untracked.bool(SKIM_REQUIRED),
-                                     MCFilterPath = cms.string(MCFILTER),
-                                     isMC         = cms.untracked.bool(IsMC),
-                                     VVMode = cms.int32(int(VVMODE)),
-                                     VVDecayMode = cms.int32(int(VVDECAYMODE)),
-                                     signalDefinition = cms.int32(SIGNALDEFINITION),
-                                     JetAlgo        = cms.string("AK4PFchs"),        
-                                     jetResFile_pt  = cms.FileInPath('JRDatabase/textFiles/Fall15_25nsV2_MC/Fall15_25nsV2_MC_PtResolution_AK4PFchs.txt'),
-                                     jetResFile_phi = cms.FileInPath('JRDatabase/textFiles/Fall15_25nsV2_MC/Fall15_25nsV2_MC_PhiResolution_AK4PFchs.txt'),
-                                     jetResFile_SF  = cms.FileInPath('JRDatabase/textFiles/Fall15_25nsV2_MC/Fall15_25nsV2_MC_SF_AK4PFchs.txt'),
-                                     muons        = cms.InputTag("postCleaningMuons"),     # all good isolated muons BUT the ones coming from ZZ decay
-                                     electrons    = cms.InputTag("postCleaningElectrons"), # all good isolated electrons BUT the ones coming from ZZ decay
-                                     jets         = cms.InputTag("disambiguatedJets"),     # jets which do not contains leptons from ZZ or other good isolated leptons are removed
-                                     Vhad         = cms.InputTag("VhadCand"),
-                                     ZZ           = cms.InputTag("ZZFiltered"),            # only the best ZZ->4l candidate that pass the FULL selection
-                                     ZL           = cms.InputTag("ZlCand"),
-                                     MET          = cms.InputTag("slimmedMETs"),
-                                     Vertices     = cms.InputTag("goodPrimaryVertices"),                                    
-                                     XSection     = cms.untracked.double(XSEC)
-     )
-
-
-### ------------------------------------------------------------------------- ###
-### Run the TreePlanter
-### ------------------------------------------------------------------------- ###
-
-process.filltrees = cms.EndPath(cms.ignore(process.zzTrigger) + process.srCounter + process.cr2P2FCounter + process.cr3P1FCounter + process.treePlanter)
-
-########################################################################################################################################################################
-
-########################################################
-### Tools for further debuging                       ###
-########################################################
-
-process.printTree = cms.EDAnalyzer("ParticleListDrawer",
-                                   maxEventsToPrint = cms.untracked.int32(-1),
-                                   printVertex = cms.untracked.bool(False),
-                                   src = cms.InputTag("prunedGenParticles")
-                                   )
-
-
-process.dumpUserData =  cms.EDAnalyzer("dumpUserData",
-     dumpTrigger = cms.untracked.bool(True),
-
-     muonSrcs =  cms.PSet(
-        slimmedMuons = cms.InputTag("slimmedMuons"),
-        calibratedMuons   = cms.InputTag("calibratedMuons"),
-        muons        = cms.InputTag("appendPhotons:muons"),
-        postCleaningMuons  = cms.InputTag("postCleaningMuons")
-     ),
-
-     electronSrcs = cms.PSet(
-        slimmedElectron        = cms.InputTag("slimmedElectrons"),
-        calibratedPatElectrons = cms.InputTag("calibratedPatElectrons"),
-        electrons              = cms.InputTag("appendPhotons:electrons"),
-        postCleaningElectrons  = cms.InputTag("postCleaningElectrons")
-     ),
-     candidateSrcs = cms.PSet(
-        Z   = cms.InputTag("ZCand"),
-        ZZ  = cms.InputTag("ZZCand"),
-        ZLL = cms.InputTag("ZLLCand"),  
-        ZL  = cms.InputTag("ZlCand") 
-        ),
-      jetSrc = cms.PSet(
-        cleanJets          = cms.InputTag("cleanJets"),
-        JetsWithLeptonsRemover = cms.InputTag("JetsWithLeptonsRemover"),
-        disambiguatedJets = cms.InputTag("disambiguatedJets")
-        )
-)
-
-
-#process.filltrees = cms.Path(process.preselection * process.genCategory * process.treePlanter * process.printTree)
-#process.filltrees = cms.EndPath(process.treePlanter *process.dumpUserData)
-#process.filltrees = cms.EndPath(process.treePlanter *process.printTree)
-
-########################################################
--- Producers/test/analyzer_2016.py
+++ Producers/test/analyzer_2016.py
@@ -16,7 +16,7 @@ import os
 
 
 PyFilePath = os.environ['CMSSW_BASE'] + "/src/VVXAnalysis/Producers/python/"
-execfile(PyFilePath + "analyzer_ZZjj.py")
+execfile(PyFilePath + "analyzer_VVjj.py")
 
 ### ----------------------------------------------------------------------
 
@@ -33,7 +33,7 @@ process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(
 #'/store/mc/RunIISpring16MiniAODv2/WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/60000/38BB4913-A21B-E611-80A1-005056021116.root'
 #'/store/mc/RunIISpring16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/40000/00200284-F15C-E611-AA9B-002590574776.root'
 #'/store/mc/RunIISpring16MiniAODv2/ZZJJTo4L_EWK_13TeV-madgraph-pythia8/MINIAODSIM/premix_withHLT_80X_mcRun2_asymptotic_v14-v1/80000/403C4E51-2A72-E611-B589-002590DE7230.root'
-'/store/mc/RunIISpring16MiniAODv2/ZZTo4L_13TeV_powheg_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/00000/0AA68890-7F1A-E611-B8B0-0CC47A7FC6E6.root'
+#'/store/mc/RunIISpring16MiniAODv2/ZZTo4L_13TeV_powheg_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/00000/0AA68890-7F1A-E611-B8B0-0CC47A7FC6E6.root'
 #'/store/mc/RunIISpring16MiniAODv2/VBFToHiggs0PMContinToZZTo2e2muJJ_M125_GaSM_13TeV_phantom_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/20000/021115D1-983F-E611-A4B2-0CC47A4D768E.root'
 #'/store/mc/RunIISpring16MiniAODv2/GluGluToHiggs0MContinToZZTo2e2mu_M125_10GaSM_13TeV_MCFM701_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14-v1/50000/02D23032-EE5B-E611-8E99-A0369F301924.root'
 #'/store/mc/RunIISpring16MiniAODv1/GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v4/10000/0419BFA2-8C3E-E611-8C5E-00259021A0E2.root'
@@ -45,5 +45,9 @@ process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(
 # '/store/mc/RunIISpring16MiniAODv1/VBFToContinZZTo4eJJ_13TeV_phantom_pythia8/MINIAODSIM/PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_v3-v1/10000/1431B7BB-6D33-E611-B3BC-0017A4770C00.root'
 # '/store/mc/RunIISpring16MiniAODv1/ZZTo4L_13TeV_powheg_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/00000/046E1689-FD0D-E611-BE90-002590791D60.root'
 # '/store/mc/RunIISpring16MiniAODv2/WZTo3LNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/00000/0CD58AA0-D12A-E611-850E-00266CFCCD94.root'
+
+'/store/mc/RunIISummer16MiniAODv2/WZTo3LNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/288A3E54-DFBB-E611-9906-1418774117C7.root'
+
+
 ))
-process.maxEvents.input = -1
+process.maxEvents.input = 100
