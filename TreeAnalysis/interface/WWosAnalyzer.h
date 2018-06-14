5a6479cde5352ae391c32e09775618e43021bea6
Alberto <alberto.mecca@cern.ch>
2018-06-14 16:21:49 +0200

    New WW os class

---
+++ TreeAnalysis/interface/WWosAnalyzer.h
@@ -0,0 +1,69 @@
+#ifndef WWsoAnalyzer_h
+#define WWsoAnalyzer_h
+
+/** \class WWsoAnalyzer
+ *  Concrete class for VVX analysis
+ *
+ *  $Date: 2013/03/15 13:37:31 $
+ *  $Revision: 1.4 $
+ *  \author R. Bellan - UNITO <riccardo.bellan@cern.ch>
+ */
+
+
+#include "EventAnalyzer.h"
+#include "RegistrableAnalysis.h"
+#include "VVXAnalysis/Commons/interface/Constants.h"
+#include "VVXAnalysis/Commons/interface/LeptonScaleFactors.h"
+
+class WWsoAnalyzer: public EventAnalyzer, RegistrableAnalysis<WWsoAnalyzer>{
+
+public:
+
+  //, const std::string& filename, const double& lumi = 1., const double& externalXSection = -1., bool doBasicPlots = false
+
+ WWosAnalyzer(const AnalysisConfiguration& configuration)
+   : EventAnalyzer(*(new Selector<WWsoAnalyzer>(*this)), 
+		   configuration){
+    //theHistograms.profile(genCategory);
+  }
+
+  virtual ~WWsoAnalyzer(){}
+
+  virtual void analyze();
+  
+  virtual Int_t cut();
+
+
+ private:
+  friend class Selector<WWsoAnalyzer>;
+  template< class PAR >
+    bool ZBosonDefinition(phys::Boson<PAR> cand) const{
+    bool checkCharge = cand.daughter(0).charge() + cand.daughter(1).charge() == 0;
+    return checkCharge && fabs(cand.p4().M() - phys::ZMASS) < 30;
+  }
+
+
+  template< class PAR >
+    bool WBosonDefinition(phys::Boson<PAR> cand) {
+
+    bool gooddaughters = false;
+    if(fabs(cand.daughter(0).eta()) < 2.5 && cand.daughter(0).pt() > 30 &&
+       cand.daughter(0).passPUID() && cand.daughter(0).passLooseJetID() &&
+       fabs(cand.daughter(1).eta()) < 2.5 && cand.daughter(1).pt() > 30 &&
+       cand.daughter(1).passPUID() && cand.daughter(1).passLooseJetID())
+      gooddaughters = true;
+
+    if(fabs(cand.p4().M() - phys::WMASS) < 150 && gooddaughters)
+      return true;
+    return false;
+
+  }
+
+
+
+};
+#endif
+
+
+/*  LocalWords:  WWosAnalyzer
+ */
---
+++ TreeAnalysis/src/WWosAnalyzer.cc
@@ -0,0 +1,35 @@
+#include "VVXAnalysis/TreeAnalysis/interface/WWosAnalyzer.h"
+#include "VVXAnalysis/Commons/interface/SignalDefinitions.h"
+#include "VVXAnalysis/Commons/interface/Utilities.h"
+
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+#include <boost/assign/std/vector.hpp> 
+using namespace boost::assign;
+
+using std::cout;
+using std::endl;
+
+
+using namespace phys;
+
+Int_t WWosAnalyzer::cut() {
+  return 1;
+}
+
+
+
+void WWosAnalyzer::analyze(){
+
+  cout << "------------------------------------------------------------------"<<endl;
+  cout << "Run: " << run << " event: " << event << endl;
+  
+  foreach(const phys::Particle &gen, *genParticles){
+    if(abs(gen.id()) != 11 && abs(gen.id()) != 13 || (!(gen.genStatusFlags().test(phys::GenStatusBit::isPrompt)) || !(gen.genStatusFlags().test(phys::GenStatusBit::fromHardProcess)))) continue;
+       cout << "id: " << gen.id() << " pt: " << gen.pt() << endl;
+       theHistograms.fill("ptAllGenParticle","pt ", 100, 0, 100, gen.pt());
+  }
+  
+}
+  
