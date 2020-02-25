d5564d8bf3759e3941dac1af2ef852f8291d5a4e
amecca <alberto.mecca@edu.unito.it>
2020-02-25 19:09:34 +0100

    Added an overload of deltaR for TLorentzVectors

--- Commons/interface/Utilities.h
+++ Commons/interface/Utilities.h
@@ -17,12 +17,17 @@ namespace physmath{
     return deltaPhi(a.Phi(),b.Phi());
   }
     
-
+	
   template<typename T1, typename T2> double deltaR(const T1& p1, const T2& p2){
     return sqrt( deltaPhi(p1.phi(),p2.phi())*deltaPhi(p1.phi(),p2.phi()) +
 		 (p1.eta()-p2.eta())*(p1.eta()-p2.eta()) );
   }
-
+	
+	inline double deltaR(const TLorentzVector &a, const TLorentzVector &b){
+		//Overloads the template deltaR<T1,T2>(...). Created for deltaRComparator in "Comparators.h"
+		double dPhi = deltaPhi(a.Phi(), b.Phi());
+		return sqrt( dPhi*dPhi + (a.Eta() - b.Eta())*(a.Eta() - b.Eta()) );
+	}
   
   template<typename T> bool isAlmostEqual(const T& a, const T& b, const double &tollerance = 0.0001){
     if      (a != 0) return fabs(a-b)/a < tollerance;
