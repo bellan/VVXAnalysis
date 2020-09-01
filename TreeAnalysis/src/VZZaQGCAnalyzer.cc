9dfc02cef7a2ecf56ddfab79849ba561333675d3
Giovanni <giovanni.marozzo@gmail.com>
2020-09-01 15:37:21 +0200

    fixed more/less energetic leptons

--- TreeAnalysis/src/VZZaQGCAnalyzer.cc
+++ TreeAnalysis/src/VZZaQGCAnalyzer.cc
@@ -22,7 +22,7 @@ Int_t VZZaQGCAnalyzer::cut() {
 
 void VZZaQGCAnalyzer::analyze(){
    foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
-    if((genVBParticle.daughter(0).id()==11&&genVBParticle.daughter(1).id()==-11)||(genVBParticle.daughter(0).id()==13&&genVBParticle.daughter(1).id()==-13)){
+     if((genVBParticle.daughter(0).id()==11&&genVBParticle.daughter(1).id()==-11)||(genVBParticle.daughter(0).id()==13&&genVBParticle.daughter(1).id()==-13)){
       theHistograms.fill("massa bosoni generati","Massa bosoni generati",180,50,130,genVBParticle.mass());
       theHistograms.fill("pt bosoni generati","Pt bosoni generati",150,0,900,genVBParticle.pt());
       theHistograms.fill("theta bosoni generati","Theta bosoni generati",75 ,0,3.5,genVBParticle.p4().Theta());
@@ -30,10 +30,10 @@ void VZZaQGCAnalyzer::analyze(){
       if(genVBParticle.daughter(1).e()>genVBParticle.daughter(0).e()){
 	theHistograms.fill("E leptone maggiore","Energia leptone piu' energetico",200,0,2000,genVBParticle.daughter(1).e());}
       else{theHistograms.fill("E leptone maggiore","Energia leptone piu' energetico",200,0,2000,genVBParticle.daughter(0).e());};
-	}
       if(genVBParticle.daughter(1).e()>genVBParticle.daughter(0).e()){
 	theHistograms.fill("E leptone minore","Energia leptone meno energetico",200,0,800,genVBParticle.daughter(0).e());}
       else{theHistograms.fill("E leptone minore","Energia leptone meno energetico",200,0,800,genVBParticle.daughter(1).e());};
-  }
- 
+     }
+   }
 }
+ 
