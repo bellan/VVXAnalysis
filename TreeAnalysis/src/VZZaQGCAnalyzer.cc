ec87aa655e16390eb2f26599a051ce42164868ee
Giovanni <giovanni.marozzo@gmail.com>
2020-09-01 15:26:28 +0200

    removed pointless analysis, added more/less energetic leptons

--- TreeAnalysis/src/VZZaQGCAnalyzer.cc
+++ TreeAnalysis/src/VZZaQGCAnalyzer.cc
@@ -21,19 +21,19 @@ Int_t VZZaQGCAnalyzer::cut() {
 
 
 void VZZaQGCAnalyzer::analyze(){
-
- cout << "------------------------------------------------------------------"<<endl;
-  cout << "Run: " << run << " event: " << event << endl;
-
-  cout << "# muons: " << muons->size() <<endl;
-
-  theHistograms.fill("nmuons", "Number of muons per event", 6,0,6, muons->size());
-  foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
+   foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
     if((genVBParticle.daughter(0).id()==11&&genVBParticle.daughter(1).id()==-11)||(genVBParticle.daughter(0).id()==13&&genVBParticle.daughter(1).id()==-13)){
       theHistograms.fill("massa bosoni generati","Massa bosoni generati",180,50,130,genVBParticle.mass());
       theHistograms.fill("pt bosoni generati","Pt bosoni generati",150,0,900,genVBParticle.pt());
       theHistograms.fill("theta bosoni generati","Theta bosoni generati",75 ,0,3.5,genVBParticle.p4().Theta());
       theHistograms.fill("energia bosoni generati","Energia bosoni generati",135,0,2700,genVBParticle.e());
-	}}
+      if(genVBParticle.daughter(1).e()>genVBParticle.daughter(0).e()){
+	theHistograms.fill("E leptone maggiore","Energia leptone piu' energetico",200,0,2000,genVBParticle.daughter(1).e());}
+      else{theHistograms.fill("E leptone maggiore","Energia leptone piu' energetico",200,0,2000,genVBParticle.daughter(0).e());};
+	}
+      if(genVBParticle.daughter(1).e()>genVBParticle.daughter(0).e()){
+	theHistograms.fill("E leptone minore","Energia leptone meno energetico",200,0,800,genVBParticle.daughter(0).e());}
+      else{theHistograms.fill("E leptone minore","Energia leptone meno energetico",200,0,800,genVBParticle.daughter(1).e());};
+  }
  
 }
