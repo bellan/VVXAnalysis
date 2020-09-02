980df9fd42dc1cac6e595bed76a82818322fc979
Giovanni <giovanni.marozzo@gmail.com>
2020-09-02 16:03:25 +0200

    added partial reconstructed diboson mass

--- TreeAnalysis/src/VZZaQGCAnalyzer.cc
+++ TreeAnalysis/src/VZZaQGCAnalyzer.cc
@@ -19,6 +19,7 @@ Int_t VZZaQGCAnalyzer::cut() {
   return 1;
 }
 
+double a,b,c,d,mass,a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4,d1,d2,d3,d4,eta1,eta2,eta3,eta4,phi1,phi2,phi3,phi4,mass1,mass2;
 
 void VZZaQGCAnalyzer::analyze(){
    foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
@@ -35,5 +36,24 @@ void VZZaQGCAnalyzer::analyze(){
       else{theHistograms.fill("E leptone minore","Energia leptone meno energetico",200,0,800,genVBParticle.daughter(1).e());};
      }
    }
-}
+   theHistograms.fill("numero bosoni","Numero bosoni",5,0,5,genVBParticles->size());
+   if(genVBParticles->size()==2){
+     a=0;
+     b=0;
+     c=0;
+     d=0;
+     mass=0;
+      foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
+	a+=genVBParticle.p4().Px();
+	b+=genVBParticle.p4().Py();
+        c+=genVBParticle.p4().Pz();
+	d+=genVBParticle.p4().E();}
+      mass=sqrt((d*d)-(a*a)-(b*b)-(c*c));
+      theHistograms.fill("massa dibosoni generati","Massa dibosoni generati",90,80,440,mass);
+   }
+  
+   }
+
+     
+
  
