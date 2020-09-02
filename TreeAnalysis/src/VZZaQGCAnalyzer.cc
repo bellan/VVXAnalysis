e14ee9511f9acd3db202d850c58e7c6a53145a78
Giovanni <giovanni.marozzo@gmail.com>
2020-09-02 16:22:24 +0200

    finished reconstructed diboson mass

--- TreeAnalysis/src/VZZaQGCAnalyzer.cc
+++ TreeAnalysis/src/VZZaQGCAnalyzer.cc
@@ -20,6 +20,7 @@ Int_t VZZaQGCAnalyzer::cut() {
 }
 
 double a,b,c,d,mass,a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4,d1,d2,d3,d4,eta1,eta2,eta3,eta4,phi1,phi2,phi3,phi4,mass1,mass2;
+int good;
 
 void VZZaQGCAnalyzer::analyze(){
    foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
@@ -38,19 +39,90 @@ void VZZaQGCAnalyzer::analyze(){
    }
    theHistograms.fill("numero bosoni","Numero bosoni",5,0,5,genVBParticles->size());
    if(genVBParticles->size()==2){
-     a=0;
-     b=0;
-     c=0;
-     d=0;
-     mass=0;
+     good=0;
+     foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
+	  if((genVBParticle.daughter(0).id()==11&&genVBParticle.daughter(1).id()==-11)||(genVBParticle.daughter(0).id()==13&&genVBParticle.daughter(1).id()==-13)){
+	    good+=1;}
+	  if(good==2){
+     a=b=c=d=mass=0;
       foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
 	a+=genVBParticle.p4().Px();
 	b+=genVBParticle.p4().Py();
         c+=genVBParticle.p4().Pz();
 	d+=genVBParticle.p4().E();}
       mass=sqrt((d*d)-(a*a)-(b*b)-(c*c));
-      theHistograms.fill("massa dibosoni generati","Massa dibosoni generati",90,80,440,mass);
-   }
+      theHistograms.fill("massa dibosoni generati","Massa dibosoni generati",110,80,520,mass);}
+     }}
+   if(genVBParticles->size()==4){
+      foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
+	  if((genVBParticle.daughter(0).id()==11&&genVBParticle.daughter(1).id()==-11)||(genVBParticle.daughter(0).id()==13&&genVBParticle.daughter(1).id()==-13)){
+	    good+=1;}
+	  if(good==4){
+     a1=a2=a3=a4=b1=b2=b3=b4=c1=c2=c3=c4=d1=d2=d3=d4=eta1=eta2=eta3=eta4=phi1=phi2=phi3=phi4=mass1=mass2=0;
+     foreach(const phys::Boson<phys::Particle> genVBParticle,*genVBParticles){
+       if(a1==0){
+	 a1=genVBParticle.p4().Px();
+	 b1=genVBParticle.p4().Py();
+         c1=genVBParticle.p4().Pz();
+	 d1=genVBParticle.p4().E();
+	 eta1=genVBParticle.p4().Theta();
+	 phi1=genVBParticle.p4().Phi();}
+       else{if(a2==0){
+	 a2=genVBParticle.p4().Px();
+	 b2=genVBParticle.p4().Py();
+         c2=genVBParticle.p4().Pz();
+	 d2=genVBParticle.p4().E();
+	 eta2=genVBParticle.p4().Theta();
+	 phi2=genVBParticle.p4().Phi();
+	 }
+	 else{if(a3==0){
+	   if(physmath::deltaPhi(genVBParticle.p4().Phi(),phi1)<physmath::deltaPhi(phi2,phi1)){
+	     a3=a2;
+	     b3=b2;
+	     c3=c2;
+	     d3=d2;
+	     eta3=eta2;
+	     phi3=phi2;
+	     a2=genVBParticle.p4().Px();
+	     b2=genVBParticle.p4().Py();
+             c2=genVBParticle.p4().Pz();
+	     d2=genVBParticle.p4().E();
+	     eta2=genVBParticle.p4().Theta();
+	     phi2=genVBParticle.p4().Phi();}
+	   else{a3=genVBParticle.p4().Px();
+	        b3=genVBParticle.p4().Py();
+                c3=genVBParticle.p4().Pz();
+	        d3=genVBParticle.p4().E();
+	        eta3=genVBParticle.p4().Theta();
+	        phi3=genVBParticle.p4().Phi();}
+	 }
+	   else{if(physmath::deltaPhi(genVBParticle.p4().Phi(),phi1)<physmath::deltaPhi(phi2,phi1)){
+	       a4=a2;
+	       b4=b2;
+	       c4=c2;
+	       d4=d2;
+	       eta4=eta2;
+	       phi4=phi2;
+	       a2=genVBParticle.p4().Px();
+	       b2=genVBParticle.p4().Py();
+               c2=genVBParticle.p4().Pz();
+	       d2=genVBParticle.p4().E();
+	       eta2=genVBParticle.p4().Theta();
+	       phi2=genVBParticle.p4().Phi();}
+	     else{a4=genVBParticle.p4().Px();
+	       b4=genVBParticle.p4().Py();
+               c4=genVBParticle.p4().Pz();
+	       d4=genVBParticle.p4().E();
+	       eta4=genVBParticle.p4().Theta();
+	       phi4=genVBParticle.p4().Phi();}
+	   }
+	 }
+       }
+     }
+       mass1=sqrt((d1+d2)*(d1+d2)-(a1+a2)*(a1+a2)-(b1+b2)*(b1+b2)-(c1+c2)*(c1+c2));
+       mass2=sqrt((d3+d4)*(d3+d4)-(a3+a4)*(a3+a4)-(b3+b4)*(b3+b4)-(c3+c4)*(c3+c4));
+       theHistograms.fill("massa dibosoni generati","Massa dibosoni generati",110,80,520,mass1);
+       theHistograms.fill("massa dibosoni generati","Massa dibosoni generati",110,80,520,mass2);}}}
   
    }
 
