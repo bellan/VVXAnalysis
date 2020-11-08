34a7908ac19f7d53da0bae682347f25a672aebd0
Giovanni <giovanni.marozzo@gmail.com>
2020-11-08 15:42:44 +0100

    fixed distribution sum

--- TreeAnalysis/macros/VZZDistributionComparison.C
+++ TreeAnalysis/macros/VZZDistributionComparison.C
@@ -15,21 +15,13 @@ double scale(double crosssec){return crosssec/SM_crosssec;}
 
 //cross sections for anomalous phenomena
 
-double sec1=0;
-double sec2=5.21541181034e-06;
-double sec3=0;
-double sec4=7.84718458183e-06;
-double sec5=-5.4669356329e-07;
-double sec6=6.6865180464e-05;
+double sec1=-5.4669356329e-07;
+double sec2=6.6865180464e-05;
 
 //scale factors for anomalous phenomenona
 
 double scale1=scale(sec1);
 double scale2=scale(sec2);
-double scale3=scale(sec3);
-double scale4=scale(sec4);
-double scale5=scale(sec5);
-double scale6=scale(sec6);
 
 //generates the filename input for other functions
 
@@ -50,7 +42,7 @@ TH1F* DistributionPlot(TFile *result, TLegend *legend, string histname, string t
 
 //plots the distribution that sums SM, linear and quadratic contributions
 
-  TH1F *DistributionSummer(TFile *result1, TFile *result2, double scale1, TFile *result3, double scale2, TLegend *legend, string histname, string title, string axisname, string legendname, double ymax, double colour){
+TH1F *DistributionSummer(double parameter, TFile *result1, TFile *result2, double scale1, TFile *result3, double scale2, TLegend *legend, string histname, string title, string axisname, double ymax, double colour){
   TH1F *hist1= (TH1F*)result1->Get(histname.c_str());
   TH1F *hist2= (TH1F*)result2->Get(histname.c_str());
   TH1F *hist3= (TH1F*)result3->Get(histname.c_str());
@@ -59,39 +51,42 @@ TH1F* DistributionPlot(TFile *result, TLegend *legend, string histname, string t
   hist1->GetYaxis()->SetRangeUser(0,ymax);
   hist1->GetXaxis()->SetTitle(axisname.c_str());
   hist1->SetLineWidth(2);
-  histsum->Add(hist1,hist2,1,scale1);
-  histsum->Add(histsum,hist3,1,scale2*scale2);
-  histsum->Scale(1/(1+scale1+scale2*scale2));
+  histsum->Add(hist1,hist2,1,scale1*parameter);
+  histsum->Add(histsum,hist3,1,scale2*parameter*parameter);
+  histsum->Scale(1/(1+scale1*parameter+scale2*parameter*parameter));
   histsum->SetLineColor(colour);
-  legend->AddEntry(histsum,legendname.c_str());
+  ostringstream stream;
+  stream << parameter;
+  string insert= "cW= "+stream.str();
+  legend->AddEntry(histsum,insert.c_str());
   histsum->Draw("same");
   return histsum;}
 
 //compares SM and anomalous couplings distributions for a single variable
 
-  void DistributionComparison(string histname, string title,string axisname, double ymax, TFile *result1, TFile *result2, TFile *result3){
+void DistributionComparison(double parameter, string histname, string title,string axisname, double ymax, TFile *result1, TFile *result2, TFile *result3){
+  
   TLegend *legend= new TLegend(0.75,0.75,0.98,0.95);
   TH1F *hist=DistributionPlot(result1,legend,histname,title,axisname,"SM data",ymax,kBlack);
-  /*
-  TH1F *histsum=DistributionSummer(result1, result2, scale1, result3, scale2, legend, histname, title, axisname, "cW=0.65", ymax, kMagenta);
-  TH1F *histsum=DistributionSummer(result1, result2, scale3, result3, scale4, legend, histname, title, axisname, "cW=0.8", ymax, kGreen);
-  */
-  TH1F *histsum=DistributionSummer(result1, result2, scale5, result3, scale6, legend, histname, title, axisname, "cW=1", ymax, kRed);
+  TH1F *histsum=DistributionSummer(parameter,result1, result2, scale1, result3, scale2, legend, histname, title, axisname, ymax, kRed);
 
-  double chi=hist->Chi2Test(histsum,"WWCHI2");
-  ostringstream stream1,stream2;
-  stream1 << hist->Chi2Test(histsum,"WWCHI2/NDF");
-  string insert1= "chi2/NDF: "+stream1.str();
+  ostringstream stream1,stream2,stream3;
+  stream1 << hist->Chi2Test(histsum,"WWCHI2");
+  string insert1= "chi2: "+stream1.str();
   legend->AddEntry((TObject*)0,insert1.c_str(),"");
-  stream2 << hist->Chi2Test(histsum,"WW");
-  string insert2= "p value: "+stream2.str();
+  stream2 << hist->Chi2Test(histsum,"WWCHI2/NDF");
+  string insert2= "chi2/NDF: "+stream2.str();
   legend->AddEntry((TObject*)0,insert2.c_str(),"");
+  legend->Draw();
+  stream3 << hist->Chi2Test(histsum,"WW");
+  string insert3= "p value: "+stream3.str();
+  legend->AddEntry((TObject*)0,insert3.c_str(),"");
   legend->Draw();
 }  
 
 //compares distributions for multiple variables (modifiable as needed)
 
-  void VZZDistributionComparison(string samplename1, string samplename2, string samplename3){
+void VZZDistributionComparison(double parameter, string samplename1, string samplename2, string samplename3){
     
   gErrorIgnoreLevel = kWarning; //to ignore "Info" messages
 
@@ -101,32 +96,37 @@ TH1F* DistributionPlot(TFile *result, TLegend *legend, string histname, string t
   TFile *result1 = TFile::Open(filename1.c_str());
   TFile *result2 = TFile::Open(filename2.c_str());
   TFile *result3 = TFile::Open(filename3.c_str());
-
+  /*
   TCanvas *c1 = new TCanvas("c1","canvas",0,0,1000,1000);
+  */
   TCanvas *c2 = new TCanvas("c2","canvas",0,0,1000,1000);
   TCanvas *c3 = new TCanvas("c3","canvas",0,0,1000,1000);
+  /*
   TCanvas *c4 = new TCanvas("c4","canvas",0,0,1000,1000);
   TCanvas *c5 = new TCanvas("c5","canvas",0,0,1000,1000);
   TCanvas *c6 = new TCanvas("c6","canvas",0,0,1000,1000);
   TCanvas *c7 = new TCanvas("c7","canvas",0,0,1000,1000);
   TCanvas *c8 = new TCanvas("c8","canvas",0,0,1000,1000);
-  
+
   c1->cd();
-  DistributionComparison("mass of tribosons","Triboson mass","mass (GeV/c^2)",1600,result1,result2,result3);
+  DistributionComparison(parameter,"mass of tribosons","Triboson mass","mass (GeV/c^2)",1600,result1,result2,result3);
+  */
   c2->cd();
-  DistributionComparison("mass of coupled bosons","Coupled bosons mass","mass (GeV/c^2)",2500,result1,result2,result3);
+  DistributionComparison(parameter,"mass of coupled bosons","Coupled bosons mass","mass (GeV/c^2)",2500,result1,result2,result3);
   c3->cd();
-  DistributionComparison("energy of all bosons","Total boson energy","energy (GeV)",1600,result1,result2,result3);
+  DistributionComparison(parameter,"energy of all bosons","Total boson energy","energy (GeV)",1600,result1,result2,result3);
+  /*
   c4->cd();
-  DistributionComparison("energy of major bosons","Major boson energy","energy (GeV)",1000,result1,result2,result3);
+  DistributionComparison(parameter,"energy of major bosons","Major boson energy","energy (GeV)",1000,result1,result2,result3);
   c5->cd();
-  DistributionComparison("pt of major bosons","Maximum boson pt","pt (GeV/c)",1200,result1,result2,result3);
+  DistributionComparison(parameter,"pt of major bosons","Maximum boson pt","pt (GeV/c)",1200,result1,result2,result3);
   c6->cd();
-  DistributionComparison("boson relative angle","Boson relative angle","angle (rad)",900,result1,result2,result3);
+  DistributionComparison(parameter,"boson relative angle","Boson relative angle","angle (rad)",900,result1,result2,result3);
   c7->cd();
-  DistributionComparison("energy of leptonic bosons","Leptonic boson energy","energy (GeV)",2800,result1,result2,result3);
+  DistributionComparison(parameter,"energy of leptonic bosons","Leptonic boson energy","energy (GeV)",2800,result1,result2,result3);
   c8->cd();
-  DistributionComparison("pt of leptonic bosons","Leptonic boson pt","pt (GeV/c)",3600,result1,result2,result3);
+  DistributionComparison(parameter,"pt of leptonic bosons","Leptonic boson pt","pt (GeV/c)",3600,result1,result2,result3);
+  */
 }
 
 //compares distribution of a variable (with chi squared) for two different samples
