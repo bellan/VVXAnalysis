c25afcef033c8b2ef2c3322c45024c47614948e0
Riccardo <riccardo.bellan@cern.ch>
2021-05-06 18:19:53 +0200

    add WZ cand, trigger still missing

--- Producers/python/analyzer_VVjj.py
+++ Producers/python/analyzer_VVjj.py
@@ -132,7 +132,7 @@ process.genPath = cms.Path(process.genParticlesFromHardProcess + process.selecte
 execfile(VVjj_search_path + "analyzer_ZZjj.py")
 
 ## WZ->3lnu
-#execfile(VVjj_search_path + "analyzer_WZjj.py") 
+execfile(VVjj_search_path + "analyzer_WZjj.py") 
 
 ## VZ->2l2j
 #execfile(VVjj_search_path + "analyzer_VZjj.py")
@@ -141,7 +141,6 @@ execfile(VVjj_search_path + "analyzer_ZZjj.py")
 #execfile(VVjj_search_path + "analyzer_WWjj.py")
 
 ### ----------------------------------------------------------------------
-
 
 
 
@@ -346,6 +345,7 @@ process.treePlanter = cms.EDAnalyzer("TreePlanter",
                                      Vhad         = cms.InputTag(""),
                                      ZZ           = cms.InputTag("ZZFiltered"),            # only the best ZZ->4l candidate that pass the FULL selection
                                      ZL           = cms.InputTag("ZlSelected"),
+                                     ZW           = cms.InputTag("ZWCand"),
                                      MET          = cms.InputTag("slimmedMETs"),
                                      Vertices     = cms.InputTag("goodPrimaryVertices"),                                    
                                      XSection     = cms.untracked.double(XSEC)
