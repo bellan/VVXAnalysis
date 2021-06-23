9d379193a0312377ec59e2507d4df84193191586
amecca <alberto.mecca@cern.ch>
2021-06-23 12:21:27 +0200

    Multithread hadd and merging of the various channels of gg to 4l

--- TreeAnalysis/merge1618.sh
+++ TreeAnalysis/merge1618.sh
@@ -7,16 +7,29 @@
 #  Co-author: E. Racca (eleonora.racca@cern.ch)  #
 ##################################################
 
+haddOpt="-k -j $(grep processor /proc/cpuinfo | wc -l)"
 
-exist_an_reg=$(echo $(ls results/2016) $(ls results/2017) $(ls results/2018) | uniq)
+cd results
+
+exist_an_reg=$(echo $(ls 2016) $(ls 2017) $(ls 2018) | uniq)
 #echo $exist_an_reg
 
-exist_sam=$(find results -type f -name "*.root" | grep -oP "[^/]+\.root" | grep -oP "[^\.]+(?=\.root)" | sort | uniq)
+for y in 2016 2017 2018 ; do
+	for an_reg in $exist_an_reg ; do
+		#echo "+++" $y "+++" $an_reg
+		[ -d $y/$an_reg ] || continue
+		hadd $haddOpt $y/$an_reg/gg.root $(find $y/$an_reg -type f -name "ggTo*.root")
+	done
+done
+
+exist_sam=$(find . -type f -name "*.root" | grep -oP "[^/]+\.root" | grep -oP "[^\.]+(?=\.root)" | grep -v "ggTo" | sort | uniq)
 #echo $exist_sam
 
 for an_reg in $exist_an_reg ; do
-	mkdir -p results/1618/${an_reg}
+	mkdir -p 1618/${an_reg}
 	for sam in $exist_sam ; do
-		hadd -k -f results/1618/${an_reg}/${sam}.root results/2016/${an_reg}/${sam}.root results/2017/${an_reg}/${sam}.root results/2018/${an_reg}/${sam}.root
+		hadd $haddOpt 1618/${an_reg}/${sam}.root 2016/${an_reg}/${sam}.root 2017/${an_reg}/${sam}.root 2018/${an_reg}/${sam}.root
 	done
 done
+
+cd ..
