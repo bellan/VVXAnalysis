2f5442627daef5002a7669744b25183c36a5b174
eleoracca <eleonora.racca@cern.ch>
2021-10-28 22:38:33 +0200

    update for 2018 data samples

--- TreeAnalysis/merge1618.sh
+++ TreeAnalysis/merge1618.sh
@@ -16,6 +16,10 @@ cd results
 
 
 # ~~~~~ Removing previous files
+echo "  " 
+echo "--- Removing elder files ---"
+echo "  " 
+
 rm $rmOpt 1618
 
 for y in 2016 2017 2018 ; do
@@ -23,13 +27,27 @@ for y in 2016 2017 2018 ; do
 done
 
 
-# ~~~~~ Hadd-ing results
 # array of analyses and regions names
 exist_an_reg=$(echo $(ls 2016) $(ls 2017) $(ls 2018) | tr ' ' '\n' | sort -u | tr '\n' ' ')
-#echo $exist_an_reg
 
 
+# changing names of some of the results as the original samples have different names across years despide being the same process
+for an_reg in $exist_an_reg ; do
+	if test -f 2016/$an_reg/ZZ4lJJ.root ; then
+		rm $rmOpt 2016/$an_reg/ZZJJTo4L.root
+		mv -f 2016/$an_reg/ZZ4lJJ.root 2016/$an_reg/ZZJJTo4L.root
+	fi
+	
+	if test -f 2018/$an_reg/ZZTo4lext1.root ; then
+		rm $rmOpt 2018/$an_reg/ZZTo4l.root
+		mv -f 2018/$an_reg/ZZTo4lext1.root 2018/$an_reg/ZZTo4l.root	
+	fi
+done
+
+
+# ~~~~~ Hadd-ing results
 # hadd-ing ggTo4l results and removing hadd-ed files
+echo "  " 
 echo "--- Hadd-ing gg -> ZZ results ---"
 echo "  " 
 
@@ -45,9 +63,8 @@ for y in 2016 2017 2018 ; do
 	done
 done
 
-
 # array of results file names
-exist_sam=$(find ./201*/ -type f -name "*.root" | grep -oP "[^/]+\.root" | grep -oP "[^\.]+(?=\.root)" | grep -v "ggTo" | sort | uniq)
+exist_sam=$(find ./201*/ -type f -name "*.root" | grep -oP "[^/]+\.root" | grep -oP "[^\.]+(?=\.root)" | grep -v "ggTo" | grep -v "Double" | grep -v "Single" | grep -v "Muon" | grep -v "EGamma" | grep -v "20" | sort | uniq)
 #echo $exist_sam
 
 
@@ -55,6 +72,7 @@ exist_sam=$(find ./201*/ -type f -name "*.root" | grep -oP "[^/]+\.root" | grep 
 echo "  " 
 echo "--- Hadd-ing results into 1618 ---"
 echo "  " 
+
 for an_reg in $exist_an_reg ; do
 	mkdir -p 1618/$an_reg
  
@@ -68,6 +86,7 @@ done
 echo "  " 
 echo "--- Creating CR for all years ---"
 echo "  " 
+
 for y in 2016 2017 2018 1618 ; do
 	mkdir -p $y/ZZjjAnalyzer_CR
 
