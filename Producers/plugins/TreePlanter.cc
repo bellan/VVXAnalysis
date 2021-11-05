a3ff470bd0413706921c61bcae0a26ab46ec9e1f
Riccardo <riccardo.bellan@cern.ch>
2021-11-05 16:23:27 +0100

    correct name for ph id

--- Producers/plugins/TreePlanter.cc
+++ Producers/plugins/TreePlanter.cc
@@ -730,9 +730,9 @@ phys::Photon TreePlanter::fill(const pat::Photon &photon) const {
 	phys::Photon output(phys::Particle::convert(corrP4), photon.charge(), 22);
 	
 	// IDs
-	output.cutBasedIDLoose_  = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_loose");
-	output.cutBasedIDMedium_ = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_medium");
-	output.cutBasedIDTight_  = photon.photonID("cutBasedPhotonID_Fall17_94X_V2_tight");
+	output.cutBasedIDLoose_  = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-loose");
+	output.cutBasedIDMedium_ = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-medium");
+	output.cutBasedIDTight_  = photon.photonID("cutBasedPhotonID-Fall17-94X-V2-tight");
 	
 	// Isolations
 	output.chargedIsolation_       = photon.userFloat("phoChargedIsolation");
