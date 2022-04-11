249388f897bd9bfe6d1f98d83b9f95ed0c5c505a
ÃGiovanni Marozzo <giovanni.marozzo@edu.unito.it>
2022-04-11 22:40:29 +0200

    new script to filter LHE files

---
+++ TreeAnalysis/python/lheFilter.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+import sys
+import os
+
+def filter(input_file_name):
+    
+    input_file = open(input_file_name, 'r')
+    output_file = open(output_file_name, 'w')
+
+    linelist=[]
+
+    in_ev   = 0 # To know if we're inside an event
+    in_ev_1 = 0 # The first line after <event> is information so we must skip that as well
+    had_num = 0 # Counts the amount of hadrons (excluding protons) in the event
+    header  = True
+
+    for line in input_file:
+
+        if line.startswith("#"): continue
+
+        if in_ev_1 == 1:
+            in_ev_1 = 0
+            in_ev = 1
+            continue
+    
+        if line.startswith("<event>"):
+            header = False
+            in_ev_1 = 1
+            continue
+  
+        if header: 
+            output_file.write(line)
+            continue
+    
+        if in_ev == 1 and line.startswith("</event>"):
+            in_ev = 0
+            if had_num == 0:
+                output_file.write("<event>\n")
+                for line1 in linelist:     
+                    print(line1)
+                    output_file.write(line1)
+                output_file.write("</event>\n")
+            had_num = 0
+            for line1 in linelist:
+                linelist.remove(line1)    #no clear method in lxplus's python version (?)
+            continue
+            
+        if (line.startswith("<") or line.startswith("  <") or line.startswith("   <")):
+            continue
+    
+        if in_ev == 1:
+        
+            if (abs(int(line.split()[0]))>23 and int(line.split()[0])!=2212):
+                had_num = had_num + 1
+            else:
+                #temp_file.write(line)
+                linelist.append(line) 
+            #print(line)
+    output_file.write("</LesHouchesEvents>")
+                      
+                      
+    input_file.close()     
+    temp_file.close()
+    output_file.close()
+ 
+
+input_file_name = sys.argv[1]
+temp_file_name = input_file_name.replace(".lhe","temp.lhe")
+output_file_name = input_file_name.replace(".lhe","filtered.lhe")
+
+
+filter(input_file_name)
