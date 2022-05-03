3c0f7ebab32d835f7fd1f1b86e9d03806ba7ab26
Riccardo <riccardo.bellan.cern.ch>
2022-05-03 15:17:45 +0200

    an example

--- TreeAnalysis/src/VVXAnalyzer.cc
+++ TreeAnalysis/src/VVXAnalyzer.cc
@@ -27,28 +27,36 @@ Int_t VVXAnalyzer::cut() {
 
 void VVXAnalyzer::analyze(){
 
-  theHistograms->fill("nZtoChLep"    ,"Number of Z->ll per event" , 7,0,7, genVBHelper_.ZtoChLep().size());
-  theHistograms->fill("nZtoNeutrinos","Number of Z->nn per event" , 7,0,7, genVBHelper_.ZtoNeutrinos().size());
-  theHistograms->fill("nWtoLep"      ,"Number of W->lnu per event", 7,0,7, genVBHelper_.WtoLep().size());
-  theHistograms->fill("nZtoQ"        ,"Number of Z->qq per event" , 7,0,7, genVBHelper_.ZtoQ().size());
-  theHistograms->fill("nWtoQ"        ,"Number of W->qq' per event", 7,0,7, genVBHelper_.WtoQ().size());
+  //theHistograms->fill("nZtoChLep"    ,"Number of Z->ll per event" , 7,0,7, genVBHelper_.ZtoChLep().size());
+  //theHistograms->fill("nZtoNeutrinos","Number of Z->nn per event" , 7,0,7, genVBHelper_.ZtoNeutrinos().size());
+  //theHistograms->fill("nWtoLep"      ,"Number of W->lnu per event", 7,0,7, genVBHelper_.WtoLep().size());
+  //theHistograms->fill("nZtoQ"        ,"Number of Z->qq per event" , 7,0,7, genVBHelper_.ZtoQ().size());
+  //theHistograms->fill("nWtoQ"        ,"Number of W->qq' per event", 7,0,7, genVBHelper_.WtoQ().size());
 
-  int nVBs = genVBHelper_.ZtoChLep().size() + genVBHelper_.ZtoNeutrinos().size() + genVBHelper_.WtoLep().size() + genVBHelper_.ZtoQ().size() + genVBHelper_.WtoQ().size();
-  theHistograms->fill("nVBs", "Number of VB per event", 7,0,7, nVBs);
+  //int nVBs = genVBHelper_.ZtoChLep().size() + genVBHelper_.ZtoNeutrinos().size() + genVBHelper_.WtoLep().size() + genVBHelper_.ZtoQ().size() + genVBHelper_.WtoQ().size();
+  //theHistograms->fill("nVBs", "Number of VB per event", 7,0,7, nVBs);
+
+  theHistograms->fill("ZZ4l_mass", "ZZ mass", 15 , 0, 1500, ZZ->mass(), theWeight);
+  int ZZsumid = abs(ZZ->first().daughter(0).id())+abs(ZZ->second().daughter(0).id());
+  if(ZZsumid == 22)      theHistograms->fill("ZZ4e_mass"  , "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
+  else if(ZZsumid == 23) theHistograms->fill("ZZ2e2m_mass", "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
+  else if(ZZsumid == 26) theHistograms->fill("ZZ4m_mass"  , "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
+
+
+  theHistograms->fill("ZW3l_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+  
+  int ZWsumid = abs(ZW->first().daughter(0).id())+abs(ZW->second().daughter(0).id());
+  if(ZWsumid == 22)                                   theHistograms->fill("ZW3e_tmass"  , "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+  else if(ZWsumid == 23){
+    if(abs(ZW->first().daughter(0).id()) == 11)       theHistograms->fill("ZZ2e1m_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+    else if (abs(ZW->first().daughter(0).id()) == 13) theHistograms->fill("ZZ2m1e_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+  }
+  else if(ZWsumid == 26)                              theHistograms->fill("ZW3m_tmass"  , "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+    
+
+
+  
 }
-
-void begin() {
-	cout<<"begin VVXAnalyzer\n";
-}
-
-void end(TFile &) {
-	cout<<"end VVXAnalyzer: [region]\n";//<<regionType(region_)<<'\n';
-}
-
-void true_end() {
-	cout<<"true_end VVXAnalyzer\n";
-}
-
 
 
 
