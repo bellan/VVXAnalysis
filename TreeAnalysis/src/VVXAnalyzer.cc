8cf87a2fc6d34b6fa10407c02b0acc6bb4516d20
Alberto <alberto.mecca@cern.ch>
2022-05-24 17:40:04 +0200

    Fixed sumid 23-->24; Added unweighted plots

--- TreeAnalysis/src/VVXAnalyzer.cc
+++ TreeAnalysis/src/VVXAnalyzer.cc
@@ -36,23 +36,44 @@ void VVXAnalyzer::analyze(){
   //int nVBs = genVBHelper_.ZtoChLep().size() + genVBHelper_.ZtoNeutrinos().size() + genVBHelper_.WtoLep().size() + genVBHelper_.ZtoQ().size() + genVBHelper_.WtoQ().size();
   //theHistograms->fill("nVBs", "Number of VB per event", 7,0,7, nVBs);
 
-  theHistograms->fill("ZZ4l_mass", "ZZ mass", 15 , 0, 1500, ZZ->mass(), theWeight);
+  theHistograms->fill("ZZ4l_mass"      , "ZZ4l mass"  , 15 , 0, 1500, ZZ->mass(), theWeight);
+  theHistograms->fill("ZZ4l_mass_u"    , "ZZ4l mass"  , 15 , 0, 1500, ZZ->mass());
   int ZZsumid = abs(ZZ->first().daughter(0).id())+abs(ZZ->second().daughter(0).id());
-  if(ZZsumid == 22)      theHistograms->fill("ZZ4e_mass"  , "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
-  else if(ZZsumid == 23) theHistograms->fill("ZZ2e2m_mass", "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
-  else if(ZZsumid == 26) theHistograms->fill("ZZ4m_mass"  , "ZZ mass", 15 , 0, 1500, ZZ->mass(),theWeight);
+  if(ZZsumid == 22){
+    theHistograms->fill("ZZ4e_mass"    , "ZZ4e mass"  , 15 , 0, 1500, ZZ->mass(), theWeight);
+    theHistograms->fill("ZZ4e_mass_u"  , "ZZ4e mass"  , 15 , 0, 1500, ZZ->mass());
+  }
+  else if(ZZsumid == 24){
+    theHistograms->fill("ZZ2e2m_mass"  , "ZZ2e2m mass", 15 , 0, 1500, ZZ->mass(), theWeight);
+    theHistograms->fill("ZZ2e2m_mass_u", "ZZ2e2m mass", 15 , 0, 1500, ZZ->mass());
+  }
+  else if(ZZsumid == 26){
+    theHistograms->fill("ZZ4m_mass"    , "ZZ4m mass"  , 15 , 0, 1500, ZZ->mass(), theWeight);
+    theHistograms->fill("ZZ4m_mass_u"  , "ZZ4m mass"  , 15 , 0, 1500, ZZ->mass());
+  }
 
-
-  theHistograms->fill("ZW3l_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+  theHistograms->fill("ZW3l_tmass"  , "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(), theWeight);
+  theHistograms->fill("ZW3l_tmass_u", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt());
   
   int ZWsumid = abs(ZW->first().daughter(0).id())+abs(ZW->second().daughter(0).id());
-  if(ZWsumid == 22)                                   theHistograms->fill("ZW3e_tmass"  , "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
-  else if(ZWsumid == 23){
-    if(abs(ZW->first().daughter(0).id()) == 11)       theHistograms->fill("ZZ2e1m_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
-    else if (abs(ZW->first().daughter(0).id()) == 13) theHistograms->fill("ZZ2m1e_tmass", "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
+  if(ZWsumid == 22){
+    theHistograms->fill("ZW3e_tmass"      , "ZW3e tmass"  , 15 , 0, 1500, ZW->p4().Mt(), theWeight);
+    theHistograms->fill("ZW3e_tmass"      , "ZW3e tmass"  , 15 , 0, 1500, ZW->p4().Mt());
   }
-  else if(ZWsumid == 26)                              theHistograms->fill("ZW3m_tmass"  , "ZW tmass", 15 , 0, 1500, ZW->p4().Mt(),theWeight);
-    
+  else if(ZWsumid == 24){
+    if(abs(ZW->first().daughter(0).id()) == 11){
+      theHistograms->fill("ZZ2e1m_tmass"  , "ZW2e1m tmass", 15 , 0, 1500, ZW->p4().Mt(), theWeight);
+      theHistograms->fill("ZZ2e1m_tmass_u", "ZW2e1m tmass", 15 , 0, 1500, ZW->p4().Mt());
+    }
+    else if (abs(ZW->first().daughter(0).id()) == 13){
+      theHistograms->fill("ZZ2m1e_tmass"  , "ZW2m1e tmass", 15 , 0, 1500, ZW->p4().Mt(), theWeight);
+      theHistograms->fill("ZZ2m1e_tmass_u", "ZW2m1e tmass", 15 , 0, 1500, ZW->p4().Mt());
+    }
+  }
+  else if(ZWsumid == 26){
+    theHistograms->fill("ZW3m_tmass"      , "ZW4m tmass"  , 15 , 0, 1500, ZW->p4().Mt(), theWeight);
+    theHistograms->fill("ZW3m_tmass_u"    , "ZW4m tmass"  , 15 , 0, 1500, ZW->p4().Mt());
+  }
 
 
   
