49cff8689639ba34f0dba7c064356aa2c857333a
Alberto <alberto.mecca@cern.ch>
2022-11-16 15:35:05 +0100

    Fixed paths for EGamma SF for Photons

--- Commons/src/PhotonSFHelper.cc
+++ Commons/src/PhotonSFHelper.cc
@@ -52,41 +52,48 @@ float PhotonSFHelper::getSFError(float pt, float eta/*, float SCeta*/) const
 
 
 const char* PhotonSFHelper::_getSFFilename(int year, bool preVFP) const{
-  const char *filename;
-  
-  const char* thefile;
+  const char *path;          // Final path to the file
+  const char POGrepo[] =     "/eos/cms/store/group/phys_egamma/SF-Repository";  // Path to the central EGamma repository
+  const char *folderEGamma;  // Folder inside the EGamma repository
+  const char *filename;      // Name of the SF file both in the EGamma folder and in our local "data/"
+
   switch(year){
     case 2016:
-      thefile = preVFP ? "egammaEffi.txt_EGM2D_Pho_Loose_UL16.root" : "egammaEffi.txt_EGM2D_Pho_Loose_UL16_postVFP.root";
+      filename = preVFP ?
+	"egammaEffi.txt_EGM2D_Pho_Loose_UL16.root" :
+	"egammaEffi.txt_EGM2D_Pho_Loose_UL16_postVFP.root";
+      folderEGamma = preVFP ?
+	"UL16/preVFP/Photons/Loose" :
+	"UL16/postVFP/Photons/Loose";
       break;
     case 2017:
-      thefile = "egammaEffi.txt_EGM2D_Pho_Loose_UL17.root";
+      filename = "egammaEffi.txt_EGM2D.root";
+      folderEGamma = "UL17/Photons/Loose/passingLoose100XV2_lowpTChebychev_addGaus";
       break;
     case 2018:
-      thefile = "egammaEffi.txt_EGM2D_Pho_Loose_UL18.root";
+      filename = "egammaEffi.txt_EGM2D.root";
+      folderEGamma = "UL18/Photons/Loose/passingLoose100XV2";
       break;
   }
   
   // First try the official egamma folder
-  const char repository[] = "/eos/cms/store/group/phys_egamma/SF-Repository";
-  
-  if(!gSystem->AccessPathName(repository)){
-    const char *folder = Form("UL%d", year%100);
+  if(!gSystem->AccessPathName(POGrepo)){
+    path = Form("%s/%s/%s", POGrepo, folderEGamma, filename);
+
+    if(!gSystem->AccessPathName(path))
+      return path;
+    else
+      std::cout << Form("[PhotonSFHelper] SF file not found in EGamma repo: (%s)\n", path);
+  }
+  // If the official repository is unreachable, fall back to local copies
+  else{
+    path = Form("$CMSSW_BASE/src/VVXAnalysis/Commons/data/%s", filename);
     
-    if(year == 2016)
-      folder = Form("%s/%s", folder, preVFP ? "preVFP" : "postVFP");    
-    
-    const char *filename = Form("%s/%s/Photons/Loose/%s", repository, folder, thefile);
-    
-    if(!gSystem->AccessPathName(filename))
-      return filename;
+    if(!gSystem->AccessPathName(path))
+      return path;
+    else
+      std::cout << Form("[PhotonSFHelper] SF file not found local folder: (%s)\n", path);
   }
   
-  // If the official repository is unreachable, fall back to local copies
-  filename = Form("$CMSSW_BASE/src/VVXAnalysis/Commons/data/%s", thefile);
-  
-  if(!gSystem->AccessPathName(filename))
-    return filename;
-  else
-    return nullptr;
+  return nullptr;
 }
