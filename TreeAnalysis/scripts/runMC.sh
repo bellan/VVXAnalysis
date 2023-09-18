3f4ee86f1a68968a6180b511d734cc46a71cb9f0
Alberto <alberto.mecca@cern.ch>
2023-09-18 11:37:24 +0200

    Added command parsing for year in runMC and rundata
    Minor optimizations in rundata.sh

--- TreeAnalysis/scripts/runMC.sh
+++ TreeAnalysis/scripts/runMC.sh
@@ -3,7 +3,7 @@
 haddOpt="-k -f"  # "-j $(grep processor /proc/cpuinfo | wc -l)"
 rmOpt="-r -f"
 analyzer=VVGammaAnalyzer # VVXAnalyzer
-years="2018"
+year=2018
 nevents=-1
 
 regions="SR4P;CR3P1F;CR2P2F;SR3P;CR110;CR101;CR011;CR100;CR010;CR001;CR000;SR2P;CRLFR"
@@ -15,28 +15,31 @@ if [ ! -d logdir ] ; then
     mkdir logdir
 fi
 
+while getopts "y:r:" option ; do
+    case "$option" in
+	y) year=$OPTARG ;;
+	r) regions=$OPTARG ;;
+    esac
+done
+
 echo -e "--- Analyses on MC ---\n"
+git log -1 --oneline HEAD
 
 make || exit
 
-for year in $years ; do
-    mc_samples=$(ls samples/MC/$year | grep -v $year | grep -oP ".+(?=\.root)" | grep -v "WLLGTo2L2j_5f_LO\|ZZTo4l_M1ToInf" | sort)
+mc_samples=$(ls samples/MC/$year | grep -v $year | grep -oP ".+(?=\.root)" | grep -v "WLLGTo2L2j_5f_LO\|ZZTo4l_M1ToInf" | sort)
 
-    for sample in $mc_samples ; do
-	./python/run.py $analyzer $sample -r $regions -y $year -n $nevents -d samples/MC $options >logdir/${sample}_${year}.log 2>&1 &
-    done
-    
-    wait
-    
-    for region in $(echo $regions | tr ';' ' ') ; do
-	(
-	    cd results/$year/${analyzer}_${region}
-    	    hadd $haddOpt ggTo4l.root ggTo4e_Contin_MCFM701.root ggTo2e2mu_Contin_MCFM701.root ggTo4mu_Contin_MCFM701.root
-	    hadd $haddOpt triboson.root ZZZ.root WZZ.root WWZ.root
-	    hadd $haddOpt ttXY.root TTWW.root TTZZ.root
-	)
-    done
-    
+for sample in $mc_samples ; do
+    ./python/run.py $analyzer $sample -r $regions -y $year -n $nevents -d samples/MC $options >logdir/${sample}_${year}.log 2>&1 &
 done
 
 wait
+
+for region in $(echo $regions | tr ';' ' ') ; do
+    (
+	cd results/$year/${analyzer}_${region}
+	hadd $haddOpt ggTo4l.root ggTo4e_Contin_MCFM701.root ggTo2e2mu_Contin_MCFM701.root ggTo4mu_Contin_MCFM701.root
+	hadd $haddOpt triboson.root ZZZ.root WZZ.root WWZ.root
+	hadd $haddOpt ttXY.root TTWW.root TTZZ.root
+    )
+done
--- TreeAnalysis/scripts/rundata.sh
+++ TreeAnalysis/scripts/rundata.sh
@@ -3,48 +3,53 @@
 haddOpt="-k -f"
 rmOpt="-r -f"
 analyzer=VVGammaAnalyzer  # VVXAnalyzer
-years="2018"
+year=2018
 
 regions="SR4P;CR3P1F;CR2P2F;SR3P;CR110;CR101;CR011;CR100;CR010;CR001;CR000;SR2P;CRLFR"
 options=""  # "--nofr"  # 
 
+while getopts "y:r:" option ; do
+    case "$option" in
+	y) year=$OPTARG ;;
+	r) regions=$OPTARG ;;
+    esac
+done
+
+echo -e "--- Analyses on Data ---\n"
+git log -1 --oneline HEAD
+
 make || exit
 
-# ~~~~~ Analysis on data
-echo -e "--- Analyses on Data ---\n"
-
-for year in $years ; do
-    data_samples=$(ls samples/Data/$year | grep $year | grep -oP ".+(?=\.root)" | sort)
-    for sample in ${data_samples} ; do
-        ./python/run.py $analyzer $sample -y $year -r $regions $options -d samples/Data &> logdir/${sample}.log &
-    done
-
-    wait
-
-    for region in $(echo $regions | tr ';' ' ') ; do
-        folder="results/$year/${analyzer}_${region}"
-        (
-            cd $folder
-            hadd $haddOpt data.root $(printf "%s.root " ${data_samples})  # printf reuses the format string to consume all of its arguments
-        )
-    done
-    
-    # Hadd lepton CRs only if they've been regenerated
-    if echo $regions | grep -q CR3P1F || echo $regions | grep -q CR2P2F ; then
-        hadd -k -f results/$year/${analyzer}_SR4P/fake_leptons.root \
-            results/$year/${analyzer}_CR3P1F/data.root \
-            results/$year/${analyzer}_CR2P2F/data.root
-    fi
-    
-    if echo $regions | grep -q CR110 || echo $regions | grep -q CR101 || echo $regions | grep -q CR011 || \
-       echo $regions | grep -q CR100 || echo $regions | grep -q CR010 || echo $regions | grep -q CR001 || echo $regions | grep -q CR000 ; then
-        hadd -k -f results/$year/${analyzer}_SR3P/fake_leptons.root \
-            results/$year/${analyzer}_CR110/data.root \
-            results/$year/${analyzer}_CR101/data.root \
-            results/$year/${analyzer}_CR011/data.root \
-            results/$year/${analyzer}_CR100/data.root \
-            results/$year/${analyzer}_CR010/data.root \
-            results/$year/${analyzer}_CR001/data.root \
-            results/$year/${analyzer}_CR000/data.root
-    fi
+data_samples=$(ls samples/Data/$year | grep $year | grep -oP ".+(?=\.root)" | sort)
+for sample in ${data_samples} ; do
+    ./python/run.py $analyzer $sample -y $year -r $regions $options -d samples/Data &> logdir/${sample}.log &
 done
+
+wait
+
+folder_year=results/$year
+for region in $(echo $regions | tr ';' ' ') ; do
+    (
+        cd ${folder_year}/${analyzer}_${region} || exit
+        hadd $haddOpt data.root $(printf "%s.root " ${data_samples})  # printf reuses the format string to consume all of its arguments
+    )
+done
+
+# Hadd lepton CRs only if they've been regenerated
+if echo $regions | grep -q CR3P1F || echo $regions | grep -q CR2P2F ; then
+    hadd $haddOpt ${folder_year}/${analyzer}_SR4P/fake_leptons.root \
+        ${folder_year}/${analyzer}_CR3P1F/data.root \
+        ${folder_year}/${analyzer}_CR2P2F/data.root
+fi
+
+if echo $regions | grep -q CR110 || echo $regions | grep -q CR101 || echo $regions | grep -q CR011 || \
+   echo $regions | grep -q CR100 || echo $regions | grep -q CR010 || echo $regions | grep -q CR001 || echo $regions | grep -q CR000 ; then
+    hadd $haddOpt ${folder_year}/${analyzer}_SR3P/fake_leptons.root \
+         ${folder_year}/${analyzer}_CR110/data.root \
+         ${folder_year}/${analyzer}_CR101/data.root \
+         ${folder_year}/${analyzer}_CR011/data.root \
+         ${folder_year}/${analyzer}_CR100/data.root \
+         ${folder_year}/${analyzer}_CR010/data.root \
+         ${folder_year}/${analyzer}_CR001/data.root \
+         ${folder_year}/${analyzer}_CR000/data.root
+fi
