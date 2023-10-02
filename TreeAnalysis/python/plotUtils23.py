679e2cd488367c0a930217343925847c786e2d73
Alberto <alberto.mecca@cern.ch>
2023-10-02 17:50:09 +0200

    function to rebin 2D histograms
    added verbose printout to rebin2D

--- TreeAnalysis/python/plotUtils23.py
+++ TreeAnalysis/python/plotUtils23.py
@@ -1,3 +1,5 @@
+from array import array
+from math import sqrt
 import ROOT
 
 class TFileContext(object):
@@ -23,3 +25,63 @@ def addIfExisting(*args):
         else:
             result.Add(a)
     return result
+
+
+def debug_hist(hist):
+    string = ''
+    for bx in range(1, hist.GetNbinsX()+1):
+        for by in range(1, hist.GetNbinsY()+1):
+            b = hist.GetBin(bx, by)
+            val = hist.GetBinContent(b)
+            string += ' '.join(('\tx: %d,  y: %d' %(bx, by)
+                                , '  bin: %2d' %(b)
+                                , ' [%.0f, %3.0f]' %(hist     .GetXaxis().GetBinLowEdge(bx), hist     .GetXaxis().GetBinUpEdge(bx))
+                                , ' [%.2f, %.2f]'  %(hist     .GetYaxis().GetBinLowEdge(by), hist     .GetYaxis().GetBinUpEdge(by))
+                                , ' value: %+6.3g' %(val)
+                                ))+'\n'
+    return string
+
+
+def rebin2D(hist_orig, x_bins=None, y_bins=None, verbose=False):
+    x_bins_orig = array('d', hist_orig.GetXaxis().GetXbins())
+    y_bins_orig = array('d', hist_orig.GetYaxis().GetXbins())
+    if(x_bins is None): x_bins = x_bins_orig
+    if(y_bins is None): y_bins = y_bins_orig
+    assert all(edge in x_bins_orig for edge in x_bins), 'x_bins must not contain edges which are not in the original histogram: '+str(x_bins_orig)
+    assert all(edge in y_bins_orig for edge in y_bins), 'y_bins must not contain edges which are not in the original histogram: '+str(y_bins_orig)
+
+    hist = ROOT.TH2F(hist_orig.GetName()+'_rebin', hist_orig.GetTitle(),
+                     len(x_bins) - 1, x_bins,
+                     len(y_bins) - 1, y_bins)
+
+    if(verbose): print('*****', hist_orig.GetName(), '*****')
+    for bx in range(1, hist_orig.GetNbinsX()+1):
+        x = hist_orig.GetXaxis().GetBinCenter(bx)
+        for by in range(1, hist_orig.GetNbinsY()+1):
+            y = hist_orig.GetYaxis().GetBinCenter(by)
+            b = hist_orig.GetBin(bx, by)
+            val = hist_orig.GetBinContent(b)
+            err = hist_orig.GetBinError(b)
+            b_new = hist.FindFixBin(x, y)
+            previous_val_new = hist.GetBinContent(b_new)
+            val_new = hist.GetBinContent(b_new) + val
+            err_new = sqrt( hist.GetBinError(b_new)**2 + err**2 )  # Manually track error because Fill() assumes that each call is a single entry --> err^2 = summ(w_i^2)
+            hist.SetBinContent(b_new, val_new)
+            hist.SetBinError  (b_new, err_new)
+            if(verbose):
+                print('\tx:', bx, ' y:', by
+                      , '  bin: %2d' %(b)
+                      , ' [%.0f, %3.0f]' %(hist_orig.GetXaxis().GetBinLowEdge(bx), hist_orig.GetXaxis().GetBinUpEdge(bx))
+                      , ' [%.2f, %.2f]'  %(hist_orig.GetYaxis().GetBinLowEdge(by), hist_orig.GetYaxis().GetBinUpEdge(by))
+                      , ' value: %+6.3g' %(val)
+                      , ' - new'
+                      , ' bin: %2d' %(b_new)
+                      , ' value: %+6.3g + %+6.3g = %+6.3g' %(previous_val_new, val, hist.GetBinContent(b_new))
+                      # , '+- %5.3g' %(err),
+                      # , ' -  err_new: %5.3g' %(err_new)
+                      )
+
+    if(verbose):
+        print('    * rebinned *')
+        print(debug_hist(hist))
+    return hist
