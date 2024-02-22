2ffa7982f33fd1fe15e2b634e6c9fb90c67b1a6b
Alberto Mecca <alberto.mecca@cern.ch>
2024-02-22 18:09:48 +0100

    py3: make_vclean

--- TreeAnalysis/python/make_vclean.py
+++ TreeAnalysis/python/make_vclean.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python
 
-import commands
-commands.getstatusoutput("make clean")
-commands.getstatusoutput("rm CMakeCache.txt cmake_install.cmake Makefile")
-commands.getstatusoutput("rm -r CMakeFiles/")
+import subprocess
+subprocess.run(["make", "clean"])
+subprocess.run(["rm", "CMakeCache.txt", "cmake_install.cmake", "Makefile"])
+subprocess.run(["rm", "-r", "CMakeFiles/"])
