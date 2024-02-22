0d536feccdedbe66b664f6609a2440f72827fe26
Alberto Mecca <alberto.mecca@cern.ch>
2024-02-22 18:09:48 +0100

    make_vclean is compatible with py2 and py3
    commands -> subprocess

--- TreeAnalysis/python/make_vclean.py
+++ TreeAnalysis/python/make_vclean.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python
 
-import commands
-commands.getstatusoutput("make clean")
-commands.getstatusoutput("rm CMakeCache.txt cmake_install.cmake Makefile")
-commands.getstatusoutput("rm -r CMakeFiles/")
+import subprocess
+subprocess.call(["make", "clean"])
+subprocess.call(["rm", "CMakeCache.txt", "cmake_install.cmake", "Makefile"])
+subprocess.call(["rm", "-r", "CMakeFiles/"])
