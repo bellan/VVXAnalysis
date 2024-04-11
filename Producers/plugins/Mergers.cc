007fb3667028e88a302d6e6999239dbe5b894b6d
Alberto Mecca <alberto.mecca@cern.ch>
2024-04-11 17:15:10 +0200

    Renamed the local class Merger to PriorityMerger to distinguish it from the one in CommonTools/UtilAlgos; updated it to global::EDProducer<>

--- Producers/plugins/Mergers.cc
+++ Producers/plugins/Mergers.cc
@@ -1,5 +1,5 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "VVXAnalysis/Producers/plugins/Merger.h"
+#include "VVXAnalysis/Producers/interface/PriorityMerger.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
 
 #include "DataFormats/Candidate/interface/Candidate.h"
@@ -7,7 +7,7 @@
 
 // A merger which produces a collection of pat::CompositeCandidate
 // (instead of a collection of reco::CompositeCandidate)
-typedef ZZAnalysis::Merger<pat::CompositeCandidateCollection, pat::CompositeCandidateCollection> PATCompositeCandidateMergerWithPriority;
+typedef ZZAnalysis::PriorityMerger<pat::CompositeCandidateCollection, pat::CompositeCandidateCollection> PATCompositeCandidateMergerWithPriority;
 DEFINE_FWK_MODULE( PATCompositeCandidateMergerWithPriority );
 
 #include "CommonTools/UtilAlgos/interface/Merger.h"
