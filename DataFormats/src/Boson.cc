f907f94820a87b7adc591dddf983d4f6f2648289
Alberto Mecca <alberto.mecca@cern.ch>
2024-04-12 17:01:09 +0200

    Added Jet to libVVXAnalysisDataFormat (and Boson<Jet>)

--- DataFormats/src/Boson.cc
+++ DataFormats/src/Boson.cc
@@ -1,6 +1,7 @@
 #include "VVXAnalysis/DataFormats/interface/Boson.h"
 #include "VVXAnalysis/DataFormats/interface/Lepton.h"
 #include "VVXAnalysis/DataFormats/interface/Photon.h"
+#include "VVXAnalysis/DataFormats/interface/Jet.h"
 
 template <class T>
 phys::Boson<T>::~Boson(){}
@@ -8,3 +9,4 @@ phys::Boson<T>::~Boson(){}
 template<> phys::Boson<phys::Particle>::~Boson(){}
 template<> phys::Boson<phys::Lepton  >::~Boson(){}
 template<> phys::Boson<phys::Photon  >::~Boson(){}
+template<> phys::Boson<phys::Jet     >::~Boson(){}
--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -1,12 +1,14 @@
 #include <VVXAnalysis/DataFormats/interface/Particle.h>
 #include <VVXAnalysis/DataFormats/interface/Lepton.h>
 #include <VVXAnalysis/DataFormats/interface/Photon.h>
+#include <VVXAnalysis/DataFormats/interface/Jet.h>
 #include <VVXAnalysis/DataFormats/interface/Boson.h>
 #include <VVXAnalysis/DataFormats/interface/DiBoson.h>
 
 phys::Boson<phys::Particle> dummyBosonParticle;
 phys::Boson<phys::Lepton>   dummyBosonLepton;
 phys::Boson<phys::Photon>   dummyBosonPhoton;
+phys::Boson<phys::Jet>      dummyBosonJet;
 
 phys::DiBoson<phys::Particle, phys::Particle> dummyDiBosonParticleParticle;
 phys::DiBoson<phys::Lepton  , phys::Lepton>   dummyDiBosonLeptonLepton;
