68acc299a5760da30b275f6471ddee638d4bdb83
Alberto Mecca <alberto.mecca@cern.ch>
2024-04-17 14:44:33 +0200

    Add Boson<Electron> explicit instantiation

--- DataFormats/src/Boson.cc
+++ DataFormats/src/Boson.cc
@@ -2,6 +2,7 @@
 #include "VVXAnalysis/DataFormats/interface/Lepton.h"
 #include "VVXAnalysis/DataFormats/interface/Photon.h"
 #include "VVXAnalysis/DataFormats/interface/Jet.h"
+#include "VVXAnalysis/DataFormats/interface/Electron.h"
 
 template <class T>
 phys::Boson<T>::~Boson(){}
@@ -10,3 +11,4 @@ template<> phys::Boson<phys::Particle>::~Boson(){}
 template<> phys::Boson<phys::Lepton  >::~Boson(){}
 template<> phys::Boson<phys::Photon  >::~Boson(){}
 template<> phys::Boson<phys::Jet     >::~Boson(){}
+template<> phys::Boson<phys::Electron>::~Boson(){}
