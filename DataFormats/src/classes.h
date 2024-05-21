1b8d3b40a2fff08b8f3e5f02a5dca10569ab1676
Alberto Mecca <alberto.mecca@cern.ch>
2024-05-21 14:25:05 +0200

    Declare explicit templates in classes_def.xml instead of the header classes.h

--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -8,27 +8,3 @@
 #include <VVXAnalysis/DataFormats/interface/RegionsCounter.h>
 #include <VVXAnalysis/DataFormats/interface/GenEventWeights.h>
 #include <VVXAnalysis/DataFormats/interface/MELA.h>
-
-phys::Boson<phys::Particle> dummyBosonParticle;
-phys::Boson<phys::Lepton>   dummyBosonLepton;
-phys::Boson<phys::Photon>   dummyBosonPhoton;
-phys::Boson<phys::Jet>      dummyBosonJet;
-phys::Boson<phys::Electron> dummyBosonElectron;
-
-phys::DiBoson<phys::Particle, phys::Particle > dummyDiBosonParticleParticle;
-phys::DiBoson<phys::Lepton  , phys::Lepton   > dummyDiBosonLeptonLepton;
-phys::DiBoson<phys::Lepton  , phys::Electron > dummyDiBosonLeptonElectron;
-phys::DiBoson<phys::Lepton  , phys::Jet      > dummyDiBosonLeptonJet;
-phys::DiBoson<phys::Lepton  , phys::Photon   > dummyDiBosonLeptonPhoton;
-phys::DiBoson<phys::Electron, phys::Lepton   > dummyDiBosonElectronLepton;
-phys::DiBoson<phys::Electron, phys::Electron > dummyDiBosonElectronElectron;
-phys::DiBoson<phys::Electron, phys::Jet      > dummyDiBosonElectronJet;
-phys::DiBoson<phys::Electron, phys::Photon   > dummyDiBosonElectronPhoton;
-phys::DiBoson<phys::Jet     , phys::Lepton   > dummyDiBosonJetLepton;
-phys::DiBoson<phys::Jet     , phys::Electron > dummyDiBosonJetElectron;
-phys::DiBoson<phys::Jet     , phys::Jet      > dummyDiBosonJetJet;
-phys::DiBoson<phys::Jet     , phys::Photon   > dummyDiBosonJetPhoton;
-phys::DiBoson<phys::Photon  , phys::Lepton   > dummyDiBosonPhotonLepton;
-phys::DiBoson<phys::Photon  , phys::Electron > dummyDiBosonPhotonElectron;
-phys::DiBoson<phys::Photon  , phys::Photon   > dummyDiBosonPhotonPhoton;
-phys::DiBoson<phys::Photon  , phys::Jet      > dummyDiBosonPhotonJet;
