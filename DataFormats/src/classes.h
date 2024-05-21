a05c92dcd84ba22e7f22664bd82b2803040abec2
Alberto Mecca <alberto.mecca@cern.ch>
2024-05-21 15:57:25 +0200

    Boson and DiBoson patterns to classes.h and classes_def.xml

--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -1,3 +1,13 @@
 #include <VVXAnalysis/DataFormats/interface/Particle.h>
 #include <VVXAnalysis/DataFormats/interface/Lepton.h>
 #include <VVXAnalysis/DataFormats/interface/Photon.h>
+#include <VVXAnalysis/DataFormats/interface/Boson.h>
+#include <VVXAnalysis/DataFormats/interface/DiBoson.h>
+
+phys::Boson<phys::Particle> dummyBosonParticle;
+phys::Boson<phys::Lepton>   dummyBosonLepton;
+phys::Boson<phys::Photon>   dummyBosonPhoton;
+
+phys::DiBoson<phys::Particle, phys::Particle> dummyDiBosonParticleParticle;
+phys::DiBoson<phys::Lepton  , phys::Lepton>   dummyDiBosonLeptonLepton;
+phys::DiBoson<phys::Photon  , phys::Photon>   dummyDiBosonPhotonPhoton;
