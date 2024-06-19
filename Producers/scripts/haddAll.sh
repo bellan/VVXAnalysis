d6e24165b69f07589843a6f2599dbafe066b044e
Alberto Mecca <alberto.mecca@cern.ch>
2024-06-19 12:28:57 +0200

    Rewrote haddAll so that it works with the --transfer option of batch_Condor.py

--- Producers/scripts/haddAll.sh
+++ Producers/scripts/haddAll.sh
@@ -24,21 +24,84 @@ for dir in $jobdirs ; do
     (
 	cd $dir
 	printf "%s \t--> " $dir
+        # Were all the samples in this jobdir completed?
 	if ls | grep -q Chunk ; then
+	    # ... no
 	    j_done=$( [ -d AAAOK ] && ls AAAOK/ | grep Chunk | wc -l || echo "0" )
 	    j_todo=$(ls        | grep Chunk | wc -l)
 	    printf "Still not done (%d/%d)\n" $j_done $(($j_done + $j_todo))
-	else
-	    samples=$(ls AAAOK/ | grep -oP ".+(?=_Chunk)" | uniq)
-	    echo $samples | sed "s/\n/ /g" | xargs printf "%s "
-	    dirsExist $(echo "$samples" | sed "s:^:AAAOK/:g") && echo "(Hadd-ed)" || haddChunks.py AAAOK
-	    for sampl in $samples ; do
-		[ -e AAAOK/$sampl/ZZ4lAnalysis.root ] || { echo "Tried to hadd but no rootfile has been created" 1>&2 ; exit 1 ; }
-		stripDir=$(echo $dir | sed -r "s:/[^/]+$::g")
-		mkdir -p "$eosdir/$stripDir"
-		destFile="$eosdir/$stripDir/$sampl.root"
-		[ -e $destFile ] || { rsync -au --progress AAAOK/$sampl/ZZ4lAnalysis.root $destFile && echo "Copied to $destFile" ; }
-	    done
+	    exit 0
 	fi
+	# ... yes
+
+        # There may be more than one sample in a jobdir
+        samples=$(ls AAAOK/ | grep -oP ".+(?=_Chunk)" | uniq)
+        echo $samples | sed "s/\n/ /g" | xargs printf "%s "
+
+	# Do we store rootfiles in AAAOK or is there a symlink at trasferPath?
+	has_transfer_path=false
+	for sample in $samples ; do
+	    if [ -L transferPath ] ; then
+		has_transfer_path=true
+	    elif has_transfer_path ; then
+		echo
+		echo "WARNING: some samples in $dir have a transferPath symlink, but $sample does not!" 1>&2
+		exit 1
+	    fi
+	done
+
+	# Hadd either with a transfer path (repeat for each sample) or with the classic AAAOK directory (once for all the samples in this jobdir)
+	jobdir="$(basename "${dir}")"
+	if $has_transfer_path ; then
+	    for sample in $samples ; do
+		target_hadd="transferPath/${jobdir}/"
+		# Was hadd already called?
+		# if [ -e "${target_hadd}/${sample}/ZZ4lAnalysis.root" ] ; then
+		if [ -d "${target_hadd}/${sample}" ] ; then
+		    echo "(Hadd-ed)"
+		else
+		    haddChunks.py "${target_hadd}"
+		fi
+	    done
+	else
+	    # Was hadd already called?
+	    if dirsExist $(echo "$samples" | sed "s:^:AAAOK/:g") ; then
+		echo "(Hadd-ed)"
+	    else
+		haddChunks.py AAAOK
+	    fi
+	fi
+
+	# Now transfer the rootfiles to eosdir
+        for sample in $samples ; do
+	    if ${has_transfer_path} ; then
+		sourceFile="transferPath/${jobdir}/${sample}/ZZ4lAnalysis.root"
+	    else
+		sourceFile="AAAOK/${sample}/ZZ4lAnalysis.root"
+	    fi
+	    stripDir=$(echo $dir | sed -r "s:/[^/]+$::g")
+	    destFile="$eosdir/$stripDir/${sample}.root"
+
+	    # Check that the source file exists
+	    if ! [ -e "$sourceFile" ] ; then
+		if [ -e "$destFile" ] ; then
+		    exit 0  # No source, but there is the destination. Probably the source was cleaned up
+		else
+		    printf "ERROR: Tried to hadd but no rootfile has been created at %s\n" "$sourceFile" 1>&2
+		    exit 1
+		fi
+	    fi
+
+	    # Transfer output
+            mkdir -p "$eosdir/$stripDir"
+	    if ! [ -e $destFile ] ; then
+		if [ $(stat -c "%d" "$eosdir") -eq $(stat -c "%d" "$sourceFile") ] ; then
+		    # Same filesystem, a simple mv will suffice
+		    mv "$sourceFile" "$destFile" && echo "Copied to $destFile"
+		else
+		    rsync -au --progress "$sourceFile" "$destFile" && echo "Copied to $destFile"
+		fi
+	    fi
+        done
     )
 done
