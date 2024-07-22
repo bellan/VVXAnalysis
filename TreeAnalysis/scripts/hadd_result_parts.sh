bead31c1c4a024110c843f4c41b534a00adc8654
Alberto Mecca <alberto.mecca@cern.ch>
2024-07-22 16:40:20 +0200

    Fix hadd_result_data, add option for a dry-run for both hadd_result_data and hadd_result_parts

--- TreeAnalysis/scripts/hadd_result_parts.sh
+++ TreeAnalysis/scripts/hadd_result_parts.sh
@@ -11,12 +11,16 @@ EOF
 }
 
 haddOpt="-k -f"
+dryrun=false
 OPTIND=1
-while getopts "h" opt; do
+while getopts "hd" opt; do
     case $opt in
 	h)
 	    show_help
 	    exit 0
+	    ;;
+	d)
+	    dryrun=true
 	    ;;
 	\?)
 	    show_help 1>&2
@@ -27,13 +31,13 @@ done
 shift "$((OPTIND-1))"
 
 [ $# -eq 1 ] || { show_help 1>&2 ; exit 1 ; }
-
+$dryrun && HADD="echo hadd" || HADD=hadd
 
 # Target files WITHOUT the ".root" suffix
 targets="$(find $1 -mindepth 3 -type f -name '*_part*.root' | sed -r 's:_part.+$::' | sort | uniq)"
 
 for target in $targets ; do
-    hadd $haddOpt $target.root ${target}_part*.root \
+    $HADD $haddOpt $target.root ${target}_part*.root \
 	|| continue
 	# && rm ${target}_part*.root \
 done
