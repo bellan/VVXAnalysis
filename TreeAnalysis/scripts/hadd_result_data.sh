070a5e43c52bbebd683ffd21b33ccaa5a5d71852
Alberto Mecca <alberto.mecca@cern.ch>
2024-07-22 16:40:20 +0200

    Fix hadd_result_data, add dry-run option for hadd_result_data and hadd_result_parts

--- TreeAnalysis/scripts/hadd_result_data.sh
+++ TreeAnalysis/scripts/hadd_result_data.sh
@@ -12,12 +12,16 @@ EOF
 }
 
 haddOpt="-k -f"
+dryrun=false
 OPTIND=1
-while getopts "h" opt; do
+while getopts "hd" opt; do
     case $opt in
 	h)
 	    show_help
 	    exit 0
+	    ;;
+	d)
+	    dryrun=true
 	    ;;
 	\?)
 	    show_help 1>&2
@@ -28,6 +32,7 @@ done
 shift "$((OPTIND-1))"
 
 [ $# -eq 1 ] || { show_help 1>&2 ; exit 1 ; }
+$dryrun && HADD="echo hadd" || HADD=hadd
 
 basenames(){
     # read stdin if present, arguments ($@) otherwise
@@ -46,14 +51,14 @@ for yeardir in $(find "$1" -maxdepth 1 -mindepth 1 -type d -name "20*" | sed s:^
 
     # Hadd data streams to create data.root in each region of each year
     for dir in $dirs; do
-    	data_samples=$(ls $dir/*.root | grep -P '((Single|Double)(Ele|Mu|EG)|EGamma|Mu(on)?EG)' | sort)
-        echo hadd $haddOpt $dir/data.root $(printf "$dir/%s " ${data_samples})  # printf reuses the format string to consume all of its arguments
+        data_samples=$(find $dir -name "*.root" | grep -P '((Single|Double)(Ele|Mu|EG)|EGamma|Mu(on)?EG)' | sort)
+        $HADD $haddOpt $dir/data.root ${data_samples}
     done
     echo
 
     # fake_leptons in SR4P
     if echo "$regions" | grep -q -e CR3P1F -e CR2P2F ; then
-        echo hadd $haddOpt $yeardir/${analyzer}_SR4P/fake_leptons.root \
+        $HADD $haddOpt $yeardir/${analyzer}_SR4P/fake_leptons.root \
     	    $yeardir/${analyzer}_CR3P1F/data.root \
     	    $yeardir/${analyzer}_CR2P2F/data.root
     fi
@@ -61,7 +66,7 @@ for yeardir in $(find "$1" -maxdepth 1 -mindepth 1 -type d -name "20*" | sed s:^
 
     # fake_leptons in SR3P
     if echo "$regions" | grep -q -P 'CR[10][10][10]' ; then
-        echo hadd $haddOpt $yeardir/${analyzer}_SR3P/fake_leptons.root \
+        $HADD $haddOpt $yeardir/${analyzer}_SR3P/fake_leptons.root \
     	    $yeardir/${analyzer}_CR110/data.root \
     	    $yeardir/${analyzer}_CR101/data.root \
     	    $yeardir/${analyzer}_CR011/data.root \
