3dc4f1af71a891cba09fa65026b520baf9448a55
bellan <riccardo.bellan>
2026-06-12 16:08:23 +0200

    test

--- NanoAnalysis/python/nanoVVXAnalysis.py
+++ NanoAnalysis/python/nanoVVXAnalysis.py
@@ -24,10 +24,11 @@ from VVXAnalysis.NanoAnalysis.Regions import flagDefinitions
 from VVXAnalysis.NanoAnalysis.Regions import Flags
 
 flagsSelection = [
-    Flags.L4P, Flags.L3P1F, Flags.L2P2F,
-    Flags.L3P, Flags.L2P1F, Flags.L1P2F, Flags.L3F,
-    Flags.L2P1L,
-    Flags.L2P_J2
+    # Flags.L4P, Flags.L3P1F, Flags.L2P2F,
+    # Flags.L3P, Flags.L2P1F, Flags.L1P2F, Flags.L3F,
+    # Flags.L2P1L,
+    # Flags.L2P_J2
+    Flags.L4P_L1P
 ]
     
 insertAfter(ZZSequence, 'jetFiller', FSTagger(flagDefinitions, flagsSelection))
