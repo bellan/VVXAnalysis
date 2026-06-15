0f0b146aa6a08d3f50cdcf0aff67d057af1945f6
bellan <riccardo.bellan>
2026-06-15 16:58:15 +0200

    typo

--- NanoAnalysis/python/Regions.py
+++ NanoAnalysis/python/Regions.py
@@ -39,7 +39,7 @@ class Flags(IntFlag):
 
 
     ## Composite flags ##
-    L4P_L1P    = L4P    | P1P
+    L4P_P1P    = L4P    | P1P
     L4P_J2     = L4P    | J2
     L3P_P1P    = L3P    | P1P
     L2P_J2     = L2P    | J2
