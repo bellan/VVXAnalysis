3a0a2d2517c8ec39e90eab067b3a4667a3cb8169
bellan <riccardo.bellan>
2026-06-15 17:01:07 +0200

    typo

--- NanoAnalysis/python/nanoVVXAnalysis.py
+++ NanoAnalysis/python/nanoVVXAnalysis.py
@@ -28,7 +28,7 @@ flagsSelection = [
     # Flags.L3P, Flags.L2P1F, Flags.L1P2F, Flags.L3F,
     # Flags.L2P1L,
     # Flags.L2P_J2
-    Flags.L4P_L1P
+    Flags.L4P_P1P
 ]
     
 insertAfter(ZZSequence, 'jetFiller', FSTagger(flagDefinitions, flagsSelection))
