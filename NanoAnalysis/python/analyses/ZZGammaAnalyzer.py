a19fcc80f599ef4b789014ba3483df2028143b53
Sara <sara.milanesio539@edu.unito.it>
2026-07-14 12:03:38 +0200

    signal definition kinematic requirements

--- NanoAnalysis/python/analyses/ZZGammaAnalyzer.py
+++ NanoAnalysis/python/analyses/ZZGammaAnalyzer.py
@@ -1,4 +1,5 @@
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Object
 from ZZAnalysis.NanoAnalysis.tools import getLeptons, get_genEventSumw
 
 from VVXAnalysis.NanoAnalysis.EventAnalyzer import EventAnalyzer
@@ -35,14 +36,35 @@ class ZZGammaAnalyzer(EventAnalyzer, analysis_name="ZZGammaAnalyzer"):
 
             if self.analyzeMC: self.weight = (self.event.overallEventWeight*theZZ.dataMCWeight/self.genEventSumw)
 
-            # ZZMass pre and post Fsr            
+            # ZZMass pre and post FSR            
             
             ZZMass = theZZ.mass
             self.hEvent.fill1D("ZZMass_10GeV", "ZZMass_10GeV", 93, 20., 1000., ZZMass, self.weight)
 
-            ZZMassPreFSR = theZZ.massPreFSR
-            self.hEvent.fill1D("ZZMassPreFSR_10GeV", "ZZMassPreFSR_10GeV", 93, 20., 1000., ZZMassPreFSR, self.weight)
+            ZZMassPreFsr = theZZ.massPreFSR
+            self.hEvent.fill1D("ZZMassPreFSR_10GeV", "ZZMassPreFSR_10GeV", 93, 20., 1000., ZZMassPreFsr, self.weight)
 
             GenZZMass = self.event.GenZZ_mass
-            self.hEvent.fill1D("GenZZMass_10GeV", "GenZZMass_10GeV", 93, 20., 1000., ZZMassPreFSR, self.weight)
-         
+            self.hEvent.fill1D("GenZZMass_10GeV", "GenZZMass_10GeV", 93, 20., 1000., GenZZMass, self.weight)
+          
+            # GenZZ: se guardo dentro ZZRo4l.root GenZZ ha indici, ma se li richiamo così vengono tutti 0
+            
+            idx1 = self.event.GenZZ_Z1l1Idx
+            self.hEvent.fill1D("GenZZidx1_10GeV", "GenZZIdx1_10GeV", 93, -100., 100., idx1, self.weight)
+          
+            # definizione di segnale
+
+            def ZZGammaSignalDefinition():
+                if not self.analyzeMC: return False
+
+                # leptons kinematic requirements
+                if len(GenLeptons) != 4: return False
+                if sum(l.pt > 5 for l in GenLeptons) < 4: return False
+                if sum(l.pt > 10 for l in GenLeptons) < 2: return False
+                if sum(l.pt > 20 for l in GenLeptons) < 1: return False
+                if any(abs(l.eta) > 2.5 for l in GenLeptons): return False
+
+                # photons kinematic requirments
+                if len(GenPhotons) < 1: return False
+                if sum(p.pt > 20 for p in GenPhotons) < 1: return False
+                if any(abs(p.eta) > 2.4 or 1.444 < abs(p.eta) < 1.566 for p in GenPhotons): return False
