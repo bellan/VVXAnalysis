8bbbcb708f2826c75cee6c569f7198926c3f08ff
bellan <riccardo.bellan>
2026-07-20 14:08:41 +0200

    WZGammaAnalyzer

---
+++ NanoAnalysis/python/analyses/WZGammaAnalyzer.py
@@ -0,0 +1,29 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from ZZAnalysis.NanoAnalysis.tools import getLeptons, get_genEventSumw
+
+from VVXAnalysis.NanoAnalysis.EventAnalyzer import EventAnalyzer
+from VVXAnalysis.NanoAnalysis.Histogrammer import *
+from VVXAnalysis.NanoAnalysis.Regions import Flags as Regions
+
+class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
+
+    def __init__(self, regions):
+        super().__init__(regions)
+
+    def analyze(self):
+        #print(Regions.check(self.regionWord,Regions.L4P))
+        bestCandIdx = self.event.bestCandIdx
+    
+        # Check that the event contains a selected candidate, and that
+        # passes the required triggers (which is necessary for samples
+        # processed with TRIGPASSTHROUGH=True)
+        if(bestCandIdx != -1 and self.event.HLT_passZZ4l): 
+            weight = 1.
+            ZZs = Collection(self.event, 'ZZCand') ## move it in EventAnalyzer::init(event) ??
+            theZZ = ZZs[bestCandIdx]
+            if self.analyzeMC: self.weight = (self.event.overallEventWeight*theZZ.dataMCWeight/self.genEventSumw)
+            
+            m4l = theZZ.mass
+
+            self.hEvent.fill1D("ZZMass_10GeV", "ZZMass_10GeV", 93, 70., 1000., m4l, self.weight)
+
