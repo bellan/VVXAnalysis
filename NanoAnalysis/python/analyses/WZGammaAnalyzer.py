cac6bad175b1c6098262a7d7c50be7cdec4c80c0
Giacomo <giacomo.conte@cern.ch>
2026-08-18 14:29:10 +0200

    Pull con prima implementazione signal region

--- NanoAnalysis/python/analyses/WZGammaAnalyzer.py
+++ NanoAnalysis/python/analyses/WZGammaAnalyzer.py
@@ -6,6 +6,7 @@ from VVXAnalysis.NanoAnalysis.Histogrammer import *
 from VVXAnalysis.NanoAnalysis.Regions import Flags as Regions
 
 import ROOT
+import math
 from ROOT import TDatabasePDG   
 
 
@@ -16,6 +17,7 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
 
     def analyze(self):   
         cutFlowStage = 0.
+        cutFlowStageSR = 0.
         if self.event.HLT_passZZ4l :
             weight = 1.   #da implementare.....  
     
@@ -23,9 +25,20 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
             GenChargedLeptons = [p for p in GenParts if abs(p.pdgId) in (11,13)]
             GenPhotons = [p for p in GenParts if p.pdgId == 22]
             GenNeutrinos = [p for p in GenParts if abs(p.pdgId) in (12,14)]
+            
+            Electrons = Collection(self.event, 'Electron')
+            Muons = Collection(self.event, 'Muon')
+            #ChargedLeptons = Collection(self.event, 'Lepton')
+            ChargedLeptons = list(Electrons) + list(Muons)
+            Photons = Collection(self.event, 'Photon')
+            ZCands = Collection(self.event, 'ZCand')
+            MET_pt = self.event.PFMET_pt 
+            bestZIdx = self.event.bestZIdx 
 
-                  
+            
+            #---------------------      
             #SIGNAL DEFINITION
+            #---------------------
             def isSignal() : 
                 # KINEMATIC CUT ON GENERATED PARTICLES
                 if self.pass_ChLepKinCut(GenChargedLeptons) is None : return False, 1.
@@ -38,25 +51,71 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
                 theW, theZ = self.WZRecon(GenBestLeptons, GenNeutrinos, True, False)
                 if theW is None or theZ is None : return False, 3.
                 else : 
-                    self.hEvent.fill1D("ZMass_91Gev", "ZMass_91Gev", 180, 60., 120., theZ.mass, self.weight)
-                    self.hEvent.fill1D("WMass_80Gev", "WMass_80Gev", 160, 50., 110., theW.mass, self.weight)
+                    self.hEvent.fill1D("ZGenMass_91Gev", "ZGenMass_91Gev", 180, 60., 120., theZ.mass, self.weight)
+                    self.hEvent.fill1D("WGenMass_80Gev", "WGenMass_80Gev", 160, 50., 110., theW.mass, self.weight)
                     #self.hEvent.fill1D("WPt", "WPt", 90, 0., 180., theW.pt, self.weight)
        
                 notpassedZ, invMassLLGamma = self.isPhotonFSR(theZ.leptons, genPhoton, 110)
-                self.hEvent.fill1D("llGamma_invMass", "llGamma_invMass", 100, 40., 250., invMassLLGamma, self.weight)
+                #self.hEvent.fill1D("llGammaGen_invMass", "llGammaGen_invMass", 100, 40., 250., invMassLLGamma, self.weight)
                 if notpassedZ : return False, 4.
 
                 # only cause the neutrino is gen
                 notpassedW, invMassLNuGamma = self.isPhotonFSR(theW.leptons, genPhoton, 100)
-                self.hEvent.fill1D("lNuGamma_invMass", "lNuGamma_invMass", 100, 40., 250., invMassLNuGamma, self.weight)
+                #self.hEvent.fill1D("lNuGammaGen_invMass", "lNuGammaGen_invMass", 100, 40., 250., invMassLNuGamma, self.weight)
                 if notpassedW : return False, 5.
                   
                 return True, -1.
 
             
-            verdict, cutFlowStage = isSignal()           
-        
-        self.hEvent.fill1D("CutFlowStage", "CutFlowStage",7, -1.5, 5.5, cutFlowStage, self.weight)
+            #-----------------------
+            # SIGNAL REGION
+            #-----------------------
+            def isInSignalRegion() :
+                # KINEMATIC CUT ON PARTICLES
+                if self.pass_ChLepKinCut(ChargedLeptons) is None : return False, 1.
+                else : BestLeptons = self.pass_ChLepKinCut(ChargedLeptons)  
+                                           
+                bestPhoton = self.pass_PhKinCut(Photons, ChargedLeptons)
+                if bestPhoton is None : return False, 2.
+                else : self.hEvent.fill1D("Gamma_pt", "Gamma_pt", 122, -0.0, 121., bestPhoton.pt, self.weight)
+
+                # CUT on MISSING ENERGY
+                self.hEvent.fill1D("MET_pt", "MET_pt", 121, -0.5, 120.5, MET_pt, self.weight)
+                if MET_pt <= 30: return False, 3.
+
+                # Z RECONSTRUCTION AND COMPARISON (ricostruire anche massa trasversa W)
+                '''theZRec, coupleZRec = self.BosonRecon(ChargedLeptons, 23, (60,120), False)
+                if theZRec is None : return False, 3.
+                else : self.hEvent.fill1D("ZRecMass_91Gev_SR", "ZRecMass_91Gev_SR", 180, 60., 120., theZRec.mass, self.weight)'''
+
+                theZ = ZCands[bestZIdx]
+                if 60. < theZ.mass < 120. : 
+                    self.hEvent.fill1D("ZCandMass_91Gev_SR", "ZCandMass_91Gev_SR", 180, 60., 120., theZ.mass, self.weight)
+                    lep1, lep2 = ChargedLeptons[theZ.l1Idx], ChargedLeptons[theZ.l2Idx]
+                    lepW = next((l for l in BestLeptons if l not in (lep1, lep2)), None)
+                    self.hEvent.fill1D("Wlep_pt", "Wlep_pt", 120, 0., 120., lepW.pt, self.weight)
+                else : return False, 4.
+
+                
+                # ricostruire la massa trasversa di W, mi sa che posso solo vedere il limite superiore per info su MET che ho
+                Mt_WMax = 2*math.sqrt(lepW.pt*MET_pt)
+                self.hEvent.fill1D("Mt_W_Max", "Mt_W_Max", 120, 0., 150.,  Mt_WMax, self.weight)
+                
+
+                notpassedZ, invMassLLGamma = self.isPhotonFSR([lep1,lep2], bestPhoton, 110)
+                self.hEvent.fill1D("llGamma_invMass", "llGamma_invMass", 100, 40., 250., invMassLLGamma, self.weight)
+                if notpassedZ : return False, 5.
+                    
+
+                return True, -1.
+                     
+
+            if self.analyzeMC : isSig, cutFlowStage = isSignal()   
+
+            isInSR, cutFlowStageSR = isInSignalRegion()
+
+        self.hEvent.fill1D("CutFlowStage", "CutFlowStage",8, -1.5, 6.5, cutFlowStage, self.weight)
+        self.hEvent.fill1D("CutFlowStage_SignalRegion", "CutFlowStage_SignalRegion",8, -1.5, 6.5, cutFlowStageSR, self.weight)
         
     
           
@@ -88,7 +147,7 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
             return max(BestPhotons, key=lambda p: p.pt, default=None)
         else : return None
 
-
+        
         
   
     #--------------------------------------
@@ -187,10 +246,15 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
 
     
     # given a variable number of "daughter particles" it calculates the invariant mass
-    def getInvMass(self, *particle) :
+    def getInvMass(self, *particles) :
         ptot = ROOT.TLorentzVector()
-        for part in particle :
-            ptot += part.p4()
+        for i, part in enumerate(particles):
+            if i == 2:  
+                p4_temp = ROOT.TLorentzVector()
+                p4_temp.SetPtEtaPhiM(part.pt, part.eta, part.phi, 0.0)
+                ptot += p4_temp
+            else:
+                ptot += part.p4()
                 
         return ptot.M()
 
@@ -205,7 +269,7 @@ class WZGammaAnalyzer(EventAnalyzer, analysis_name="WZGammaAnalyzer"):
             GoodLeptons.append(chosenNeutrino)
         return GoodLeptons
 
-
+        
 
 #---------------------------
 # other CLASSES
