7ad4a6b82537bd20f976d0b7aa8aadcf84773bcd
Riccardo <riccardo.bellan@cern.ch>
2026-08-20 19:32:19 +0200

    MET

--- NanoAnalysis/test/prod/pyFragments/nanoVVXAnalysis.py
+++ NanoAnalysis/test/prod/pyFragments/nanoVVXAnalysis.py
@@ -13,8 +13,8 @@ print("Overriding ZZ4lAnalysis PostProcessor")
 
 
 
-more_branchsel_in =['keep Photon*','keep MET*']
-more_branchsel_out=['keep Photon*','keep MET*','keep regionWord*']
+more_branchsel_in =['keep Photon*','keep *MET*']
+more_branchsel_out=['keep Photon*','keep PFMET*', 'keep PuppiMET*', 'keep MET*', 'keep regionWord*']
 
 #setConf("branchsel_in_ext"  , 'keep Photon*' , append=True)
 #setConf("branchsel_out_ext" , 'keep Photon*' , append=True)
